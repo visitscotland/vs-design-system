6a78122b4dd5df40cb93a799ca6c3b2249e1e5f9
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-03-01 13:04:16 +0000

    Removes commented out and redundant code from Logo rnderer

--- docs/components/Logo.js
+++ docs/components/Logo.js
@@ -2,10 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Styled from "rsg-components/Styled"
 import logoSvg from "../../src/assets/svg/logo.svg"
-// import docsConfig from '../../config/docs.config.js';
-
-const logoUrl =
-  "http://www.scopeproductions.co.uk/wp/wp-content/uploads/2016/10/LOGO-CLIENT-VISITSCOTLAND.png"
 
 const styles = ({ fontFamily, color }) => ({
   title: {
@@ -29,18 +25,6 @@ export function LogoRenderer({ classes, children }) {
   const h1 = React.createElement("h1", { className: classes.title }, children)
 
   return [logoWrapped, h1]
-
-  // const img = React.createElement('img', { className: classes.image, src: logoUrl, key: 'thistle-ds-logo-01' })
-  // const h1 = React.createElement('h1', null, children);
-
-  // return [img, h1];
-
-  // return (
-  // 	<h1 className={classes.logo}>
-  // 		<img className={classes.image} src={logo} />
-  // 		{children}
-  // 	</h1>
-  // );
 }
 
 LogoRenderer.propTypes = {
