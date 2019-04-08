00885b91e1bb462aac736087a8eca019b9da73d6
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-04-08 12:02:29 +0100

    Commits contentful auth info

--- config/remote.config.js
+++ config/remote.config.js
@@ -2,9 +2,9 @@ const transformConfig = require("../build/contentful-response-transform")
 
 module.exports = {
   // enter the correct URI and access_token for Contentful instance
-  uriBase: "https://cdn.contentful.com/spaces/xxxxxxxxxx/environments/master/entries",
+  uriBase: "https://cdn.contentful.com/spaces/xf5qyv95yk3c/environments/master/entries",
   uriParams: {
-    access_token: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    access_token: "73c1a3252bd526262639b627c11271fc6d561848991ef76ce37453431eeda6f6",
     content_type: "instance",
     include: 5,
   },
