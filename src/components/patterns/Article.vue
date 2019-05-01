91b12a6a298f11657ae732cae40e8a3218efcb63
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-05-01 20:42:38 +0100

    WIP: Example template
    - Adds content sections, article and sidebar components
    - Alters heading and hero components for example template
    - Removes redundant design system colours

---
+++ src/components/patterns/Article.vue
@@ -0,0 +1,41 @@
+<template>
+  <component :is="type" class="article"> <slot /> </component>
+</template>
+
+<script>
+/**
+ * Article component
+ */
+export default {
+  name: "VsArticle",
+  status: "prototype",
+  release: "0.0.1",
+  props: {
+    /**
+     * The html element name used for the component
+     */
+    type: {
+      type: String,
+      default: "article",
+    },
+  },
+  computed: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.article {
+  @extend .mt-5;
+}
+/deep/ .heading:first-of-type {
+  @extend .mb-5;
+}
+</style>
+
+<docs>
+  ```jsx
+  <div>
+  
+  </div>
+  ```
+</docs>
