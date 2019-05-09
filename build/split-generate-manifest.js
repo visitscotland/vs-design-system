3cde1cd051fac426054eac9b0c2c8918fa40746c
Luke Lewis <luke.lewis@edrington.com>
2019-05-10 00:47:49 +0100

    Completes add chunking
    - Adds webpack chunking optimization
    - Adds manifest plugin and creates custom manifest generation function

--- build/split-generate-manifest.js
+++ build/split-generate-manifest.js
@@ -1,28 +1,35 @@
-const { extend, reduce, invoke, mapKeys, map, filter } = require("lodash")
+const _ = require("lodash")
 
 function extractAllEntriesFromFileDescriptor(FileDescriptor) {
-  return reduce(
-    filter(FileDescriptor, "isInitial"),
+  return _.reduce(
+    _.filter(FileDescriptor, "isInitial"),
     (acc, val) => {
-      return extend({}, acc, extractEntriesFromOutputFile(val))
+      return _.extend({}, acc, extractEntriesFromOutputFile(val))
     },
     {}
   )
 }
 
 function extractEntriesFromOutputFile(outputFile) {
-  let entries = Array.from(invoke(outputFile, "chunk._groups.values"))
+  let entries = Array.from(_.invoke(outputFile, "chunk._groups.values"))
 
-  return entries.length ? mapKeys(entries, "options.name") : {}
+  return entries.length ? _.mapKeys(entries, "options.name") : {}
+}
+
+function generateEntry(entry) {
+  const files = _.flatMap(_.get(entry, "chunks"), "files")
+
+  return {
+    css: _.filter(files, _.ary(_.partial(_.endsWith, _, ".css"), 1)),
+    js: _.filter(files, _.ary(_.partial(_.endsWith, _, ".js"), 1)),
+  }
 }
 
 /**
  * Generates manifest file for split component build
  */
 module.exports = (Object, FileDescriptor) => {
-  let manifest = {}
-
   let componentEntries = extractAllEntriesFromFileDescriptor(FileDescriptor)
 
-  return {}
+  return _.mapValues(componentEntries, generateEntry)
 }
