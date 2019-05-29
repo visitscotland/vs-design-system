71b13ba49bf9351f373053a013675d427aff326c
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-05-29 18:41:11 +0100

    Adds full width toggle to docs examples

---
+++ docs/components/Examples.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Styled from "rsg-components/Styled"
+
+const styles = () => ({
+  // Just default jss-isolate rules
+  root: {},
+})
+
+export class ExamplesRenderer extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.node,
+  }
+
+  state = { fullWidth: false, baseClass: null }
+
+  constructor(props) {
+    super(props)
+    this.state.baseClass = props.classes.root
+  }
+
+  toggleFullWidth = () => {
+    this.setState({ fullWidth: !this.state.fullWidth })
+  }
+
+  render() {
+    let toggleButton = React.createElement(
+      "button",
+      { className: "vds-example-toggle", onClick: this.toggleFullWidth, key: 0 },
+      [(this.state.fullWidth ? "Exit" : "Enter") + " full width view"]
+    )
+
+    let className =
+      this.state.baseClass +
+      " vds-example" +
+      (this.state.fullWidth ? " vds-example-full-screen" : "")
+
+    return React.createElement("article", { className: className }, [
+      toggleButton,
+      ...this.props.children,
+    ])
+  }
+}
+
+export default Styled(styles)(ExamplesRenderer)
