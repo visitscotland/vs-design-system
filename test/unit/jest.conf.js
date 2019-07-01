ab688b8b5ca07ca4e19f361080d35cd81e4fe9c0
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-07-01 13:45:53 +0100

    Completes adding component unit testing
    - Moves jest configuration into jest.config.js file
    - Properly configures jest and babel for ES6 dependencies
    - Adds button component tests
    - Removes redundant cypress example files
    - Moves docs component tests into docs folder
    - Removes redundant router index file

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -2,6 +2,8 @@ const path = require("path")
 
 module.exports = {
   rootDir: path.resolve(__dirname, "../../"),
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/?(*.)jest.spec.js?(x)"],
   modulePaths: ["<rootDir>"],
   moduleFileExtensions: ["js", "json", "vue"],
   moduleNameMapper: {
@@ -11,20 +13,19 @@ module.exports = {
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
     ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
   },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(bootstrap-vue)/)"],
   snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
   setupFiles: ["<rootDir>/test/unit/setup"],
   coverageDirectory: "<rootDir>/test/unit/coverage",
   collectCoverageFrom: [
-    "<rootDir>/src/ExampleComponent.js",
-    "<rootDir>/src/util/**/*.{js,vue}",
-    "<rootDir>/docs/**/*.{js,vue}",
+    "<rootDir>/src/**/*.{js,vue}",
+    "!<rootDir>/cypress/**/*.{js,vue}",
+    "!<rootDir>/**/*.spec.js",
     "!<rootDir>/src/main.js",
     "!<rootDir>/src/router/index.js",
     "!<rootDir>/node_modules/**",
     "!<rootDir>/src/system.js",
-    "!<rootDir>/docs/docs.helper.js",
-    "!<rootDir>/docs/components/status/*",
-    "!<rootDir>/docs/components/Preview.js",
-    "!<rootDir>/docs/utils/tabs.js",
+    "!<rootDir>/src/system-components.js",
+    "!<rootDir>/docs/**/*",
   ],
 }
