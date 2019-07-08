57aa8cc0b98c49dc7a240cea374719fa1dc9cf27
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-07-08 17:07:44 +0100

    Completes adding tests for container component

--- src/components/elements/layout/layout.cypress.spec.js
+++ src/components/elements/layout/layout.cypress.spec.js
@@ -1,50 +1,71 @@
 const VsContainer = require("./Container.vue").default
 const { vueHelper, getToken } = require("@cypress/helpers/index.js").default
-const { each } = require("lodash")
+
+const mockContents = "test contents"
+
+const props = { fluid: null }
+
+const breakpoints = {
+  sm: getToken("breakpoint_sm"),
+  md: getToken("breakpoint_md"),
+  lg: getToken("breakpoint_lg"),
+  xl: getToken("breakpoint_xl"),
+  xxl: getToken("breakpoint_xxl"),
+}
+
+const maxWidths = {
+  sm: getToken("max_container_width_sm"),
+  md: getToken("max_container_width_md"),
+  lg: getToken("max_container_width_lg"),
+  xl: getToken("max_container_width_xl"),
+  xxl: getToken("max_container_width_xxl"),
+}
+
+vueHelper.init("vs-container", VsContainer, props, mockContents)
 
 describe("Container component", () => {
-  let props = { fluid: null }
-  let mockContents = "test contents"
-
-  let breakpoints = {
-    sm: getToken("breakpoint_sm"),
-    md: getToken("breakpoint_md"),
-    lg: getToken("breakpoint_lg"),
-    xl: getToken("breakpoint_xl"),
-    xxl: getToken("breakpoint_xxl"),
-  }
-
-  let maxWidths = {
-    sm: getToken("max_container_width_sm"),
-    md: getToken("max_container_width_md"),
-    lg: getToken("max_container_width_lg"),
-    xl: getToken("max_container_width_xl"),
-    xxl: getToken("max_container_width_xxl"),
-  }
-
-  vueHelper.init("vs-container", VsContainer, props, mockContents)
-
   beforeEach(() => {
     cy.document().then(doc => {
       doc.body.setAttribute("style", "margin:0")
     })
   })
 
-  it("wraps the contents in a static container", () => {
-    cy.contains("div", mockContents).should("have.class", "container")
+  it("should render a fluid width container below sm breakpoint", () => {
+    let viewportWidth = 450
+
+    cy.viewport(viewportWidth, 700)
+
+    cy.contains(".container", mockContents).should("have.css", "width", viewportWidth + "px")
   })
 
-  it("should render a 100% width container below sm breakpoint", () => {
-    cy.viewport(500, 700)
+  Cypress._.each(breakpoints, (breakpoint, breakpointName) => {
+    let fixedWidth = maxWidths[breakpointName]
 
-    cy.contains("div.container", mockContents).should("have.css", "width", "500px")
-  })
+    it(
+      "should render a fixed " +
+        fixedWidth +
+        " width container above " +
+        breakpointName +
+        " breakpoint",
+      () => {
+        cy.viewport(pixelStringToInteger(breakpoint) + 100, 700)
 
-  each(breakpoints, (breakpointMinWidth, breakpointName) => {
-    it("should render a fixed width container above " + breakpointName + " breakpoint", () => {
-      cy.viewport(pixelStringToInteger(breakpointMinWidth) + 100, 1000)
+        cy.contains(".container", mockContents).should("have.css", "width", fixedWidth)
+      }
+    )
 
-      cy.contains(".container", mockContents).should("have.css", "width", maxWidths[breakpointName])
+    it("should render a fluid width container  above " + breakpointName + " breakpoint", () => {
+      let viewportWidth = pixelStringToInteger(breakpoint) + 100
+
+      cy.viewport(viewportWidth, 700)
+
+      vueHelper.setProp("fluid", true)
+
+      cy.contains(".container-fluid", mockContents).should(
+        "have.css",
+        "width",
+        viewportWidth + "px"
+      )
     })
   })
 })
