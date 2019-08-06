27d6d42cdfb1715ee8ab090160f037b775d33b59
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-08-06 14:24:21 +0100

    Updates docs icon component

--- docs/components/Icons.vue
+++ docs/components/Icons.vue
@@ -1,35 +1,48 @@
 <template>
-  <div>
-    <pre>{{ icons }}</pre>
-  </div>
+  <card-grid>
+    <card v-for="path in icons" v-bind:key="path">
+      <vs-icon :name="path" />
+      <h3>{{ iconName(path) }}</h3>
+      <pre>{{ path }}</pre>
+    </card>
+  </card-grid>
 </template>
 
 <script>
 const allIcons = getAllIcons()
 import VsIcon from "@components/elements/icon/"
-import { trimStart, map, partial, trimEnd } from "lodash"
+import Card from "./common/Card"
+import CardGrid from "./common/CardGrid"
+import { trimStart, map, partial, trimEnd, capitalize, last, split } from "lodash"
 
 export default {
   name: "Icons",
-  components: { VsIcon },
+  components: { VsIcon, Card, CardGrid },
   data() {
     return {
       icons: allIcons,
     }
+  },
+  methods: {
+    iconName(path) {
+      return capitalize(last(split(path, "/")))
+    },
   },
 }
 
 function getAllIcons() {
   const all = require.context("@/assets/svg/icons", true, /^\.\/.*\.svg$/)
 
-  return map(all.keys(), path => {
-    return trimEnd(trimStart(path, "./"), ".svg")
+  return map(all.keys(), key => {
+    return trimEnd(trimStart(key, "./"), ".svg")
   })
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../styles/docs.tokens.scss";
+.card {
+  text-align: center;
+}
 </style>
 
 <docs>
