e0b68957c44e608411a77bc117dbd009659003e1
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-08-19 17:25:55 +0100

    Commits new system components build artifacts

--- dist/system-components/components/vendors~Article~Col~ColWrapper~Example~Hero~Sidebar~VueXExample~VueXExample2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[7],{VUAv:function(n,e,t){"use strict";var a=t("DVoA");e.a=function(n){return Object(a.e)(n)||(n=String(n)),(n=n.trim()).charAt(0).toUpperCase()+n.slice(1)}}}]);
