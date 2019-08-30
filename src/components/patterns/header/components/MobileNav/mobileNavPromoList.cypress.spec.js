9e232a4cd9ab5ca1304f18fbbbbf8a6735bfa376
Kelly Marshall <kelly.marshall@visitscotland.com>
2019-08-30 16:21:29 +0100

    Refactor MobileNav Cypress tests.

--- src/components/patterns/header/components/MobileNav/mobileNavPromoList.cypress.spec.js
+++ src/components/patterns/header/components/MobileNav/mobileNavPromoList.cypress.spec.js
@@ -1,16 +1,16 @@
-const VsMainNavPromoList = require("./MainNavPromoList.vue").default
+const VsMobileNavPromoList = require("./MobileNavPromoList.vue").default
 const { vueHelper } = require("@cypress/helpers/index.js").default
 const sizes = ["iphone-6", "ipad-2", [1024, 768]]
-const mainNav = require("../../../../assets/fixtures/mainNav.json")
+const mainNav = require("../../../../../assets/fixtures/mainNav.json")
 const promoList = mainNav[4].promoList
 
 const props = {
   list: promoList,
 }
 
-vueHelper.init("vs-main-nav-promo-list", VsMainNavPromoList, props)
+vueHelper.init("vs-mobile-nav-promo-list", VsMobileNavPromoList, props)
 
-describe("Main Nav Promo List component", () => {
+describe("Mobile Nav Promo List component", () => {
   sizes.forEach(size => {
     context(`${size} resolution`, () => {
       beforeEach(() => {
@@ -26,9 +26,17 @@ describe("Main Nav Promo List component", () => {
       })
 
       it("should render list items", () => {
-        cy.get(".vs-promo__list-item")
+        cy.get("li")
           .should("be.visible")
           .should("have.length", "5")
+      })
+
+      it("should only display the image of the last promo item", () => {
+        cy.get("img").should("have.length", "1")
+        cy.get("li")
+          .last()
+          .find("img")
+          .should("be.visible")
       })
     })
   })
