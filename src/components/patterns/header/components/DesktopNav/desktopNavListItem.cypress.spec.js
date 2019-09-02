64215461b5c7e82c9142565cf0a09030ba2ab8bf
Kelly Marshall <kelly.marshall@visitscotland.com>
2019-09-02 10:33:40 +0100

    add desktopNavListItem and desktopNavPromoItem cypress specs.

---
+++ src/components/patterns/header/components/DesktopNav/desktopNavListItem.cypress.spec.js
@@ -0,0 +1,93 @@
+const VsDesktopNavListItem = require("./DesktopNavListItem.vue").default
+const { vueHelper } = require("@cypress/helpers/index.js").default
+const sizes = ["iphone-6", "ipad-2", [1024, 768]]
+const mainNav = require("../../../../../assets/fixtures/mainNav.json")
+const item = mainNav[0]
+const level2 = item.subnav[0]
+const level3 = level2.subnav[0]
+
+const props = {
+  slot: "subnav",
+  level: 2,
+  href: level2.href,
+  subnav: level2.subnav,
+  isExternal: level2.isExternal,
+  title: level2.title,
+}
+
+const content = `
+  <vs-desktop-nav-list-item
+    slot: "subnav"
+    level: 3
+    href: ${level3.href}
+    subnav: ${level3.subnav}
+    is-external: ${level3.isExternal}
+    title: ${level3.title}
+  >
+  </vs-desktop-nav-list-item>
+`
+vueHelper.init("vs-desktop-nav-list-item", VsDesktopNavListItem, props, content)
+
+describe("Desktop Nav List Item component", () => {
+  sizes.forEach(size => {
+    context(`${size} resolution`, () => {
+      beforeEach(() => {
+        if (Cypress._.isArray(size)) {
+          cy.viewport(size[0], size[1])
+        } else {
+          cy.viewport(size)
+        }
+      })
+
+      it("should render a list item", () => {
+        cy.get(".vs-desktop-nav__list-item").should("be.visible")
+      })
+
+      // it("button should contain an icon", () => {
+      //   cy.get(".vs-mobile-nav__button")
+      //     .find("svg")
+      //     .should("be.visible")
+      // })
+
+      // it("clicking the button should toggle a submenu", () => {
+      //   cy.get("ul").should("not.be.visible")
+      //   cy.get(".vs-mobile-nav__button")
+      //     .first()
+      //     .click()
+      //   cy.get("ul").should("be.visible")
+      // })
+
+      // it("clicking the button should toggle the button aria-expanded state", () => {
+      //   cy.get(".vs-mobile-nav__button")
+      //     .first()
+      //     .should("not.have.attr", "aria-expanded", "true")
+      //     .click()
+      //     .should("have.attr", "aria-expanded", "true")
+      // })
+
+      // it("a button that opens a submenu should have an aria-haspopup property", () => {
+      //   cy.get(".vs-mobile-nav__button")
+      //     .first()
+      //     .should("have.attr", "aria-haspopup", "true")
+      //     .click()
+      //     .should("have.attr", "aria-expanded", "true")
+      // })
+
+      // it("the nav levels should rendered within the classes in each dom element", () => {
+      //   cy.get(".vs-mobile-nav__button--level1")
+      //     .first()
+      //     .should("be.visible")
+      //     .click()
+      //   cy.get(".vs-mobile-nav__list-item--level2")
+      //     .first()
+      //     .should("be.visible")
+      //   cy.get(".vs-mobile-nav__button--level2")
+      //     .first()
+      //     .should("be.visible")
+      //     .first()
+      //     .click()
+      //   cy.get(".vs-mobile-nav__list-item--level3").should("be.visible")
+      // })
+    })
+  })
+})
