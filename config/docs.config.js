1c9cbf6840216cbcf8f9af8f922c9eb0368c94d1
Kelly Marshall <kelly.marshall@visitscotland.com>
2019-09-05 10:13:42 +0100

    Update node engines settings to current Node LTS 10.16.3. Pin corejs setting in Babel as per Babel warning.

--- config/docs.config.js
+++ config/docs.config.js
@@ -23,6 +23,7 @@ const webpackBabelRuleUse = {
         "@babel/preset-env",
         {
           useBuiltIns: "usage",
+          corejs: "3.0.0",
           targets: {
             ie: "11",
             chrome: "71",
