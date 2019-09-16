8be936acd4f8b79d78d119c9aad490ccd33191d1
jose.calcines <jose.calcines@visitscotland.com>
2019-09-16 15:01:47 +0100

    Project created on version 13.3.0

---
+++ site/components/src/main/java/com/visitscotland/www/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.visitscotland.www.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="visitscotland:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
