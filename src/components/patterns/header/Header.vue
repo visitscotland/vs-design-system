74cb56874a0cc879b226c326fd14c56e841f6731
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-09-27 17:44:57 +0100

    Removes old header components
    - Replaces main VsHeader component with refactored VsHeader2
    - Removes favourites and search sub-components from VsHeader
    - Renames FavouritesButton2, FavouritesList2 and favourites2.store to FavouritesButton, FavouritesList and favourites.store

--- src/components/patterns/header/Header.vue
+++ src/components/patterns/header/Header.vue
@@ -26,14 +26,16 @@
                 <slot name="desktop-nav-toggles" />
               </ul>
               <div class="vs-controls__wrapper d-flex">
-                <slot name="search-button" /> <slot name="favourites-button" />
+                <slot name="header-drawer-toggles" />
                 <div class="d-lg-none"><slot name="mobile-nav-button" /></div>
               </div>
             </div>
           </vs-col>
         </vs-row>
       </vs-container>
-      <slot name="search" /> <slot name="favourites-list" />
+      <vs-header-drawer>
+        <slot name="header-drawer-modules" />
+      </vs-header-drawer>
       <div class="d-none d-lg-block">
         <vs-desktop-nav name="Main navigation"> <slot name="desktop-submenu" /> </vs-desktop-nav>
       </div>
@@ -47,24 +49,34 @@
 </template>
 
 <script>
-import VsContainer from "../../elements/layout/Container"
-import VsSvg from "../../elements/svg/Svg"
-import VsRow from "../../elements/layout/Row"
-import VsCol from "../../elements/layout/Col"
+import VsContainer from "@components/elements/layout/Container"
+import VsSvg from "@components/elements/svg/Svg"
+import VsRow from "@components/elements/layout/Row"
+import VsCol from "@components/elements/layout/Col"
 import smoothscroll from "smoothscroll-polyfill"
+import store, { names as storeNames } from "./header.store"
+import VsHeaderDrawer from "./components/Drawer/HeaderDrawer"
+import VsHeaderDrawerToggle from "./components/Drawer/HeaderDrawerToggle"
 
 export default {
   name: "VsHeader",
   status: "prototype",
   release: "0.0.1",
+  store,
   components: {
     VsCol,
     VsContainer,
     VsRow,
     VsSvg,
+    VsHeaderDrawer,
+    VsHeaderDrawerToggle,
   },
   data() {
-    return {}
+    return {
+      drawer: {
+        isOpen: !!store.getters["header/drawer/module"],
+      },
+    }
   },
   props: {
     /**
@@ -73,6 +85,22 @@ export default {
     type: {
       type: String,
       default: "header",
+    },
+    favouriteHref: {
+      type: String,
+    },
+    favouriteTitle: {
+      type: String,
+    },
+  },
+  computed: {
+    drawerModule() {
+      return store.getters["header/drawer/module"]
+    },
+  },
+  watch: {
+    drawerModule(newValue) {
+      this.drawer.isOpen = !!newValue
     },
   },
   methods: {
@@ -98,13 +126,6 @@ export default {
   },
   created() {
     smoothscroll.polyfill()
-  },
-  mounted() {
-    this.$root.$on("bv::collapse::state", (openedId, isOpen) => {
-      if (isOpen) {
-        this.collapseOtherMenus(openedId)
-      }
-    })
   },
 }
 </script>
@@ -165,156 +186,102 @@ export default {
   <template>
     <div style="overflow-y: scroll; min-height: 600px;">
       <vs-skip-to
-        tabindex="1000"
         :target="contentContainer"
       >
         Skip to Content
       </vs-skip-to>
 
-      <vs-skip-to
-        tabindex="1001"
-        @activated="skipToSearch"
+      <vs-header-drawer-toggle
+        module-name="site-search"
+        type="vs-skip-to-button"
       >
         Skip to Search
-      </vs-skip-to>
+      </vs-header-drawer-toggle>
 
-      <vs-header>
-        <vs-desktop-universal-nav
-          slot="desktop-universal-nav"
-          name="Our sites"
-          :dropdown-list="ourSites"
-        />
-        <vs-mobile-universal-nav
-          slot="mobile-universal-nav"
-          name="Our sites"
-          class="vs-dropdown--mobile-universal-nav"
-          :dropdown-list="ourSites"
-        />
-        <vs-login
-          slot="login" 
-          username=""
+      <vs-header
+        :favourite-href="jjj"
+        :favourite-title="jjj"
+      >
+
+        <template #desktop-universal-nav>
+          <vs-desktop-universal-nav
+            name="Our sites"
+            :dropdown-list="ourSites"
           />
-        <vs-language
-          slot="language"
-          name="Language"
-          class="vs-dropdown--language"
-          :dropdown-list="language"
-        />
-        <vs-logo 
-          slot="logo" />
+        </template>
 
-        <vs-search-button
-          slot="search-button" />
-        <vs-mobile-nav-button
-          slot="mobile-nav-button" />
-        <vs-favourites-button
-          slot="favourites-button"
-          :href="favourite.href"
-          :title="favourite.title" />
+        <template #mobile-universal-nav>
+          <vs-mobile-universal-nav
+            name="Our sites"
+            class="vs-dropdown--mobile-universal-nav"
+            :dropdown-list="ourSites"
+          />
+        </template>
 
-        <vs-search
-          slot="search" />
-        <vs-favourites-list
-          slot="favourites-list" />
-        
-        <vs-desktop-nav-toggles
-          slot="desktop-nav-toggles"
-          v-for="(item, index) in mainNav"
-          :level="1"
-          :href="item.href"
-          :is-external="item.isExternal"
-          :title="item.title"
-          :subnav="item.subnav"
-          :promo-list="item.promoList"
-          :promo-item="item.promoItem"
-          :chart-widgets="item.chartWidgets"
-          :key="index"
-          :toggleId="index+1"
-        ></vs-desktop-nav-toggles>
+        <template #login>
+          <vs-login
+            username=""
+          />
+        </template>
 
-        <vs-desktop-nav-submenu
-          slot="desktop-submenu"
-          v-for="(item, index) in mainNav"
-          :level="1"
-          :href="item.href"
-          :is-external="item.isExternal"
-          :tracking-id="item.trackingId"
-          :title="item.title"
-          :subnav="item.subnav"
-          :promo-list="item.promoList"
-          :promo-item="item.promoItem"
-          :chart-widgets="item.chartWidgets"
-          :key="index"
-          :subnavId="index+1"
-        >
-          <vs-desktop-nav-list-item
-            slot="subnav"
-            v-for="(level2, index2) in item.subnav"
-            :level="2"
-            :href="level2.href"
-            :is-external="level2.isExternal"
-            :tracking-id="level2.trackingId"
-            :title="level2.title"
-            :subnav="level2.subnav"
-            :key="index2"
-          >
-            <vs-desktop-nav-list-item
-              slot="subnav"
-              v-for="(level3, index3) in level2.subnav"
-              :level="3"
-              :href="level3.href"
-              :is-external="level3.isExternal"
-              :title="level3.title"
-              :tracking-id="level3.trackingId"
-              :subnav="level3.subnav"
-              :key="index3"
-            >
-            </vs-desktop-nav-list-item>
-          </vs-desktop-nav-list-item>
-        </vs-desktop-nav-submenu>
+        <template #language>
+          <vs-language
+            name="Language"
+            class="vs-dropdown--language"
+            :dropdown-list="language"
+          />
+        </template>
 
-        <vs-mobile-nav-list-item
-          slot="mobile-nav-items"
-          v-for="(item, index) in mainNav"
-          :level="1"
-          :href="item.href"
-          :is-external="item.isExternal"
-          :tracking-id="item.trackingId"
-          :title="item.title"
-          :subnav="item.subnav"
-          :promo-list="item.promoList"
-          :promo-item="item.promoItem"
-          :key="index"
-        >
-          <vs-mobile-nav-list-item
-            slot="subnav"
-            v-for="(level2, index2) in item.subnav"
-            :level="2"
-            :href="level2.href"
-            :is-external="level2.isExternal"
-            :tracking-id="level2.trackingId"
-            :title="level2.title"
-            :subnav="level2.subnav"
-            :promo-list="level2.promoList"
-            :promo-item="level2.promoItem"
-            :key="index2"
-          >
-            <vs-mobile-nav-list-item
-              slot="subnav"
-              v-for="(level3, index3) in level2.subnav"
-              :level="3"
-              :href="level3.href"
-              :is-external="level3.isExternal"
-              :title="level3.title"
-              :tracking-id="level3.trackingId"
-              :subnav="level3.subnav"
-              :promo-list="level3.promoList"
-              :promo-item="level3.promoItem"
-              :key="index3"
-            >
-            </vs-mobile-nav-list-item>
-          </vs-mobile-nav-list-item>
-        </vs-mobile-nav-list-item>
+        <template #logo>
+          <vs-logo />
+        </template>
+
+        <template #mobile-nav-button>
+          <vs-mobile-nav-button />
+        </template>
+
+        <template #header-drawer-toggles>
+
+          <vs-header-drawer-toggle
+            module-name="site-search"
+            type="vs-site-search-toggle-button"
+          />
+
+          <vs-header-drawer-toggle
+            module-name="favourites-list"
+            :show-close="true"
+            :href="favourite.href"
+            :title="favourite.title"
+            type="vs-favourites-button"
+          />
+
+        </template>
+
+        <template #header-drawer-modules>
+          <header-drawer-module module-name="site-search" ref="siteSearch" :show-close="true">
+            <vs-site-search />
+          </header-drawer-module>
+          <header-drawer-module module-name="favourites-list" :show-close="true">
+            <vs-favourites-list/>
+          </header-drawer-module>
+        </template>
+
+        <template #desktop-nav-toggles>
+          <vs-desktop-nav-toggles
+            v-for="(item, index) in mainNav"
+            :level="1"
+            :href="item.href"
+            :is-external="item.isExternal"
+            :title="item.title"
+            :subnav="item.subnav"
+            :promo-list="item.promoList"
+            :promo-item="item.promoItem"
+            :chart-widgets="item.chartWidgets"
+            :key="index"
+            :toggleId="index+1"
+          />
+        </template>
+
       </vs-header>
 
       <bs-wrapper class="container">
@@ -343,11 +310,13 @@ export default {
         language,
         mainNav,
         favourite,
-        contentContainer: null
+        contentContainer: null,
+        siteSearchModule: null
       }
     },
     mounted() {
       this.contentContainer = document.getElementById("content-container")
+      this.siteSearchModule = this.$refs.siteSearch
     },
     methods: {
       skipToSearch() {
