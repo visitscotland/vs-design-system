5c837218d086a762b03b452f2a6854ea7b654b66
Luke Lewis <luke.lewis@edrington.com>
2019-10-14 18:24:22 +0100

    Completes heading font fix and changes system components build
    - Removes src webFontLoader
    - Adds reference to heading fonts file in Headings component
    - Modifies system components build to build directly into dist folder
    - Changes publicPath build setting so asset paths are properly transformed
    - Changes Hippo frontend build to account for change in dist folder architecture
    - Updates Hippo templates accordingly

--- src/utils/core.system-components.js
+++ src/utils/core.system-components.js
@@ -1,3 +1,7 @@
-import "@/utils/webFontLoader" // eslint-disable-line no-unused-vars
-
+/*
+ *  This is the entry file for the core system components module.
+ *
+ *  Add assets that are needed for ALL components here. Add any core
+ *  sass to @/styles/core.styles.scss.
+ */
 import "@/styles/core.styles.scss"
