60aea21e31b1d575531153f2a0785e2fbb5329d6
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2019-10-25 10:16:39 +0100

    Adds temp fix for back tabbing problem with handDownFocus

--- src/directives/hand-down-focus.js
+++ src/directives/hand-down-focus.js
@@ -2,17 +2,17 @@ import { get, isFunction } from "lodash"
 
 /**
  * When an element with this directive is focused on, the
- * directive passes focus to the element's first child. 
+ * directive passes focus to the element passed as the value,
+ * or the element's first child if no value is given. The 
+ * element needs to be given a tabindex value to work.
  */
 
-export default {
-    inserted(el) {
+export default function(el, binding) {
         el.onfocus = () => {
-            const child = get(el, "childNodes[0]")
+            const $target = binding.value || get(el, "childNodes[0]")
         
-            if (isFunction(child.focus)) {
-              child.focus()
+            if (isFunction($target.focus)) {
+              $target.focus()
             }
         }
     }
-}
