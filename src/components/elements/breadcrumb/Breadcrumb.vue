b7218dee8e5b8d00504ae67e6cb581d44c3cd843
Kelly Marshall <kelly.marshall@vsweb.visitscotland.net>
2019-11-22 14:36:56 +0000

    Breadcrumbs initial commit.

---
+++ src/components/elements/breadcrumb/Breadcrumb.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav aria-label="breadcrumbs">
+    <b-breadcrumb class="flex-nowrap p-0" v-bind="$attrs">
+      <slot />
+    </b-breadcrumb>
+  </nav>
+</template>
+
+<script>
+import { BBreadcrumb } from "bootstrap-vue"
+
+/**
+ * Breadcrumbs are used to indicate the current page position within the navigational hierarchy.
+ */
+
+export default {
+  name: "VsBreadcrumb",
+  status: "prototype",
+  release: "0.0.1",
+  components: {
+    BBreadcrumb,
+  },
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~bootstrap/scss/breadcrumb";
+</style>
+
+<docs>
+  ```jsx
+  <vs-breadcrumb>
+    <vs-breadcrumb-item 
+      v-for="(item, index) in breadcrumb.breadcrumb"
+      :key="index"
+      :href="item.href"
+      :active="item.active"
+      :text="item.name"
+      >
+    </vs-breadcrumb-item>
+  </vs-breadcrumb>
+  ```
+</docs>
