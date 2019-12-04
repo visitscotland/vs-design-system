9df4555f940f0080bd39103345177216989e7f0e
Kelly Marshall <kelly.marshall@visitscotland.com>
2019-12-04 11:07:00 +0000

    Add day toggle

--- src/components/patterns/itineraries/components/itinerary-day/ItineraryDay.vue
+++ src/components/patterns/itineraries/components/itinerary-day/ItineraryDay.vue
@@ -1,11 +1,25 @@
 <template>
   <component :is="type" class="vs-itinerary-day__list-item">
-    <div class="vs-itinerary-day__header text-center">
+    <div class="vs-itinerary-day__header text-center position-relative">
       <slot name="day-title" />
+      <vs-button
+        class="vs-itinerary-day__toggle-button position-absolute p-0"
+        variant="transparent"
+        :animate="false"
+        @click.native="triggerToggle()"
+        aria-haspopup="true"
+        :aria-expanded="show ? 'true' : 'false'"
+      >
+        <vs-icon v-if="this.show" name="chevron-down" variant="dark" size="xs" :padding="3" />
+        <vs-icon v-else name="chevron-up" variant="dark" size="xs" :padding="3" />
+      </vs-button>
     </div>
-    <slot name="day-distance" />
-    <slot name="day-transport" />
-    <slot name="day-introduction" />
+    <div v-if="this.show">
+      <slot name="day-distance" />
+      <slot name="day-transport" />
+      <slot name="day-introduction" />
+      <slot name="stops" />
+    </div>
   </component>
 </template>
 <script>
@@ -28,6 +42,11 @@ export default {
     VsButton,
     VsIcon,
   },
+  data() {
+    return {
+      show: this.defaultShow,
+    }
+  },
   props: {
     /**
      * The html element name used for the component
@@ -36,9 +55,16 @@ export default {
       type: String,
       default: "li",
     },
+    defaultShow: {
+      type: Boolean,
+      default: true,
+    },
   },
-  computed: {},
-  methods: {},
+  methods: {
+    triggerToggle() {
+      this.show = !this.show
+    },
+  },
 }
 </script>
 
@@ -60,18 +86,11 @@ export default {
   padding: $spacer-3;
 }
 
-.itinerary-day__distance {
-  border-top: 1px solid $color-gray-tint-5;
-  margin: $spacer-9 -1rem -1rem;
-  padding: 1rem;
-  text-align: center;
-
-  dt {
-    margin-bottom: 1rem;
-    flex-basis: 1;
-    display: block;
-    width: 100%;
-  }
+.vs-itinerary-day__toggle-button {
+  border: 1px solid $color-base-text;
+  border-radius: 50%;
+  top: $spacer-2;
+  right: 0;
 }
 </style>
 
@@ -80,6 +99,7 @@ export default {
 <ul style="list-style-type: none; padding: 0px;">
 <vs-itinerary-day 
   v-for="(day, index) in itineraries.sampleItinerary.days"
+  :defaultShow="(day.count < 3) ? true : false"
   :key="index"
 >
 <vs-heading 
@@ -90,7 +110,6 @@ export default {
   <span>Day {{day.count}}</span>
   {{day.title}}
 </vs-heading>
-
   
   <dl v-if="day.dayMiles && day.dayKM" slot="day-distance" class="list-inline mb-0 text-center">
     <dt class="list-inline-item mb-0"><abbr title="miles">mi</abbr>/<abbr title="kilometres">km</abbr>:</dt>
