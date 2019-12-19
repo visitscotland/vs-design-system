bb66dc219733067f51f19fc89deaf4a05b79f490
Kelly Marshall <kelly.marshall@visitscotland.com>
2019-12-19 16:12:42 +0000

    Remove imports for deleted components

--- src/components/patterns/itineraries/index.js
+++ src/components/patterns/itineraries/index.js
@@ -1,21 +1,13 @@
 import VsItinerary from "./Itinerary"
 import VsItineraryDay from "./components/itinerary-day/ItineraryDay"
-import VsItineraryStopFacility from "./components/itinerary-stop-facility/ItineraryStopFacility"
-import VsItinerarySummaryList from "./components/itinerary-summary-list/ItinerarySummaryList"
-import VsItinerarySummaryListItem from "./components/itinerary-summary-list-item/ItinerarySummaryListItem"
 import VsItineraryStopPullout from "./components/itinerary-stop-pullout/ItineraryStopPullout"
-import VsItineraryTransportType from "./components/itinerary-transport-type/ItineraryTransportType"
 import VsItineraryMapMobileToggle from "./components/itinerary-map-mobile-toggle/ItineraryMapMobileToggle"
 import VsItineraryMap from "./components/itinerary-map/ItineraryMap"
 
 export {
   VsItinerary,
   VsItineraryDay,
-  VsItineraryStopFacility,
-  VsItinerarySummaryList,
-  VsItinerarySummaryListItem,
   VsItineraryStopPullout,
-  VsItineraryTransportType,
   VsItineraryMapMobileToggle,
   VsItineraryMap,
 }
