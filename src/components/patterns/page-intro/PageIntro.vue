f1c4434bd5c1c2ea14d68695039925ee7c91e72a
Kelly Marshall <kelly.marshall@visitscotland.com>
2020-01-14 10:24:04 +0000

    Add sample slot content to the PageIntro component

--- src/components/patterns/page-intro/PageIntro.vue
+++ src/components/patterns/page-intro/PageIntro.vue
@@ -8,7 +8,7 @@
 
 <script>
 /**
- * Itinerary Stop image element
+ * Component is just a wrapper for the page hero and introduction. Note that everything within is just a slot.
  */
 export default {
   name: "VsPageIntro",
@@ -54,8 +54,97 @@ export default {
 <docs>
   
   ```jsx
+    const sampleItinerary = require("../../../assets/fixtures/itineraries/sampleItinerary.json")
     <vs-page-intro>
-      
-    </vs-page-intro>
+      <vs-hero
+        :altText="itineraries.sampleItinerary.image.altText"
+        :credit="itineraries.sampleItinerary.image.credit"
+        :caption="itineraries.sampleItinerary.image.caption"
+        :image-src="itineraries.sampleItinerary.image.imageSrc"
+        :latitude="itineraries.sampleItinerary.image.latitude"
+        :longitude="itineraries.sampleItinerary.image.longitude"
+      >
+      <img 
+        class="lazyload" 
+        :src="itineraries.sampleItinerary.image.imageSrc"
+        srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+        :data-srcset="itineraries.sampleItinerary.image.imageSrc" 
+        :alt="itineraries.sampleItinerary.image.altText"
+        data-sizes="auto"
+        />
+      </vs-hero>
+      <vs-container>
+        <div class="vs-page-intro__wrapper--inner">
+          <vs-breadcrumb>
+            <vs-breadcrumb-item 
+              v-for="(item, index) in breadcrumb.breadcrumb"
+              :key="index"
+              :href="item.href"
+              :active="item.active"
+              :text="item.name"
+              >
+            </vs-breadcrumb-item>
+          </vs-breadcrumb>
+          <vs-row class="justify-content-md-between">
+            <vs-col cols="12" sm="6" lg="7">
+              <vs-heading level="1">
+                {{itineraries.sampleItinerary.h1Heading}}
+              </vs-heading>
+            </vs-col>
+          </vs-row>
+          <vs-row class="justify-content-md-between">
+            <vs-col cols="12" sm="6" lg="7">
+              <div class="lead" v-html="itineraries.sampleItinerary.introduction"></div>
+              <dl class="list-inline">
+                <dt class="list-inline-item">Start / Finish</dt>
+                <dd class="list-inline-item">{{itineraries.sampleItinerary.start}}/{{itineraries.sampleItinerary.finish}}</dd>
+              </dl>
+            </vs-col>
+            <vs-col cols="12" sm="6" md="5" lg="4">
+              <vs-summary-box-list class="bg-warning">
+                <vs-summary-box-list-item>
+                  <strong>Days</strong>
+                  <span>{{itineraries.sampleItinerary.totalDays}}</span>
+                </vs-summary-box-list-item>
+                <vs-summary-box-list-item>
+                  <strong>Distance <br /><abbr title="miles">mi</abbr>/<abbr title="kilometres">km</abbr></strong>
+                  <span>{{itineraries.sampleItinerary.totalMiles}}<span class="divider">/</span>{{itineraries.sampleItinerary.totalKM}}</span>
+                </vs-summary-box-list-item>
+                <vs-summary-box-list-item>
+                  <strong>Transport</strong>
+                  <div class="icon-wrapper">
+                    <vs-icon :name="itineraries.sampleItinerary.transport.key" variant="dark" size="sm" />
+                    {{itineraries.sampleItinerary.transport.value}}
+                  </div>
+                </vs-summary-box-list-item>
+                <vs-summary-box-list-item>
+                  <strong>Main theme</strong>
+                  <div class="icon-wrapper">
+                    <vs-icon :name="itineraries.sampleItinerary.theme.key" variant="dark" size="sm" />
+                    {{itineraries.sampleItinerary.theme.value}}
+                  </div>
+                </vs-summary-box-list-item>
+              </vs-summary-box-list>
+            </vs-col>
+          </vs-row>
+        </div>
+      <vs-itinerary-highlights-list>
+        <dt>Highlights</dt>
+        <dd 
+          v-for="(highlight, index) in itineraries.sampleItinerary.highlights"
+          class="mb-0"
+        >
+          {{highlight}}
+        </dd>
+        <dt class="mt-6">Areas Covered</dt>
+        <dd 
+          v-for="(areaCovered, index) in itineraries.sampleItinerary.areasCovered"
+          class="mb-0"
+        >
+          {{areaCovered}}
+        </dd>
+      </vs-itinerary-highlights-list>
+    </vs-container>
+  </vs-page-intro>
   ```
 </docs>
