e18db73a5a3efd87b0d8df1728bfedda81f7e188
Kelly Marshall <kelly.marshall@vsweb.visitscotland.net>
2020-01-16 16:59:37 +0000

    Add new props
    - add props for styling background, text and border of the summary box
    - adjust outer padding of the summary box

--- src/components/patterns/summary-box/SummaryBoxListItem.vue
+++ src/components/patterns/summary-box/SummaryBoxListItem.vue
@@ -1,11 +1,14 @@
 <template>
-  <component :is="type" cols="6" class="vs-summary-box__list-item p-2 text-center" tag="li">
-    <div
-      class="vs-summary-box__list-item__wrapper d-flex flex-column-reverse justify-content-between pt-5"
-    >
-      <slot />
-    </div>
-  </component>
+    <vs-col cols="6" class="vs-summary-box__list-item p-2 text-center" :text="border" tag="li">
+        <div
+            class="vs-summary-box__list-item__wrapper d-flex flex-column-reverse justify-content-between pt-5"
+            :class="{
+                [borderColorClass]: borderColorClass,
+            }"
+        >
+            <slot />
+        </div>
+    </vs-col>
 </template>
 
 <script>
@@ -14,57 +17,68 @@ import { VsCol } from "@components/elements/layout"
  * Itinerary Stop image element
  */
 export default {
-  name: "VsSummaryBoxListItem",
-  status: "prototype",
-  release: "0.0.1",
-  components: {
-    VsCol,
-  },
-  props: {
-    /**
-     * The image alt text for screen readers
-     */
-    type: {
-      type: String,
-      default: "vs-col",
+    name: "VsSummaryBoxListItem",
+    status: "prototype",
+    release: "0.0.1",
+    components: {
+        VsCol,
     },
-  },
+    props: {
+        /**
+         * Style the border color.
+         * `primary, secondary, success, danger, warning, info, light, dark, transparent`
+         */
+        border: {
+            type: String,
+            default: "dark",
+            validator: value => {
+                return value.match(
+                    /(primary|secondary|success|danger|warning|info|light|dark|transparent)/
+                )
+            },
+        },
+    },
+    computed: {
+        borderColorClass() {
+            return "border border-" + this.border
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .vs-summary-box__list-item ::v-deep {
-  dt {
-    font-family: $headings-font-family;
-    border-bottom: 1px solid $color-gray-tint-5;
-    margin-bottom: $spacer-3;
-    padding: $spacer-1 0;
-  }
-
-  .vs-summary-box__list-item__wrapper {
-    border: 1px solid $color-base-text;
-    width: 100%;
-    height: 100%;
-    min-height: 100px;
-    font-size: 0.875rem;
-
-    span {
-      font-size: 1.625rem;
-      font-family: $headings-font-family;
-
-      &.divider {
-        font-family: $headings-font-family-thin;
-      }
+    dt {
+        font-family: $headings-font-family;
+        border-bottom: 1px solid $color-gray-tint-5;
+        margin-bottom: $spacer-3;
+        padding: $spacer-1 0;
     }
-  }
+
+    .vs-summary-box__list-item__wrapper {
+        border: 1px solid $color-base-text;
+        width: 100%;
+        height: 100%;
+        min-height: 100px;
+        font-size: 0.875rem;
+
+        span {
+            font-size: 1.625rem;
+            font-family: $headings-font-family;
+
+            &.divider {
+                font-family: $headings-font-family-thin;
+            }
+        }
+    }
 }
 </style>
 
 <docs>
   
   ```jsx
-    <vs-summary-box-list>
-      <vs-summary-box-list-item>
+    <vs-summary-box-list textColor="light">
+      <vs-summary-box-list-item >
         <strong>Days</strong>
         <span>{{itineraries.sampleItinerary.totalDays}}</span>
       </vs-summary-box-list-item>
