acf359f516270608b3f4b6ba25e86eab1e8c9763
gstephen <gemma.stephen@visitscotland.com>
2020-01-17 15:54:13 +0000

    VS-950 added aria tags for exanding caption tooltip

--- src/components/patterns/image-with-caption/ImageWithCaption.vue
+++ src/components/patterns/image-with-caption/ImageWithCaption.vue
@@ -1,48 +1,51 @@
 <template>
-  <figure class="d-flex flex-column">
-    <div class="vs-image-with-caption__image-wrapper">
-      <slot />
-      <vs-button
-        variant="transparent"
-        class="position-absolute vs-image-with-caption__toggle-caption"
-        v-if="showToggle"
-        :animate="false"
-        @click.native="toggleCaption"
-      >
-        <vs-icon name="information" variant="light" size="sm" />
-        <span class="sr-only">{{ this.toggleButtonText }}</span>
-      </vs-button>
-    </div>
+    <figure class="d-flex flex-column">
+        <div class="vs-image-with-caption__image-wrapper">
+            <slot />
+            <vs-button
+                variant="transparent"
+                class="position-absolute vs-image-with-caption__toggle-caption"
+                v-if="showToggle"
+                :animate="false"
+                :aria-expanded="showCaption ? 'true' : 'false'"
+                aria-controls="vs-image-with-caption__caption-wrapper"
+                @click.native="toggleCaption"
+            >
+                <vs-icon name="information" variant="light" size="sm" />
+                <span class="sr-only">{{ this.toggleButtonText }}</span>
+            </vs-button>
+        </div>
 
-    <vs-container
-      class="position-relative vs-image-with-caption__caption-wrapper"
-      :class="[showCaption ? 'd-flex' : 'd-none']"
-    >
-      <figcaption ref="figcaption">
-        <vs-row>
-          <vs-col>
-            <div class="p-4" v-if="this.showCaptionData">
-              <p class="vs-image-with-caption__image-caption" v-if="this.caption">
-                {{ this.caption }}
-              </p>
-              <p class="vs-image-with-caption__image-credit m-0" v-if="this.credit">
-                &copy; {{ this.credit }}
-              </p>
-            </div>
-          </vs-col>
-          <vs-col cols="auto" class="pl-0" v-if="showMap">
-            <div class="map__wrapper">
-              <vs-image-location-map
-                :latitude="this.latitude"
-                :longitude="this.longitude"
-                map-outline-color="#191919"
-              ></vs-image-location-map>
-            </div>
-          </vs-col>
-        </vs-row>
-      </figcaption>
-    </vs-container>
-  </figure>
+        <vs-container
+            class="position-relative vs-image-with-caption__caption-wrapper"
+            :class="[showCaption ? 'd-flex' : 'd-none']"
+            id="vs-image-with-caption__caption-wrapper"
+        >
+            <figcaption ref="figcaption">
+                <vs-row>
+                    <vs-col>
+                        <div class="p-4" v-if="this.showCaptionData">
+                            <p class="vs-image-with-caption__image-caption" v-if="this.caption">
+                                {{ this.caption }}
+                            </p>
+                            <p class="vs-image-with-caption__image-credit m-0" v-if="this.credit">
+                                &copy; {{ this.credit }}
+                            </p>
+                        </div>
+                    </vs-col>
+                    <vs-col cols="auto" class="pl-0" v-if="showMap">
+                        <div class="map__wrapper">
+                            <vs-image-location-map
+                                :latitude="this.latitude"
+                                :longitude="this.longitude"
+                                map-outline-color="#191919"
+                            ></vs-image-location-map>
+                        </div>
+                    </vs-col>
+                </vs-row>
+            </figcaption>
+        </vs-container>
+    </figure>
 </template>
 
 <script>
@@ -56,149 +59,149 @@ import VsImageLocationMap from "@components/patterns/image-location-map/ImageLoc
  * Image with toggle to open a caption and image location map
  */
 export default {
-  name: "VsImageWithCaption",
-  status: "prototype",
-  release: "0.0.1",
-  components: { VsContainer, VsRow, VsCol, VsImageLocationMap, VsButton, VsIcon },
-  data() {
-    return {
-      showCaption: false,
-    }
-  },
-  props: {
-    /**
-     * The image alt text for screen readers
-     */
-    altText: {
-      type: String,
-      required: false,
+    name: "VsImageWithCaption",
+    status: "prototype",
+    release: "0.0.1",
+    components: { VsContainer, VsRow, VsCol, VsImageLocationMap, VsButton, VsIcon },
+    data() {
+        return {
+            showCaption: false,
+        }
     },
+    props: {
+        /**
+         * The image alt text for screen readers
+         */
+        altText: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The image credit
-     */
-    credit: {
-      type: String,
-      required: false,
-    },
+        /**
+         * The image credit
+         */
+        credit: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The caption for the image
-     */
-    caption: {
-      type: String,
-      required: false,
-    },
+        /**
+         * The caption for the image
+         */
+        caption: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The source URL for the image
-     */
-    imageSrc: {
-      type: String,
-      required: false,
-    },
+        /**
+         * The source URL for the image
+         */
+        imageSrc: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The image latitude
-     */
-    latitude: {
-      type: String,
-      required: false,
-    },
+        /**
+         * The image latitude
+         */
+        latitude: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The image longitude
-     */
-    longitude: {
-      type: String,
-      required: false,
-    },
+        /**
+         * The image longitude
+         */
+        longitude: {
+            type: String,
+            required: false,
+        },
 
-    /**
-     * The screenreader text for the toggle button
-     */
-    toggleButtonText: {
-      type: String,
-      default: "Toggle Caption",
+        /**
+         * The screenreader text for the toggle button
+         */
+        toggleButtonText: {
+            type: String,
+            default: "Toggle Caption",
+        },
     },
-  },
-  computed: {
-    backgroundSet() {
-      // TODO: finish computed property to build a whole data-bgset once
-      // the JAVA image scaling solution is finished.
-      return "data-bgset='" + this.imageSrc + " 320w [(max-width: 360px)]')"
+    computed: {
+        backgroundSet() {
+            // TODO: finish computed property to build a whole data-bgset once
+            // the JAVA image scaling solution is finished.
+            return "data-bgset='" + this.imageSrc + " 320w [(max-width: 360px)]')"
+        },
+        backgroundStyle() {
+            return "background-image: url('" + this.imageSrc + "');"
+        },
+        showCaptionData() {
+            return this.caption.length || this.credit.length ? true : false
+        },
+        showToggle() {
+            // only show the image detail toggle button if there's a map or caption data
+            return this.showMap || this.showCaptionData ? true : false
+        },
+        showMap() {
+            // only show the map if longitude and latitude props are both set
+            return this.longitude && this.latitude ? true : false
+        },
     },
-    backgroundStyle() {
-      return "background-image: url('" + this.imageSrc + "');"
+    methods: {
+        toggleCaption() {
+            return (this.showCaption = !this.showCaption)
+        },
     },
-    showCaptionData() {
-      return this.caption.length || this.credit.length ? true : false
-    },
-    showToggle() {
-      // only show the image detail toggle button if there's a map or caption data
-      return this.showMap || this.showCaptionData ? true : false
-    },
-    showMap() {
-      // only show the map if longitude and latitude props are both set
-      return this.longitude && this.latitude ? true : false
-    },
-  },
-  methods: {
-    toggleCaption() {
-      return (this.showCaption = !this.showCaption)
-    },
-  },
 }
 </script>
 
 <style lang="scss" scoped>
 .map__wrapper {
-  max-width: 80px;
-  width: 80px;
+    max-width: 80px;
+    width: 80px;
 }
 
 .vs-image-with-caption__caption-wrapper {
-  @include media-breakpoint-down(lg) {
-    max-width: 100%;
-    padding: 0;
-  }
+    @include media-breakpoint-down(lg) {
+        max-width: 100%;
+        padding: 0;
+    }
 }
 
 .vs-image-with-caption__toggle-caption {
-  bottom: 0;
-  padding: 0.325rem;
-  right: 0;
+    bottom: 0;
+    padding: 0.325rem;
+    right: 0;
 }
 
 figure {
-  position: relative;
+    position: relative;
 }
 
 img {
-  width: 100%;
-  height: auto;
+    width: 100%;
+    height: auto;
 }
 
 .vs-image-with-caption__image-wrapper {
-  position: relative;
+    position: relative;
 }
 
 figcaption {
-  background-color: $color-white;
-  color: $color-base-text;
-  width: 100%;
+    background-color: $color-white;
+    color: $color-base-text;
+    width: 100%;
 }
 
 .vs-image-with-caption__image-caption {
-  font-size: 0.875rem;
-  font-weight: 500;
-  line-height: 1rem;
+    font-size: 0.875rem;
+    font-weight: 500;
+    line-height: 1rem;
 }
 
 .vs-image-with-caption__image-credit {
-  font-size: 0.875rem;
-  font-weight: $font-weight-light;
-  line-height: 1rem;
+    font-size: 0.875rem;
+    font-weight: $font-weight-light;
+    line-height: 1rem;
 }
 </style>
 
