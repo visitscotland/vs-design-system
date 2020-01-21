add60a45026e2c26edfb474f174bdd5e8a62f74e
gstephen <gemma.stephen@visitscotland.com>
2020-01-21 14:01:30 +0000

    Changed list to use slots so this can also be used as an inline list

--- src/components/elements/description-list/DescriptionList.vue
+++ src/components/elements/description-list/DescriptionList.vue
@@ -1,16 +1,6 @@
 <template>
-    <dl v-if="items" class="vs-description-list py-7 mb-0">
-        <dt>{{ title }}</dt>
-        <dd v-for="item in items" :key="item" class="mb-0">
-            {{ item }}
-        </dd>
-    </dl>
-
-    <dl v-else-if="text" class="vs-description-list py-7 mb-0">
-        <dt>{{ title }}</dt>
-        <dd class="mb-0">
-            {{ text }}
-        </dd>
+    <dl class="vs-description-list">
+        <slot />
     </dl>
 </template>
 
@@ -23,35 +13,14 @@ export default {
     name: "VsDescriptionList",
     status: "prototype",
     release: "0.0.1",
-    props: {
-        /**
-         * The Title for the terms being defined
-         */
-        title: {
-            type: String,
-            required: true,
-        },
-        /**
-         * List of Items to be displayed in the definition
-         */
-        items: {
-            type: Array,
-            required: false,
-        },
-        /**
-         * String to be displayed in the defintion
-         */
-        text: {
-            type: String,
-            required: false,
-        },
-    },
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~bootstrap/scss/type";
+
 .vs-description-list {
-    dt {
+    &__title {
         font-family: $headings-font-family;
         border-bottom: 1px solid $color-gray-tint-5;
         margin-bottom: $spacer-3;
@@ -62,16 +31,29 @@ export default {
 
 <docs>
   ```jsx
-    <vs-description-list
-        title="Highlights"
-        :items="itineraries.sampleItinerary.highlights"
-    >
+    <vs-description-list class="mb-9">
+        <dt class="vs-description-list__title">Highlights</dt>
+        <dd 
+            v-for="(highlight, index) in itineraries.sampleItinerary.highlights"
+            class="mb-0"
+        >
+            {{highlight}}
+        </dd>
+        <dt class="vs-description-list__title mt-6">Areas Covered</dt>
+        <dd 
+            v-for="(areaCovered, index) in itineraries.sampleItinerary.areasCovered"
+            class="mb-0"
+        >
+            {{areaCovered}}
+        </dd>
     </vs-description-list>
 
-    <vs-description-list
-        title="Areas Covered"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar justo id risus imperdiet tristique."
-    >
+    <vs-description-list class="list-inline">
+        <dt class="list-inline-item">Start/Finish</dt>
+        <dd class="list-inline-item mb-0"
+        >
+            Angus/Fife
+        </dd>
     </vs-description-list>
   ```
 </docs>
