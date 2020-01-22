3670340265ff6772e19fd85121ece1c7a3e2dc5b
gstephen <gemma.stephen@visitscotland.com>
2020-01-22 11:02:12 +0000

    Refactoring to add 2 new dd/dt components and move classes inside component

--- src/components/elements/description-list/DescriptionList.vue
+++ src/components/elements/description-list/DescriptionList.vue
@@ -1,5 +1,5 @@
 <template>
-    <dl class="vs-description-list">
+    <dl class="vs-description-list" :class="{ 'list-inline': inline }">
         <slot />
     </dl>
 </template>
@@ -13,47 +13,36 @@ export default {
     name: "VsDescriptionList",
     status: "prototype",
     release: "0.0.1",
+    props: {
+        /**
+         * Option to create inline list
+         */
+        inline: {
+            type: Boolean,
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~bootstrap/scss/type";
-
-.vs-description-list {
-    &__title {
-        font-family: $headings-font-family;
-        border-bottom: 1px solid $color-gray-tint-5;
-        margin-bottom: $spacer-3;
-        padding: $spacer-1 0;
-    }
-}
 </style>
 
 <docs>
   ```jsx
-    <vs-description-list class="mb-9">
-        <dt class="vs-description-list__title">Highlights</dt>
-        <dd 
-            v-for="(highlight, index) in itineraries.sampleItinerary.highlights"
-            class="mb-0"
-        >
-            {{highlight}}
-        </dd>
-        <dt class="vs-description-list__title mt-6">Areas Covered</dt>
-        <dd 
-            v-for="(areaCovered, index) in itineraries.sampleItinerary.areasCovered"
-            class="mb-0"
-        >
-            {{areaCovered}}
-        </dd>
+    <vs-description-list inline>
+        <vs-description-list-term inline>Transport</vs-description-list-term>
+        <vs-description-list-detail inline>Walking</vs-description-list-detail>
+        <vs-description-list-detail inline>Bus</vs-description-list-detail>
     </vs-description-list>
 
-    <vs-description-list class="list-inline">
-        <dt class="list-inline-item">Start/Finish</dt>
-        <dd class="list-inline-item mb-0"
+    <vs-description-list>
+        <vs-description-list-term>Highlights</vs-description-list-term>
+        <vs-description-list-detail 
+            v-for="(highlight, index) in itineraries.sampleItinerary.highlights"
         >
-            Angus/Fife
-        </dd>
+            {{highlight}}
+        </vs-description-list-detail>
     </vs-description-list>
   ```
 </docs>
---
+++ src/components/elements/description-list/DescriptionListDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <dd class="vs-description-list__detail" :class="{ 'list-inline-item': inline }">
+        <slot />
+    </dd>
+</template>
+
+<script>
+/**
+ * TODO: Document usage
+ */
+
+export default {
+    name: "VsDescriptionListDetail",
+    status: "prototype",
+    release: "0.0.1",
+    props: {
+        /**
+         * Option to create inline list
+         */
+        inline: {
+            type: Boolean,
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~bootstrap/scss/type";
+
+.vs-description-list__detail {
+    margin-bottom: 0;
+}
+</style>
