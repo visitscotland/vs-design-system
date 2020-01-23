e8d1efcaa7e9b4ec5f3713275f977e96c063994e
gstephen <gemma.stephen@visitscotland.com>
2020-01-23 12:10:30 +0000

    Updated to create default 2 content layout and added comments

--- src/components/elements/description-list/DescriptionListTerm.vue
+++ src/components/elements/description-list/DescriptionListTerm.vue
@@ -16,14 +16,10 @@ export default {
     name: "VsDescriptionListTerm",
     status: "prototype",
     release: "0.0.1",
-    props: {
-        /**
-         * Option to create inline list
-         */
-        inline: {
-            type: Boolean,
-        },
-    },
+    /**
+     * Injects inline prop provided by DescriptionList
+     */
+    inject: ["inline"],
 }
 </script>
 
@@ -31,13 +27,24 @@ export default {
 @import "~bootstrap/scss/type";
 
 .vs-description-list__term {
+    // Responsive columns to achieve default 2 column list layout
     &.vs-description-list__term--styled {
         font-family: $headings-font-family;
-        border-bottom: 1px solid $color-gray-tint-5;
-        margin-bottom: $spacer-3;
-        padding: $spacer-1 0;
+        @include make-col(4);
+
+        @include media-breakpoint-down(md) {
+            @include make-col(6);
+        }
+
+        @include media-breakpoint-down(sm) {
+            border-bottom: 1px solid $color-gray-tint-5;
+            margin-bottom: $spacer-2;
+            padding: $spacer-1 0;
+            @include make-col(12);
+        }
     }
 
+    // Adds colon separator after inline term for consistency
     &.list-inline-item {
         &::after {
             content: ": ";
