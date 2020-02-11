2f75994d60a812fed11538428149742c7f194c46
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-02-11 15:42:46 +0000

    Prepares contents for merge into visitscotland-2019
    - Moves all contents into `frontend` subfolder

---
+++ frontend/hippo/site/components/src/main/java/com/visitscotland/designsystem/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.visitscotland.designsystem.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="cms:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
