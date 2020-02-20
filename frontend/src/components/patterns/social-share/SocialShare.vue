117416e60ea3b58a693535bf1941832d9f3cf901
Marcos Theofilo <marcos.theofilo@visitscotland.com>
2020-02-20 15:37:37 +0000

    VS-1119 FEAT (Social Share) Placeholder compoent created

---
+++ frontend/src/components/patterns/social-share/SocialShare.vue
@@ -0,0 +1,33 @@
+<template>
+    <vs-prototype>
+        <vs-icon name="share" />
+    </vs-prototype>
+</template>
+
+<script>
+import VsIcon from "@components/elements/icon/Icon.vue"
+import VsPrototype from "@components/elements/prototype/Prototype.vue"
+
+/**
+ * This is a placeholder component for now.
+ * Functionalities and styles still need to be coded.
+ */
+export default {
+    name: "VsSocialShare",
+    status: "prototype",
+    release: "0.0.1",
+
+    components: {
+        VsIcon,
+        VsPrototype
+    }
+}
+</script>
+
+<docs>
+```jsx
+    <bs-wrapper class="d-flex mb-4">
+        <vs-social-share />
+    </bs-wrapper>
+```
+</docs>
