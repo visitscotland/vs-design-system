45a39ed42dc17f23b9a6e23aeb70d65e84e6fdbd
Marcos Theofilo <marcos.theofilo@visitscotland.com>
2020-02-26 11:34:10 +0000

    VS-1053 FEAT (Rich Text) Rich Text Wrapper added

---
+++ frontend/src/components/elements/RichTextWrapper/RichTextWrapper.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="vs-rich-text-wrapper">
+		<slot/>
+	</div>
+</template>
+
+<script>
+	/**
+	 * Text Wrapper is used to wrap and render HTML or text strings from WYSIWYG editors or others and apply styles when needed.
+	*/
+	export default {
+		name: "VsRichTextWrapper",
+		status: "prototype",
+		release: "0.0.1",
+	}
+</script>
+
+<style>
+.vs-rich-text-wrapper {
+
+}
+</style>
+
+<docs>
+```jsx
+	<vs-rich-text-wrapper>
+		<h3>Heading Example</h3>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis <a href="#foo">eget</a> ante urna.<br> Pellentesque aliquam faucibus enim fermentum fringilla. Vivamus ultrices dictum justo ac porta.
+			Quisque mattis <b>tortor</b> dapibus tellus aliquet, finibus lacinia felis pulvinar.</br>
+	</vs-rich-text-wrapper>
+```
+</docs>
