dbc1697564982715131e255a6addad1f86573801
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2020-02-27 17:42:00 +0000

    FEAT (Listicle) (PSR)  picker extends abstract class

--- cms/src/main/java/com/visitscotland/brmx/components/DMSAwardPicker.java
+++ cms/src/main/java/com/visitscotland/brmx/components/DMSAwardPicker.java
@@ -30,22 +30,16 @@ import javax.jcr.Value;
 import java.util.*;
 
 // TODO: refactor
-public class DMSAwardPicker implements ExternalDocumentServiceFacade<JSONObject> {
+public class DMSAwardPicker extends AbstractDMSPicker {
 
     /**
      * Plugin parameter name for physical document field name (JCR property name).
      */
-    public static final String PARAM_EXTERNAL_DOCS_FIELD_NAME = "example.external.docs.field.name";
-    public static final String PRODUCT_TYPE = "dms.productype";
-    public static final String MULTIPLE_SELECTION = "selection.mode";
 
     private static final long serialVersionUID = 1L;
 
     private static Logger log = LoggerFactory.getLogger(DMSAwardPicker.class);
 
-    private JSONArray docArray;
-
-    private MetadataSearch metadata;
 
     public DMSAwardPicker() {
         try {
@@ -70,31 +64,8 @@ public class DMSAwardPicker implements ExternalDocumentServiceFacade<JSONObject>
     @Override
     public ExternalDocumentCollection<JSONObject> searchExternalDocuments(ExternalDocumentServiceContext context,
                                                                           String queryString) {
-        final String fieldName = context.getPluginConfig().getString(PRODUCT_TYPE);
-        try {
-            List<String> productTypes = Arrays.asList(context.getContextModel().getNode().getProperty(fieldName).getValue().getString().split("\\s*,\\s*"));
-            if (!productTypes.get(0).isEmpty()) {
-                List<FacilityGroup> facGroups = new ArrayList<>();
 
-                for (String productType : productTypes) {
-                    facGroups.addAll(metadata.getFacilityGroupsForType(ProductTypes.byId(productType)));
-                }
-
-                if (facGroups != null) {
-                    JSONArray subCategory = new JSONArray();
-                    for (FacilityGroup fac : facGroups) {
-                        subCategory.addAll(JSONArray.fromObject(fac.getFacilities()));
-                    }
-                    docArray = subCategory;
-                }
-
-            }
-        } catch (RepositoryException e) {
-            e.printStackTrace();
-        }
-
-        ExternalDocumentCollection<JSONObject> docCollection =
-                new SimpleExternalDocumentCollection<JSONObject>();
+        ExternalDocumentCollection<JSONObject> docCollection = new SimpleExternalDocumentCollection<JSONObject>();
         for (int i = 0; i < docArray.size(); i++) {
             JSONObject doc = docArray.getJSONObject(i);
             if (StringUtils.contains(doc.getString("name").toLowerCase(), queryString.toLowerCase())) {
@@ -104,112 +75,5 @@ public class DMSAwardPicker implements ExternalDocumentServiceFacade<JSONObject>
 
         return docCollection;
     }
-
-    //TODO : common method
-    @Override
-    public ExternalDocumentCollection<JSONObject> getFieldExternalDocuments(ExternalDocumentServiceContext context) {
-        final String fieldName = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-        final String multiple = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-
-        if (StringUtils.isBlank(fieldName)) {
-            throw new IllegalArgumentException("Invalid plugin configuration parameter for '"
-                    + PARAM_EXTERNAL_DOCS_FIELD_NAME + "': " + fieldName);
-        }
-
-        ExternalDocumentCollection<JSONObject> docCollection = new SimpleExternalDocumentCollection<JSONObject>();
-
-       try {
-            final Node contextNode = context.getContextModel().getNode();
-
-            if (contextNode.hasProperty(fieldName)) {
-                Value[] values = contextNode.getProperty(fieldName).getValues();
-
-                for (Value value : values) {
-                    String id = value.getString();
-                    JSONObject doc = findDocumentById(id);
-
-                    if (doc != null) {
-                        docCollection.add(doc);
-                    }
-                }
-            }
-        } catch (RepositoryException e) {
-            log.error("Failed to retrieve related exdoc array field.", e);
-        }
-
-        return docCollection;
-    }
-
-    //TODO : common method
-    @Override
-    public void setFieldExternalDocuments(ExternalDocumentServiceContext context,
-                                          ExternalDocumentCollection<JSONObject> exdocs) {
-        final String fieldName = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-
-        if (StringUtils.isBlank(fieldName)) {
-            throw new IllegalArgumentException("Invalid plugin configuration parameter for '"
-                    + PARAM_EXTERNAL_DOCS_FIELD_NAME + "': " + fieldName);
-        }
-
-        try {
-            final Node contextNode = context.getContextModel().getNode();
-            final List<String> docIds = new ArrayList<String>();
-
-            for (Iterator<? extends JSONObject> it = exdocs.iterator(); it.hasNext();) {
-                JSONObject doc = it.next();
-                docIds.add(doc.getString("id"));
-            }
-
-            if (!contextNode.isNodeType(HippoStdNodeType.NT_RELAXED)) {
-                contextNode.addMixin(HippoStdNodeType.NT_RELAXED);
-            }
-
-            contextNode.setProperty(fieldName, docIds.toArray(new String[docIds.size()]));
-        } catch (RepositoryException e) {
-            log.error("Failed to set related exdoc array field.", e);
-        }
-    }
-    //TODO : common method
-    @Override
-    public String getDocumentTitle(ExternalDocumentServiceContext context, JSONObject doc, Locale preferredLocale) {
-        if (doc != null && doc.has("id")) {
-            return doc.getString("id");
-        }
-
-        return "";
-    }
-    //TODO : common method
-    @Override
-    public String getDocumentDescription(ExternalDocumentServiceContext context, JSONObject doc,
-                                         Locale preferredLocale) {
-        if (doc != null && doc.has("name")) {
-            return doc.getString("name");
-        }
-
-        return "";
-    }
-
-    @Override
-    public String getDocumentIconLink(ExternalDocumentServiceContext context, JSONObject doc, Locale preferredLocale) {
-      /*  if (doc != null && doc.has("name")) {
-            return doc.getString("name");
-        }*/
-
-        return "";
-    }
-    //TODO : common method
-    private JSONObject findDocumentById(final String id) {
-        for (int i = 0; i < docArray.size(); i++) {
-            JSONObject doc = docArray.getJSONObject(i);
-
-            if (StringUtils.equals(id, doc.getString("id"))) {
-                return doc;
-            }
-        }
-
-        return null;
-    }
-
-
 
 }
--- cms/src/main/java/com/visitscotland/brmx/components/DMSFacilityPicker.java
+++ cms/src/main/java/com/visitscotland/brmx/components/DMSFacilityPicker.java
@@ -30,22 +30,16 @@ import java.io.Reader;
 import java.util.*;
 
 // TODO: refactor
-public class DMSFacilityPicker implements ExternalDocumentServiceFacade<JSONObject> {
+public class DMSFacilityPicker extends AbstractDMSPicker {
 
     /**
      * Plugin parameter name for physical document field name (JCR property name).
      */
-    public static final String PARAM_EXTERNAL_DOCS_FIELD_NAME = "example.external.docs.field.name";
-    public static final String PRODUCT_TYPE = "dms.productype";
-    public static final String MULTIPLE_SELECTION = "selection.mode";
 
     private static final long serialVersionUID = 1L;
 
     private static Logger log = LoggerFactory.getLogger(DMSFacilityPicker.class);
 
-    private JSONArray docArray;
-
-    private MetadataSearch metadata;
 
     public DMSFacilityPicker() {
         try {
@@ -114,112 +108,5 @@ public class DMSFacilityPicker implements ExternalDocumentServiceFacade<JSONObje
 
         return docCollection;
     }
-
-    //TODO : common method
-    @Override
-    public ExternalDocumentCollection<JSONObject> getFieldExternalDocuments(ExternalDocumentServiceContext context) {
-        final String fieldName = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-        final String multiple = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-
-        if (StringUtils.isBlank(fieldName)) {
-            throw new IllegalArgumentException("Invalid plugin configuration parameter for '"
-                    + PARAM_EXTERNAL_DOCS_FIELD_NAME + "': " + fieldName);
-        }
-
-        ExternalDocumentCollection<JSONObject> docCollection = new SimpleExternalDocumentCollection<JSONObject>();
-
-       try {
-            final Node contextNode = context.getContextModel().getNode();
-
-            if (contextNode.hasProperty(fieldName)) {
-                Value[] values = contextNode.getProperty(fieldName).getValues();
-
-                for (Value value : values) {
-                    String id = value.getString();
-                    JSONObject doc = findDocumentById(id);
-
-                    if (doc != null) {
-                        docCollection.add(doc);
-                    }
-                }
-            }
-        } catch (RepositoryException e) {
-            log.error("Failed to retrieve related exdoc array field.", e);
-        }
-
-        return docCollection;
-    }
-
-    //TODO : common method
-    @Override
-    public void setFieldExternalDocuments(ExternalDocumentServiceContext context,
-                                          ExternalDocumentCollection<JSONObject> exdocs) {
-        final String fieldName = context.getPluginConfig().getString(PARAM_EXTERNAL_DOCS_FIELD_NAME);
-
-        if (StringUtils.isBlank(fieldName)) {
-            throw new IllegalArgumentException("Invalid plugin configuration parameter for '"
-                    + PARAM_EXTERNAL_DOCS_FIELD_NAME + "': " + fieldName);
-        }
-
-        try {
-            final Node contextNode = context.getContextModel().getNode();
-            final List<String> docIds = new ArrayList<String>();
-
-            for (Iterator<? extends JSONObject> it = exdocs.iterator(); it.hasNext();) {
-                JSONObject doc = it.next();
-                docIds.add(doc.getString("id"));
-            }
-
-            if (!contextNode.isNodeType(HippoStdNodeType.NT_RELAXED)) {
-                contextNode.addMixin(HippoStdNodeType.NT_RELAXED);
-            }
-
-            contextNode.setProperty(fieldName, docIds.toArray(new String[docIds.size()]));
-        } catch (RepositoryException e) {
-            log.error("Failed to set related exdoc array field.", e);
-        }
-    }
-    //TODO : common method
-    @Override
-    public String getDocumentTitle(ExternalDocumentServiceContext context, JSONObject doc, Locale preferredLocale) {
-        if (doc != null && doc.has("id")) {
-            return doc.getString("id");
-        }
-
-        return "";
-    }
-    //TODO : common method
-    @Override
-    public String getDocumentDescription(ExternalDocumentServiceContext context, JSONObject doc,
-                                         Locale preferredLocale) {
-        if (doc != null && doc.has("name")) {
-            return doc.getString("name");
-        }
-
-        return "";
-    }
-
-    @Override
-    public String getDocumentIconLink(ExternalDocumentServiceContext context, JSONObject doc, Locale preferredLocale) {
-      /*  if (doc != null && doc.has("name")) {
-            return doc.getString("name");
-        }*/
-
-        return "";
-    }
-    //TODO : common method
-    private JSONObject findDocumentById(final String id) {
-        for (int i = 0; i < docArray.size(); i++) {
-            JSONObject doc = docArray.getJSONObject(i);
-
-            if (StringUtils.equals(id, doc.getString("id"))) {
-                return doc;
-            }
-        }
-
-        return null;
-    }
-
-
 
 }
