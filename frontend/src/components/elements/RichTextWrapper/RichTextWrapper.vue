4696f8a24e2d00720861f018eb0a7004711c194a
Marcos Theofilo <marcos.theofilo@visitscotland.com>
2020-03-02 13:57:17 +0000

    VS-1053 FEAT (RichText) Adding styles

--- frontend/src/components/elements/RichTextWrapper/RichTextWrapper.vue
+++ frontend/src/components/elements/RichTextWrapper/RichTextWrapper.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="vs-rich-text-wrapper">
+	<div
+		class="vs-rich-text-wrapper"
+        :class="[variant]"
+	>
 		<slot/>
 	</div>
 </template>
@@ -12,21 +15,51 @@
 		name: "VsRichTextWrapper",
 		status: "prototype",
 		release: "0.0.1",
+		props: {
+			/**
+			 * The font size used by the wrapper
+			 * `normal | lead`
+			 */
+			variant: {
+				type: String,
+				default: "normal",
+				validator: value => {
+					return value.match(/(normal|lead)/)
+				},
+			}
+		}
 	}
 </script>
 
-<style>
-.vs-rich-text-wrapper {
+<style lang="scss" scoped>
+@import "~bootstrap/scss/type";
 
+.normal ::v-deep {
+	font-family: $font-family-base;
+	font-size: $font-size-base;
+}
+
+.lead ::v-deep {
+	line-height: 1.75rem;
 }
 </style>
 
 <docs>
 ```jsx
-	<vs-rich-text-wrapper>
-		<h3>Heading Example</h3>
+	<bs-wrapper class="mb-9">
+		<h3>Normal Variant</h3>
+		<vs-rich-text-wrapper>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis <a href="#foo">eget</a> ante urna.<br/> Pellentesque aliquam faucibus enim fermentum fringilla. Vivamus ultrices dictum justo ac porta.
 			Quisque mattis <b>tortor</b> dapibus tellus aliquet, finibus lacinia felis pulvinar.
-	</vs-rich-text-wrapper>
+		</vs-rich-text-wrapper>
+    </bs-wrapper>
+
+	<bs-wrapper class="mb-4">
+		<h3>Lead Variant</h3>
+		<vs-rich-text-wrapper variant="lead">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis <a href="#foo">eget</a> ante urna.<br/> Pellentesque aliquam faucibus enim fermentum fringilla. Vivamus ultrices dictum justo ac porta.
+			Quisque mattis <b>tortor</b> dapibus tellus aliquet, finibus lacinia felis pulvinar.
+		</vs-rich-text-wrapper>
+    </bs-wrapper>
 ```
 </docs>
