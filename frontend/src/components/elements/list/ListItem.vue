201fcfd4de6e544316b80b144da80958b62fcd60
Marcos Theofilo <marcos.theofilo@visitscotland.com>
2020-03-03 14:26:03 +0000

    VS-1264 FEAT (ListItem) Adding ListItem

---
+++ frontend/src/components/elements/list/ListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li>
+      <slot/>
+  </li>
+</template>
+
+<script>
+/**
+ * List Item wrapper component, wraps the content into a list item element
+*/
+export default {
+    name: "VsListItem",
+    status: "prototype",
+    release: "0.0.1",
+}
+</script>
+
+<docs>
+```jsx
+    <ul>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+    </ul>
+
+    <ol>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+        <vs-list-item>
+            Item
+        </vs-list-item>
+    </ol>
+```
+</docs>
