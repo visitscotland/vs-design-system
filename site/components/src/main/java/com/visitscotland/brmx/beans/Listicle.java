dbf919bba49ea2ff4502632d7ba3f61c5237f405
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2020-03-27 11:54:00 +0000

    FEAT (Listicle) (Summary) summary and summaryTitle added

--- site/components/src/main/java/com/visitscotland/brmx/beans/Listicle.java
+++ site/components/src/main/java/com/visitscotland/brmx/beans/Listicle.java
@@ -3,6 +3,7 @@ package com.visitscotland.brmx.beans;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import java.util.List;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 
 @HippoEssentialsGenerated(internalName = "visitscotland:Listicle")
 @Node(jcrType = "visitscotland:Listicle")
@@ -14,5 +15,15 @@ public class Listicle extends Page {
     @HippoEssentialsGenerated(internalName = "visitscotland:descOrder")
     public Boolean getDescOrder() {
         return getSingleProperty("visitscotland:descOrder");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:summaryTitle")
+    public String getSummaryTitle() {
+        return getSingleProperty("visitscotland:summaryTitle");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:summary")
+    public HippoHtml getSummary() {
+        return getHippoHtml("visitscotland:summary");
     }
 }
