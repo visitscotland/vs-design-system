ba06b76c9437f9a362ea64b835be53204f16fab4
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-04-06 14:31:15 +0100

    Corrects lint errors in src/system.js and src/main.js

--- frontend/src/system.js
+++ frontend/src/system.js
@@ -7,28 +7,28 @@
 
 // Define contexts to require
 const contexts = [
-  require.context("@components/elements/", true, /\.vue$/),
-  require.context("@components/patterns/", true, /\.vue$/),
-  // require.context("@components/modules/", true, /\.vue$/),
-  require.context("@components/templates/", true, /\.vue$/),
+    require.context("@components/elements/", true, /\.vue$/),
+    require.context("@components/patterns/", true, /\.vue$/),
+    // require.context("@components/modules/", true, /\.vue$/),
+    require.context("@components/templates/", true, /\.vue$/),
 ]
 
 // Define components
 const components = []
-contexts.forEach(context => {
-  context.keys().forEach(key => components.push(context(key).default))
+contexts.forEach((context) => {
+    context.keys().forEach((key) => components.push(context(key).default))
 })
 
 // Install the above defined components
 const System = {
-  install(Vue) {
-    components.forEach(component => Vue.component(component.name, component))
-  },
+    install(Vue) {
+        components.forEach((component) => Vue.component(component.name, component))
+    },
 }
 
 // Automatic installation if Vue has been added to the global scope
 if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(System)
+    window.Vue.use(System)
 }
 
 // Finally export as default
