a226cdd7b69cc43c28a3e0bd3585fa9be348108b
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-04-06 17:22:45 +0100

    Lints and tidies src/utils system-components build scripts
    - Moves system-components build files into build folder from src/utils folder
    - Lints both files
    - Adds exception to import rule for build files

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -61,22 +61,30 @@ module.exports = {
 	},
 	settings: {
 		"import/resolver": {
+			node: {},
 			webpack: {
 				config: "./build/webpack.base.conf.js"
 			}
 		}
 	},
 	overrides: [
-		// Disable no-console rule for build scripts
+		// Modify rules for build scripts
 		{
 			"files": [ 
 				"src/system.js",
-				"src/utils/core.system-components.js",
-				"src/utils/entry.system-components.js",
 				"{build,config,test}/**/*",
 			],
 			"rules": {
-				"no-console": 0
+				// Disable no-console rule for build scripts
+				"no-console": 0,
+
+				// Allow devDeps to be used without error
+				"import/no-extraneous-dependencies": [
+					"error",
+					{
+						devDependencies: true
+					}
+				]
 			}
 		}
 	]
