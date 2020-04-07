5fea52a8e3a0d5eb4e71d030f02e767816cf05bd
gstephen <gemma.stephen@visitscotland.com>
2020-04-07 17:34:03 +0100

    Lining for element component files

--- frontend/src/components/elements/button/button.cypress.spec.js
+++ frontend/src/components/elements/button/button.cypress.spec.js
@@ -1,34 +1,38 @@
-const VsButton = require("./Button.vue").default
 const { vueHelper, getThemeColours } = require("@cypress/helpers/index.js").default
 const { each } = require("lodash")
+const VsButton = require("./Button.vue").default
 
 describe("Button component", () => {
-  const themeColours = getThemeColours()
-  const props = { variant: null, type: null, href: null }
+    const themeColours = getThemeColours()
+    const props = {
+        variant: null, type: null, href: null,
+    }
 
-  vueHelper.init("vs-button", VsButton, { props, childContent: "hello" })
-
-  it("renders the contents", () => {
-    cy.contains("button", "hello")
-  })
-
-  each(themeColours, (colourValue, colourName) => {
-    it("renders " + colourName + " colour", () => {
-      vueHelper.setProp("variant", colourName)
-
-      cy.get("button").should("have.css", "background-color", colourValue)
+    vueHelper.init("vs-button", VsButton, {
+        props, childContent: "hello",
     })
-  })
 
-  it("renders an anchor", () => {
-    const url = "http://www.visitscotland.com"
+    it("renders the contents", () => {
+        cy.contains("button", "hello")
+    })
 
-    vueHelper.setProp("href", url)
+    each(themeColours, (colourValue, colourName) => {
+        it(`renders ${colourName} colour`, () => {
+            vueHelper.setProp("variant", colourName)
 
-    cy.contains("a", "hello")
+            cy.get("button").should("have.css", "background-color", colourValue)
+        })
+    })
 
-    cy.get("a")
-      .should("have.attr", "href", url)
-      .should("have.css", "background-color", themeColours.primary)
-  })
+    it("renders an anchor", () => {
+        const url = "http://www.visitscotland.com"
+
+        vueHelper.setProp("href", url)
+
+        cy.contains("a", "hello")
+
+        cy.get("a")
+            .should("have.attr", "href", url)
+            .should("have.css", "background-color", themeColours.primary)
+    })
 })
