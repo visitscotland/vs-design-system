c0a75858cc3f38fc9e51731bc630e19f730a6787
harmendark <harmen.dark@visitscotland.com>
2020-04-14 14:04:22 +0100

    removed es6 include

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,5 @@
 // Vue Design System: The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
-import "es6-promise/auto"
-
 import Vue from "vue"
 import App from "@/App"
 import Meta from "vue-meta"
