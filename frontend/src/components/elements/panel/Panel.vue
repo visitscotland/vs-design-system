4168731396cc44aaecf672a0f35be0d815e01a09
gstephen <gemma.stephen@visitscotland.com>
2020-04-15 17:46:29 +0100

    Adding new panel component and integrating with listicles

---
+++ frontend/src/components/elements/panel/Panel.vue
@@ -0,0 +1,74 @@
+<template>
+    <BCard class="vs-panel">
+        <b-card-title class="vs-panel-title mb-8">
+            <!-- @slot Contains an optional title for the panel  -->
+            <slot name="vs-panel-title" />
+        </b-card-title>
+
+        <b-card-text class="vs-panel-text">
+            <!-- @slot default slot containing main body text -->
+            <slot />
+        </b-card-text>
+    </BCard>
+</template>
+
+<script>
+import { BCard } from "bootstrap-vue"
+
+/**
+ * The panel is used for simple content and contains slots for title and content.
+ */
+export default {
+    name: "VsPanel",
+    status: "prototype",
+    release: "0.0.1",
+    components: {
+        BCard,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.vs-panel {
+    background-color: $color-theme-light;
+    padding: $spacer-9 $spacer-4 $spacer-9;
+
+    @include media-breakpoint-up(md) {
+        padding: $spacer-9 $spacer-8 $spacer-10;
+    }
+
+    @include media-breakpoint-up(lg) {
+        padding: $spacer-9 $spacer-11 $spacer-10;
+    }
+
+    @include media-breakpoint-up(xxl) {
+        padding: $spacer-9 $spacer-12 $spacer-10;
+    }
+
+    .vs-panel-title {
+        display: block;
+        text-align: center;
+    }
+
+    .vs-panel-text {
+        .vs-rich-text-wrapper.lead {
+            font-size: $font-size-base;
+            line-height: $line-height-m;
+        }
+    }
+}
+</style>
+
+<docs>
+```jsx
+	<vs-panel>
+        <vs-heading thin level="4" slot="vs-panel-title">A Title Would Go Here</vs-heading>
+
+        <vs-rich-text-wrapper variant="lead">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus est congue nunc fermentum, eu hendrerit odio fringilla. Donec porta eleifend ipsum Praesent in magna fringilla.</p>
+        
+            <p>Morbi faucibus risus in varius sollicitudin. Aenean eget leo ullamcorper, scelerisque urna eu, imperdiet nibh. Duis sit amet nibh tortor. Donec ac ante eros. Praesent in magna fringilla, consectetur eros id, fermentum nibh. Duis luctus ante sapien, scelerisque fermentum massa tincidunt eu. Nullam sed arcu a justo auctor consectetur.</p>
+        </vs-rich-text-wrapper>
+    </vs-panel>
+```
+</docs>
