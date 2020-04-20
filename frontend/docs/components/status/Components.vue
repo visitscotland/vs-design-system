bf2554447950aabb973f4c53277737b4bb995cc9
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-04-21 00:26:22 +0100

    Fixes linting errors in docs component status component

--- frontend/docs/components/status/Components.vue
+++ frontend/docs/components/status/Components.vue
@@ -1,138 +1,183 @@
 <template>
-  <div class="component-status">
-    <ul class="status-list">
-      <li>
-        <vs-icon name="docs/ready" variant="success" size="xs" />
-        <p class="mb-0">Ready</p>
-      </li>
-      <li>
-        <vs-icon name="docs/review" variant="warning" size="xs" />
-        <p class="mb-0">Under review</p>
-      </li>
-      <li>
-        <vs-icon name="docs/deprecated" variant="danger" size="xs" />
-        <p class="mb-0">Deprecated</p>
-      </li>
-      <li>
-        <vs-icon name="docs/prototype" variant="info" size="xs" />
-        <p class="mb-0">Prototype</p>
-      </li>
-      <li>
-        <span>—</span>
-        <p class="mb-0">Not applicable</p>
-      </li>
-    </ul>
-    <table>
-      <thead>
-        <tr>
-          <th v-if="show === 'all'">Component Name</th>
-          <th v-if="show === 'elements'">Element Name</th>
-          <th v-if="show === 'patterns'">Pattern Name</th>
-          <th v-if="show === 'templates'">Template Name</th>
-          <th>Released in</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(component, index) in components" :key="index" class="component">
-          <td v-if="component.name">
-            <code class="name">{{ component.name }}</code>
-          </td>
-          <td v-else>N/A</td>
-          <td v-if="component.release">{{ component.release }}</td>
-          <td v-else>N/A</td>
-          <td v-if="component.status">
-            <vs-icon
-              v-if="component.status === 'docs/ready'"
-              name="docs/ready"
-              variant="success"
-              size="xs"
-            />
-            <vs-icon
-              v-if="component.status === 'under-review' || component.status === 'review'"
-              name="docs/review"
-              variant="warning"
-              size="xs"
-            />
-            <vs-icon
-              v-if="component.status === 'prototype'"
-              name="docs/prototype"
-              variant="info"
-              size="xs"
-            />
-            <vs-icon
-              v-if="component.status === 'deprecated'"
-              name="docs/deprecated"
-              variant="danger"
-              size="xs"
-            />
-          </td>
-          <td v-else>—</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="component-status">
+        <ul class="status-list">
+            <li>
+                <VsIcon
+                    name="docs/ready"
+                    variant="success"
+                    size="xs"
+                />
+                <p class="mb-0">
+                    Ready
+                </p>
+            </li>
+            <li>
+                <VsIcon
+                    name="docs/review"
+                    variant="warning"
+                    size="xs"
+                />
+                <p class="mb-0">
+                    Under review
+                </p>
+            </li>
+            <li>
+                <VsIcon
+                    name="docs/deprecated"
+                    variant="danger"
+                    size="xs"
+                />
+                <p class="mb-0">
+                    Deprecated
+                </p>
+            </li>
+            <li>
+                <VsIcon
+                    name="docs/prototype"
+                    variant="info"
+                    size="xs"
+                />
+                <p class="mb-0">
+                    Prototype
+                </p>
+            </li>
+            <li>
+                <span>—</span>
+                <p class="mb-0">
+                    Not applicable
+                </p>
+            </li>
+        </ul>
+        <table>
+            <thead>
+                <tr>
+                    <th v-if="show === 'all'">
+                        Component Name
+                    </th>
+                    <th v-if="show === 'elements'">
+                        Element Name
+                    </th>
+                    <th v-if="show === 'patterns'">
+                        Pattern Name
+                    </th>
+                    <th v-if="show === 'templates'">
+                        Template Name
+                    </th>
+                    <th>Released in</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="(component, index) in components"
+                    :key="index"
+                    class="component"
+                >
+                    <td v-if="component.name">
+                        <code class="name">{{ component.name }}</code>
+                    </td>
+                    <td v-else>
+                        N/A
+                    </td>
+                    <td v-if="component.release">
+                        {{ component.release }}
+                    </td>
+                    <td v-else>
+                        N/A
+                    </td>
+                    <td v-if="component.status">
+                        <VsIcon
+                            v-if="component.status === 'docs/ready'"
+                            name="docs/ready"
+                            variant="success"
+                            size="xs"
+                        />
+                        <VsIcon
+                            v-if="component.status === 'under-review' ||
+                                component.status === 'review'"
+                            name="docs/review"
+                            variant="warning"
+                            size="xs"
+                        />
+                        <VsIcon
+                            v-if="component.status === 'prototype'"
+                            name="docs/prototype"
+                            variant="info"
+                            size="xs"
+                        />
+                        <VsIcon
+                            v-if="component.status === 'deprecated'"
+                            name="docs/deprecated"
+                            variant="danger"
+                            size="xs"
+                        />
+                    </td>
+                    <td v-else>
+                        —
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 // If you want to use your own tokens here, change the following line to:
 // import designTokens from "@/assets/tokens/tokens.raw.json"
-import designTokens from "../../styles/docs.tokens.json"
 import orderBy from "lodash/orderBy"
 import get from "lodash/get"
+import designTokens from "../../styles/docs.tokens.json"
 
 export default {
-  name: "Components",
-  props: {
-    show: {
-      type: String,
-      default: "all",
-      validator: value => {
-        return value.match(/(all|patterns|templates|elements|modules)/)
-      },
+    name: "Components",
+    props: {
+        show: {
+            type: String,
+            default: "all",
+            validator: (value) => value.match(/(all|patterns|templates|elements|modules)/),
+        },
     },
-  },
-  methods: {
-    getComponents: function() {
-      let contexts
+    data() {
+        return {
+            components: this.orderData(this.getComponents()),
+            tokens: designTokens.props,
+        }
+    },
+    methods: {
+        getComponents() {
+            let contexts
 
-      if ((this.show === "all") | "modules") {
-        contexts = [
-          require.context("@components/elements/", true, /\.vue$/),
-          require.context("@components/patterns/", true, /\.vue$/),
-          // require.context("@components/modules/", true, /\.vue$/),
-          require.context("@components/templates/", true, /\.vue$/),
-        ]
-      } else if (this.show === "elements") {
-        contexts = [require.context("@components/elements/", true, /\.vue$/)]
-      } else if (this.show === "patterns") {
-        contexts = [require.context("@components/patterns/", true, /\.vue$/)]
-        // } else if (this.show === "modules") {
-        //   contexts = [require.context("@components/modules/", true, /\.vue$/)]
-      } else if (this.show === "templates") {
-        contexts = [require.context("@components/templates/", true, /\.vue$/)]
-      }
+            if ((this.show === "all") || this.show === "modules") {
+                contexts = [
+                    require.context("@components/elements/", true, /\.vue$/),
+                    require.context("@components/patterns/", true, /\.vue$/),
+                    // require.context("@components/modules/", true, /\.vue$/),
+                    require.context("@components/templates/", true, /\.vue$/),
+                ]
+            } else if (this.show === "elements") {
+                contexts = [require.context("@components/elements/", true, /\.vue$/)]
+            } else if (this.show === "patterns") {
+                contexts = [require.context("@components/patterns/", true, /\.vue$/)]
+                // } else if (this.show === "modules") {
+                //   contexts = [require.context("@components/modules/", true, /\.vue$/)]
+            } else if (this.show === "templates") {
+                contexts = [require.context("@components/templates/", true, /\.vue$/)]
+            }
 
-      const components = []
-      contexts.forEach(context => {
-        context.keys().forEach(key => components.push(context(key).default))
-      })
+            const components = []
+            contexts.forEach((context) => {
+                context.keys().forEach((key) => components.push(context(key).default))
+            })
 
-      return components
+            return components
+        },
+        orderData(data) {
+            return orderBy(data, "name", "asc")
+        },
+        getColour(colourName) {
+            return get(this.tokens, `docs_color_${colourName}.value`, "#aaa")
+        },
     },
-    orderData: function(data) {
-      return orderBy(data, "name", "asc")
-    },
-    getColour(colourName) {
-      return get(this.tokens, "docs_color_" + colourName + ".value", "#aaa")
-    },
-  },
-  data() {
-    return {
-      components: this.orderData(this.getComponents()),
-      tokens: designTokens.props,
-    }
-  },
 }
 </script>
 
