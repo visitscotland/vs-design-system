ae27b381045562d480e14512a96f1351c9d03b85
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-05-04 01:08:08 +0100

    Integrates Vue SSR and puppeteer POCs into frontend and Hippo

---
+++ frontend/ssr/server/proxy.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const path = require('path');
+
+if (!process.env.VS_SSR_PROXY_TARGET_HOST) {
+    require("dotenv").config()
+}
+
+const makeUrl = (subPath) => {
+    if(!process.env.VS_SSR_PROXY_TARGET_HOST) {
+        throw 'Target host URL missing'
+    }
+
+    return path.join(process.env.VS_SSR_PROXY_TARGET_HOST, subPath)
+}
+
+const getPage = async (path) => {
+    return new Promise((resolve, reject) => {
+        const url = makeUrl(path);
+
+        console.log('Proxying request to ' + url);
+
+        http.get(url, (resp) => {
+            let data = '';
+          
+            resp.on('data', (chunk) => {
+              data += chunk;
+            });
+          
+            resp.on('end', () => {
+              resolve(data);
+            });
+          
+        }).on("error", (err) => {
+            reject(err);
+        });
+    })
+}
+
+module.exports = {
+    getPage
+}
