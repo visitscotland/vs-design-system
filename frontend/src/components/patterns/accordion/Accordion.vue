a95f8d765ae012da0f7f1b5f22e9d6b01afb71b9
gstephen <gemma.stephen@visitscotland.com>
2020-05-05 12:39:52 +0100

    Updates to accordion component

--- frontend/src/components/patterns/accordion/Accordion.vue
+++ frontend/src/components/patterns/accordion/Accordion.vue
@@ -26,15 +26,66 @@ export default {
 <docs>
   ```js
     <vs-accordion>
-        <vs-accordion-item :visible=true variant="dark">
+        <vs-accordion-item :visible="true" variant="dark" index="1">
             <span slot="title">
                 This is a title
             </span>
 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
-            enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
-            maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
-            turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            <span slot="icon-open">
+                <VsIcon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <VsIcon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="py-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
+        </vs-accordion-item>
+
+        <vs-accordion-item :visible="false" variant="dark" index="2">
+            <span slot="title">
+                This is a title
+            </span>
+
+            <span slot="icon-open">
+                <VsIcon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <VsIcon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="py-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
+        </vs-accordion-item>
+        <vs-accordion-item :visible="false" variant="dark" index="3">
+            <span slot="title">
+                This is a title
+            </span>
+
+            <span slot="icon-open">
+                <VsIcon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <VsIcon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="py-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
         </vs-accordion-item>
     </vs-accordion>
 
