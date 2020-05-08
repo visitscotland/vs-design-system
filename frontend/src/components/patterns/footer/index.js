e59211d2763652b12d7b82efa5350263c49a11d6
gstephen <gemma.stephen@visitscotland.com>
2020-05-08 18:45:17 +0100

    Added FooterNavListItem component

--- frontend/src/components/patterns/footer/index.js
+++ frontend/src/components/patterns/footer/index.js
@@ -1,7 +1,9 @@
 import VsFooter from "./Footer"
 import VsFooterNavList from "./components/FooterNavList"
+import VsFooterNavListItem from "./components/FooterNavListItem"
 
 export default {
     VsFooterNavList,
     VsFooter,
+    VsFooterNavListItem,
 }
