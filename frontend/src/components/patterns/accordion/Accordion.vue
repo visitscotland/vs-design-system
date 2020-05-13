cd2269346ba379d04d98d9314b286902bea214ee
gstephen <gemma.stephen@visitscotland.com>
2020-05-13 15:21:58 +0100

    Responsive and breakPoint now set on accordion rather than the item and tidied up components

--- frontend/src/components/patterns/accordion/Accordion.vue
+++ frontend/src/components/patterns/accordion/Accordion.vue
@@ -18,6 +18,33 @@ export default {
     name: "VsAccordion",
     components: {
     },
+    props: {
+        /**
+         * If this is provided, the accordion will change to an open
+         * menu with a title instead of button.
+         */
+        responsive: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * If this is provided and responsive is set, this prop
+         * decides when the accordion expands.
+         */
+        breakPoint: {
+            type: String,
+            default: "",
+        },
+    },
+    /**
+     * Provides responsive prop to be injected to child component AccordionItem
+     */
+    provide() {
+        return {
+            responsive: this.responsive,
+            breakPoint: this.breakPoint,
+        }
+    },
 }
 </script>
 
@@ -26,6 +53,8 @@ export default {
 
 <docs>
   ```js
+
+    <h3>Basic Accordion</h3>
     <vs-accordion>
         <vs-accordion-item :visible="true" variant="dark" index="1">
             <span slot="title">
@@ -69,6 +98,71 @@ export default {
             </div>
         </vs-accordion-item>
         <vs-accordion-item :visible="false" variant="dark" index="3">
+            <span slot="title">
+                This is a title
+            </span>
+
+            <span slot="icon-open">
+                <vs-icon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <vs-icon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="p-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
+        </vs-accordion-item>
+    </vs-accordion>
+
+    <h3  class="mt-6">Responsive Accordion</h3>
+    <vs-accordion :responsive="true" break-point="sm">
+        <vs-accordion-item :visible="true" variant="dark" index="4">
+            <span slot="title">
+                This is a title
+            </span>
+
+            <span slot="icon-open">
+                <vs-icon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <vs-icon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="p-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
+        </vs-accordion-item>
+
+        <vs-accordion-item :visible="false" variant="dark" index="5">
+            <span slot="title">
+                This is a title
+            </span>
+
+            <span slot="icon-open">
+                <vs-icon name="chevron-down" variant="light" size="xs" />
+            </span>
+
+            <span slot="icon-closed">
+                <vs-icon name="chevron-up" variant="light" size="xs" />
+            </span>
+
+            <div class="p-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus quam non
+                enim commodo consectetur. Curabitur accumsan non mauris et laoreet. Praesent
+                maximus sagittis mauris a finibus. Morbi fringilla, lorem ut fringilla sollicitudin,
+                turpis enim venenatis ipsum, vitae finibus sem tellus sit amet mauris.
+            </div>
+        </vs-accordion-item>
+        <vs-accordion-item :visible="false" variant="dark" index="6">
             <span slot="title">
                 This is a title
             </span>
