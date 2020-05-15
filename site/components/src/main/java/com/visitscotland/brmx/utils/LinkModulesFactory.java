210995934ba195abbacd1713e745507ed9d97765
Jose Calcines <jose.calcines@visitscotland.com>
2020-05-15 18:14:41 +0100

    FEAT (megalinks) Bare bones of the megalinks Factory

---
+++ site/components/src/main/java/com/visitscotland/brmx/utils/LinkModulesFactory.java
@@ -0,0 +1,78 @@
+package com.visitscotland.brmx.utils;
+
+import com.visitscotland.brmx.beans.BaseDocument;
+import com.visitscotland.brmx.beans.MegaLinkItem;
+import com.visitscotland.brmx.beans.MegaLinks;
+import com.visitscotland.brmx.beans.Page;
+import com.visitscotland.brmx.beans.mapping.FlatImage;
+import com.visitscotland.brmx.beans.mapping.FlatLink;
+import com.visitscotland.brmx.beans.mapping.megalinks.AbstractLayout;
+import com.visitscotland.brmx.beans.mapping.megalinks.ListLayout;
+import com.visitscotland.brmx.beans.mapping.megalinks.SingleImageLayout;
+import com.visitscotland.brmx.beans.mapping.megalinks.StandardLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class LinkModulesFactory {
+
+    private final static int MAX_ITEMS = 6;
+
+    public AbstractLayout getModule(MegaLinks doc, Locale locale){
+        if (doc.getSingleImageModule() != null){
+            return singleImageLayout(doc, locale);
+        } else if (doc.getList() || doc.getSingleImageLinks().size() > MAX_ITEMS){
+            return list(doc, locale);
+        } else {
+            return standard(doc, locale);
+        }
+    }
+
+    public SingleImageLayout singleImageLayout(MegaLinks doc, Locale locale){
+        SingleImageLayout sil = new SingleImageLayout ();
+        sil.setTitle(doc.getTitle());
+        sil.setIntroduction(doc.getIntroduction());
+//        sil.setCta(doc.getCta());
+        sil.setImage(new FlatImage(doc.getSingleImageModule().getImage(), locale));
+        sil.setFullWidth(doc.getSingleImageModule().getFullWidth());
+        sil.setLinks(convertoToFlatLinks(doc.getMegaLinkItems()));
+
+        return sil;
+    }
+
+    public StandardLayout standard(MegaLinks doc, Locale locale){
+        //TODO
+        return new StandardLayout();
+    }
+
+    public ListLayout list(MegaLinks doc, Locale locale){
+        //TODO
+        return new ListLayout();
+    }
+
+    private List<FlatLink> convertoToFlatLinks(List<MegaLinkItem> items){
+        List<FlatLink> links = new ArrayList<>();
+        for (MegaLinkItem item : items){
+            if (item.getLink() == null) {
+                //TODO "The module %s contains a link without any reference"
+            } else if (item.getLink() instanceof Page){
+                //TODO HippoUtils.getUrl(HippoLink)
+                links.add(new FlatLink(((Page) item.getLink()).getTitle(), "TODO"));
+            } else {
+                //TODO "The module %s is pointing to a document of type %s which cannot be rendered as a page " item.getLink().getClass().getSimpleName()
+            }
+        }
+        return links;
+    }
+
+    private <L> List<L> convertToEnhancedLinks(){
+        //TODO
+        return null;
+    }
+
+
+
+
+
+}
