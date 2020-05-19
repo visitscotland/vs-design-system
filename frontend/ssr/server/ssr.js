66678f25b2b044adf90bc43ccf807330c81d3ca2
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-05-19 23:58:09 +0100

    Replaces static asset imports with dynamic ones (server-side)
    - Adds webpack loader that inserts component imports and registrations to app.js server-side
    - Adds transform-html to proxy service to prepend asset URLs with the target host in the HTML output by the target URL
    - Fixes some linting errors and tidies up

--- frontend/ssr/server/ssr.js
+++ frontend/ssr/server/ssr.js
@@ -1,101 +1,97 @@
-// const fs = require('fs');
-// const path = require('path');
+const vueServerRenderer = require("vue-server-renderer")
+const vueTemplateCompiler = require("vue-template-compiler")
 
-const vueServerRenderer = require('vue-server-renderer');
-const vueTemplateCompiler = require('vue-template-compiler');
+const cheerio = require("cheerio")
+const { html: beautifyHtml } = require("js-beautify")
 
-const cheerio = require('cheerio');
-const { html: beautifyHtml } = require('js-beautify')
+const setupDevServer = require("./setup-dev-server")
 
-const setupDevServer = require('./setup-dev-server');
+const serverBundle = require("../../dist/ssr/server/vue-ssr-server-bundle.json")
+const clientManifest = require("../../dist/ssr/client/vue-ssr-client-manifest.json")
 
-const serverBundle = require('../../dist/ssr/server/vue-ssr-server-bundle.json');
-const clientManifest = require('../../dist/ssr/client/vue-ssr-client-manifest.json');
+const transformHtml = require("./transform-html")
 
-// const testHTMLEntry = fs.readFileSync(path.resolve(__dirname, '../src/dummy-ssr-template.html'), 'utf-8');
+// const componentMap = require("../../build/entry.system-components.js")
 
-const appAttributeName = 'data-vue-app-init';
-const templatePlaceholderAttrName = 'vue-ssr-outlet';
+// const testHTMLEntry = fs.readFileSync(
+//    path.resolve(__dirname, '../src/dummy-ssr-template.html'), 'utf-8');
+
+const appAttributeName = "data-vue-app-init"
+const templatePlaceholderAttrName = "vue-ssr-outlet"
 const templatePlaceholderHtml = `<span ${templatePlaceholderAttrName}></span>`
-const xTemplateId = 'app-template';
+const xTemplateId = "app-template"
 
-let $template;
-
-const completeSsrTemplate = (appHtml, context) => {
-    const headAssetInject = `
-        ${context.renderResourceHints() }
-        ${context.renderStyles() }
-    `;
-    const bodyAssetInject = `
-        ${context.renderScripts()}
-    `;
-
-    $template('head').append(headAssetInject);
-    $template('body').append(bodyAssetInject);
-
-    $template(`[${templatePlaceholderAttrName}]`).replaceWith(appHtml);
-
-    return $template.html();
-    // return formatHtml($template.html());
-}
-
-const createRenderer = (bundle) => {
-    return vueServerRenderer.createBundleRenderer(bundle, {
-        runInNewContext: true,
-        template: completeSsrTemplate,
-        inject: true,
-        clientManifest
-    });
-}
-
-const initRenderer = (app) => {
-    if (process.env.NODE_ENV === 'development') {
-        setupDevServer(app, (serverBundle) => {
-            renderer = createRenderer(serverBundle);
-        });
-    } else {
-        renderer = createRenderer(serverBundle);
-    }
-}
-
-const renderPage = async (pageHtml) => {
-    const { $appNode, $page } = parsePageParts(pageHtml);
-
-    // HTML is formatted before rendering to ensure successful hydration
-    const formattedAppHtml = formatHtml(cheerio.html($appNode))
-
-    prepSsrTemplate($page, formattedAppHtml)
-
-    return await renderApp(formattedAppHtml);
-}
+// const sourceImports = require("../src/generate-component-library-map")(componentMap)
+// console.log(sourceImports)
+let $template
+let renderer
 
 const formatHtml = (subjectHtml) => {
-    return beautifyHtml(subjectHtml, { indent_with_tabs: true })
+    return beautifyHtml(subjectHtml, {
+        indent_with_tabs: true,
+        preserve_newlines: false,
+    })
 }
 
-const prepSsrTemplate = ($page, formattedAppHtml) => {
-    const xTemplateHtml = makeXTemplate(formattedAppHtml);
+const completeSsrTemplate = (appHtml/* , context */) => {
+    // const headAssetInject = `
+    //     ${context.renderResourceHints() }
+    //     ${context.renderStyles() }
+    // `;
+    // const bodyAssetInject = `
+    //     ${context.renderScripts()}
+    // `;
 
-    $page('body').append(xTemplateHtml);
+    // $template('head').append(headAssetInject);
+    // $template('body').append(bodyAssetInject);
 
-    $template = $page;
+    $template(`[${templatePlaceholderAttrName}]`).replaceWith(appHtml)
+
+    const pageHtml = transformHtml($template.html())
+
+    // return pageHtml;
+
+    return formatHtml(pageHtml)
+}
+
+const createRenderer = (bundle) => vueServerRenderer.createBundleRenderer(bundle, {
+    runInNewContext: true,
+    template: completeSsrTemplate,
+    inject: true,
+    clientManifest,
+});
+
+const initRenderer = (app) => {
+    if (process.env.NODE_ENV === "development") {
+        setupDevServer(app, (bundle) => {
+            renderer = createRenderer(bundle)
+        })
+    } else {
+        renderer = createRenderer(serverBundle)
+    }
 }
 
 // inserts pre-rendered app template into an x-template
 const makeXTemplate = (template) => {
     // the data-vue-app-init must be removed otherwise the app will be instantiated twice
-     return `
-        <script type="text/x-template" id="${xTemplateId}">
-            ${template.replace('data-vue-app-init', '')}
-        </script>
-    `;
+    return `
+        <script type="text/x-template" id="${xTemplateId}">${template.replace(" data-vue-app-init", "")}</script>
+    `
+}
+
+const prepSsrTemplate = ($page, formattedAppHtml) => {
+    const xTemplateHtml = makeXTemplate(formattedAppHtml)
+
+    $page(`[${templatePlaceholderAttrName}]`).after(xTemplateHtml)
+
+    $template = $page
 }
 
 // Render the HTML page using Vue SSR on the provided app template
 const renderApp = async (template) => {
 
     const compileOptions = {
-        outputSourceRange: true
+        outputSourceRange: true,
     }
 
     const compiled = vueTemplateCompiler.compileToFunctions(template, compileOptions)
@@ -107,7 +103,8 @@ const renderApp = async (template) => {
         },
         template,
     }
-    return renderer.renderToString(context);
+
+    return renderer.renderToString(context)
 }
 
 const parsePageParts = (pageHtml) => {
@@ -115,19 +112,30 @@ const parsePageParts = (pageHtml) => {
         script: true,
         style: true,
         pre: true,
-        comment: true
-    });
+        comment: true,
+    })
 
-    const $appNode = $page(`[${appAttributeName}]`).replaceWith(templatePlaceholderHtml);
+    const $appNode = $page(`[${appAttributeName}]`).replaceWith(templatePlaceholderHtml)
 
-    if(!$appNode) {
-        throw 'App element missing';
+    if (!$appNode) {
+        throw "App element missing"
     }
 
     return {
         $page,
         $appNode,
-    };
+    }
+}
+
+const renderPage = async (pageHtml) => {
+    const { $appNode, $page } = parsePageParts(pageHtml)
+
+    // HTML is formatted before rendering to ensure successful hydration
+    const formattedAppHtml = formatHtml(cheerio.html($appNode))
+
+    prepSsrTemplate($page, formattedAppHtml)
+
+    return renderApp(formattedAppHtml)
 }
 
 module.exports = {
--- frontend/ssr/src/app.js
+++ frontend/ssr/src/app.js
@@ -1,99 +1,23 @@
 
-import Vue from 'vue';
+import Vue from "vue"
+import { get } from "lodash"
 
-import VsImageWithCaption from '@components/patterns/image-with-caption/ImageWithCaption'
-import VsImg from '@components/elements/img/Img'
-import VsSvg from '@components/elements/svg/Svg'
-import VsLink from '@components/elements/link/Link'
-import VsButtonWithIcon from '@components/elements/button/ButtonWithIcon'
-import {
-    VsCol,
-    VsRow,
-    VsContainer,
-} from '@components/elements/layout'
-import VsHeading from '@components/elements/heading/Heading'
-import VsIcon from '@components/elements/icon/Icon'
-import {
-    VsDescriptionList,
-    VsDescriptionListTerm,
-    VsDescriptionListDetail,
-} from '@components/elements/description-list'
-import VsRichTextWrapper from '@components/elements/rich-text-wrapper/RichTextWrapper'
-import VsTooltip from '@components/elements/tooltip/Tooltip'
-import VsBreadcrumb from '@components/elements/breadcrumb/Breadcrumb'
-import VsBreadcrumbItem from '@components/elements/breadcrumb/BreadcrumbItem'
-import VsSocialShare from '@components/patterns/social-share/SocialShare'
-import VsListicleItem from '@components/patterns/listicle/ListicleItem'
-import VsIconList from '@components/patterns/icon-list/IconList'
-import VsIconListItem from '@components/patterns/icon-list/IconListItem'
-import VsSocialCreditLink from '@components/patterns/social-credit-link/SocialCreditLink'
-import VsPageIntro from '@components/patterns/page-intro/PageIntro'
-import VsHero from '@components/patterns/hero/Hero'
-import { 
-    VsSummaryBoxList,
-    VsSummaryBoxListItem,
-    VsSummaryBoxLabel,
-    VsSummaryBoxDisplay,
-    VsSummaryBoxIconWithLabel,
-    VsSummaryBoxDistanceDisplay,
-    VsSummaryBoxDistanceLabel,
-} from '@components/patterns/summary-box'
-import { 
-    VsItinerary,
-    // VsItineraryMap,
-    VsItineraryDay,
-    VsItineraryStop,
-    VsItineraryTips,
-    VsItineraryBorderOverlapWrapper,
-} from '@components/patterns/itineraries'
+/** PLACEHOLDER: IMPORTS */
 
-export const createApp = (context) =>  {
-    const defaultOptions = {
+export default async (context) => {
+    const defaultVueOptions = {
         components: {
-            VsImageWithCaption,
-            VsImg,
-            VsSvg,
-            VsLink,
-            VsCol,
-            VsRow,
-            VsContainer,
-            VsHeading,
-            VsRichTextWrapper,
-            VsBreadcrumb,
-            VsBreadcrumbItem,
-            VsSocialShare,
-            VsListicleItem,
-            VsIconList,
-            VsIconListItem,
-            VsSocialCreditLink,
-            VsButtonWithIcon,
-            VsIcon,
-            VsDescriptionList,
-            VsDescriptionListTerm,
-            VsDescriptionListDetail,
-            VsTooltip,
-            VsPageIntro,
-            VsHero,
-            VsSummaryBoxList,
-            VsSummaryBoxListItem,
-            VsSummaryBoxLabel,
-            VsSummaryBoxDisplay,
-            VsSummaryBoxIconWithLabel,
-            VsSummaryBoxDistanceDisplay,
-            VsSummaryBoxDistanceLabel,
-            VsItinerary,
-            // VsItineraryMap,
-            VsItineraryDay,
-            VsItineraryStop,
-            VsItineraryTips,
-            VsItineraryBorderOverlapWrapper,
+            /** PLACEHOLDER: COMPONENT REGISTRATION */
         },
         comments: true,
     }
 
-    const vueOptions = Object.assign({}, defaultOptions, context.vueOptions)
+    const vueOptions = {
+        ...defaultVueOptions,
+        ...get(context, "vueOptions", {}),
+    }
 
     const app = new Vue(vueOptions)
 
     return app
-};
+}
