20738311f87ee884a43bc725dc4119bb02b172b6
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-05-20 22:23:57 +0100

    Modifies UI integration for SSR setup
    - Switches ui-integration build to use SSR client build
    - Syntax fixes
    - Uncomments freemarker asset refs
    - Re-enables ui-integration module in main POM
    - Adds autoreload host URL fix to transform-html
    - Adds express to frontend dependencies (it was not included)
    - Adds minor formatting fixes and removes commented out code

--- frontend/ssr/server/transform-html.js
+++ frontend/ssr/server/transform-html.js
@@ -1,16 +1,25 @@
+const { flow } = require("lodash");
 
 if (!process.env.VS_SSR_PROXY_TARGET_HOST) {
-    require("dotenv").config()
+    require("dotenv").config();
 }
 
-// This prepends the target host address to all script and link href and src values,
+const targetHostWithoutProtocol = process.env.VS_SSR_PROXY_TARGET_HOST.replace(/[a-z]+:\/\//, "");
+
+// This function prepends the target host address to all script and link href and src values,
 // respectively, that begin with "/" - workaround instead of outputting absolute
 // URLs from the target site
-const correctLocalAssetUrls = (inputHtml) => {
+const correctLocalAssetUrls = (inputHtml) => inputHtml.replace(
+    /(<(?:script|link)\s+(?:[^>]*?\s+)?(?:href|src)=(?:["']))([\.]*\/)/g,
+    `$1${process.env.VS_SSR_PROXY_TARGET_HOST}/`,
+);
 
-    const search = /(<(?:script|link)\s+(?:[^>]*?\s+)?(?:href|src)=(?:["']))([\.]*\/)/g;
+const correctHippAutoreloadUrl = (inputHtml) => inputHtml.replace(
+    /("ws:\/\/)" \+ document\.location\.host/g,
+    `$1${targetHostWithoutProtocol}"`,
+);
 
-    return inputHtml.replace(search, `$1${process.env.VS_SSR_PROXY_TARGET_HOST}/`);
-}
-
-module.exports = correctLocalAssetUrls;
+module.exports = flow(
+    correctLocalAssetUrls,
+    correctHippAutoreloadUrl,
+);
--- ui-integration/build/templates/vue-app-init.ftl
+++ ui-integration/build/templates/vue-app-init.ftl
@@ -6,23 +6,6 @@
     <script src="https://unpkg.com/vuex@3.1.1/dist/vuex.js"></script>
 </@hst.headContribution>
 
-<@hst.headContribution category="htmlBodyEnd">
-    <script type="text/javascript">
-        let vs = {
-            stores: {},
-            app: null
-        }
-    </script>
-</@hst.headContribution>
-
 <@hst.headContribution category="htmlAppInit">
-    <script type="text/javascript">
-        vs.app = new Vue({
-            el: '[data-vue-app-init]',
-            data:function() {
-                return { test: {} }
-            },
-            comments: true
-        })
-    </script>
+	<script type="text/javascript" src="<@hst.webfile  path='design-system/components/main-client.js'/>"></script>
 </@hst.headContribution>
