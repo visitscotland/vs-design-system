79c443127af5cf9577cf86a843fe797341c6f35f
gstephen <gemma.stephen@visitscotland.com>
2020-06-08 16:24:32 +0100

    Added footerCopyright component

--- frontend/src/components/patterns/footer/index.js
+++ frontend/src/components/patterns/footer/index.js
@@ -1,5 +1,6 @@
 import VsFooter from "./Footer"
 import VsFooterAccordionItem from "./components/FooterAccordionItem"
+import VsFooterCopyright from "./components/FooterCopyright"
 import VsFooterSocialMenu from "./components/FooterSocialMenu"
 import VsFooterNavList from "./components/FooterNavList"
 import VsFooterNavListItem from "./components/FooterNavListItem"
@@ -9,6 +10,7 @@ import VsFooterUtilityList from "./components/FooterUtilityList"
 export default {
     VsFooter,
     VsFooterAccordionItem,
+    VsFooterCopyright,
     VsFooterSocialMenu,
     VsFooterNavList,
     VsFooterNavListItem,
