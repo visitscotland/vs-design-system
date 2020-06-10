9be486cb2217d51e72a7857de85f9df1863c2072
Luke Lewis <luke.lewis@vsweb.visitscotland.net>
2020-06-10 22:36:10 +0100

    FEAT (ui integration) Updates UI integration with frontend build changes
    - Updates UI integration package builds according to modified frontend builds
    - Fixes dynamic asset output value in UI integration freemarker generation script
    - Simplifies UI Integration freemarker generation script
    - Moves freemarker and asset output paths in UI integration build
    - Removes redundant jquery and bootstrap assets from Hippo

--- ui-integration/webpack.config.js
+++ ui-integration/webpack.config.js
@@ -4,8 +4,8 @@ const path = require("path")
 
 const DEFAULT_SOURCE_PATH = "../frontend/dist/ssr/client"
 const DEFAULT_OUTPUT_PATH_ROOT = "../repository-data/webfiles/src/main/resources/site/"
-const ASSET_OUTPUT_PATH = "design-system"
-const TEMPLATE_OUTPUT_PATH = "freemarker/vs-dotcom-ds"
+const ASSET_OUTPUT_PATH = "frontend"
+const TEMPLATE_OUTPUT_PATH = "freemarker/frontend"
 const FTL_IMPORTS_FILE_PATH = "../../include/imports.ftl"
 const VUE_TEMPLATE_PATH = "./build/templates/vue-app-init.ftl"
 
@@ -51,6 +51,7 @@ module.exports = function(env, argv) {
             {
               loader: path.resolve("./build/generateFreemarkerTemplate.js"),
               options: {
+                webfilesPath: ASSET_OUTPUT_PATH,
                 targetPath: templateOutputPath,
                 importsPath: FTL_IMPORTS_FILE_PATH
               },
