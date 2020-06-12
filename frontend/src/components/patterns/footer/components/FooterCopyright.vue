f69cc688a46f94b841e84a14244077559789c206
gstephen <gemma.stephen@visitscotland.com>
2020-06-12 18:19:03 +0100

    Integrating FooterCopyright component with FTL

--- frontend/src/components/patterns/footer/components/FooterCopyright.vue
+++ frontend/src/components/patterns/footer/components/FooterCopyright.vue
@@ -6,6 +6,14 @@
                     cols="12"
                     class="text-center"
                 >
+                    <a
+                        class="vs-footer-copyright__logo mb-4"
+                        :href="href"
+                    >
+                        <span class="sr-only">{{ logoAltText }}</span>
+                        <VsSvg path="vs-logo" />
+                    </a>
+
                     <p class="vs-footer-copyright__text">
                         &copy; {{ getCurrentYear }}
 
@@ -19,9 +27,11 @@
 </template>
 
 <script>
+import VsSvg from "@components/elements/svg/Svg"
 import {
     VsRow, VsContainer, VsCol,
 } from "@components/elements/layout"
+
 
 /**
  * The VsFooterCopyright holds an image and copyright notices for the footer.
@@ -30,9 +40,26 @@ import {
 export default {
     name: "VsFooterCopyright",
     components: {
+        VsSvg,
         VsRow,
         VsContainer,
         VsCol,
+    },
+    props: {
+        /**
+         * Accessiblity alt text for the logo button
+         */
+        logoAltText: {
+            type: String,
+            required: true,
+        },
+        /**
+         * URL for the logo button
+         */
+        href: {
+            type: String,
+            required: true,
+        },
     },
     data() {
         return {
@@ -51,18 +78,30 @@ export default {
 .vs-footer-copyright {
     padding: $spacer-6 0;
 
-    .vs-footer-copyright__text{
-        color: $color-white;
-        margin: 0;
+    .vs-footer-copyright__logo {
+        display: inline-block;
     }
 
+    .vs-footer-copyright__text {
+        color: $color-white;
+        margin: 0;
+        font-size: $font-size-sm;
+
+        @include media-breakpoint-up(lg) {
+            font-size: $small-font-size;
+        }
+
+        @include media-breakpoint-up(xl) {
+            font-size: $font-size-base;
+        }
+    }
 }
 </style>
 
 <docs>
   ```js
     <vs-footer>
-        <vs-footer-copyright>
+        <vs-footer-copyright logo-alt-text="VisitScotland Home" href="/">
             <span slot="copyright">
                VisitScotland. All rights reserved.
             </span>
