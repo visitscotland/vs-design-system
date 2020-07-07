ef28fae982e338cc9dccc4ca4222ec41a1bbbdc2
Erik Jensen <erik.jensen@visitscotland.com>
2020-07-07 14:10:32 +0100

    VS-1654 FEAT (clone-itinerary) Adding missing scope

--- cms/src/test/java/com/visitscotland/brmx/translation/plugin/UntranslatedLocaleTest.java
+++ cms/src/test/java/com/visitscotland/brmx/translation/plugin/UntranslatedLocaleTest.java
@@ -20,7 +20,7 @@ public class UntranslatedLocaleTest {
     }
 
     @Test
-    void verifyIconExists() throws Exception {
+    public void verifyIconExists() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         ResourceReference.Key key = missingTranslation.getIcon(null, null).getKey();
         // This will return null if the icon file identified by the Key does not exist.
