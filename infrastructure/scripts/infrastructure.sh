cf3c4054015f6563d1af20d4644effb180b3b0dc
Gavin Park <gavin.park@visitscotland.com>
2020-08-03 23:00:02 +0100

    feat:VS-1865 infrastructure.sh, container updates

--- infrastructure/scripts/infrastructure.sh
+++ infrastructure/scripts/infrastructure.sh
@@ -137,11 +137,11 @@ defaultSettings() {
   VS_HOST_IP_ADDRESS=`/usr/sbin/ip ad sh  | egrep "global noprefixroute" | awk '{print $2}' | sed -e "s/\/.*$//"`
   VS_PARENT_JOB_NAME=`echo $JOB_NAME | sed -e "s/\/.*//g"`
   VS_SCRIPTNAME=`basename $0`
-  if [ "$VS_SSR_PROXY_ON" == "TRUE" ]; then
-    VS_PROXY_QS_SSR="\&vs_ssr_proxy_on"
-  else
-    VS_PROXY_QS_SSR="\&vs_ssr_proxy_off"
-  fi
+#  if [ "$VS_SSR_PROXY_ON" == "TRUE" ]; then
+#    VS_PROXY_QS_SSR="&vs_ssr_proxy=on"
+#  else
+#    VS_PROXY_QS_SSR="&vs_ssr_proxy=off"
+#  fi
   # mail settings - build
   if [ -z "$VS_MAIL_NOTIFY_BUILD_TO" ]; then VS_MAIL_NOTIFY_BUILD_TO=$VS_COMMIT_AUTHOR; fi
   VS_MAIL_NOTIFY_BUILD_SENDER="$VS_PARENT_JOB_NAME"
