966c30ef50136d7cdc3d4d0ee8b3d03f821255fe
Jose Calcines <jose.calcines@visitscotland.com>
2020-08-06 13:39:35 +0100

    MERGE with develop

---
+++ frontend/build/library.generate-manifest.js
@@ -0,0 +1,63 @@
+const {
+  extend,
+  reduce,
+  invoke,
+  mapKeys,
+  flatMap,
+  mapValues,
+  filter,
+  get,
+  uniq,
+  partial,
+  ary,
+  endsWith,
+  remove,
+  isEmpty,
+} = require("lodash")
+
+function extractAllEntriesFromFileDescriptor(FileDescriptor) {
+  return reduce(
+    filter(FileDescriptor, "isInitial"),
+    (acc, val) => {
+      return extend({}, acc, extractEntriesFromOutputFile(val))
+    },
+    {}
+  )
+}
+
+function extractEntriesFromOutputFile(outputFile) {
+  let entries = Array.from(invoke(outputFile, "chunk._groups.values"))
+
+  return entries.length ? mapKeys(entries, "options.name") : {}
+}
+
+function mapComponentEntryFiles(component) {
+  const files = uniq(flatMap(get(component, "chunks"), "files"))
+  const scripts = remove(files, ary(partial(endsWith, partial.placeholder, ".js"), 1))
+  const styles = remove(files, ary(partial(endsWith, partial.placeholder, ".css"), 1))
+
+  let assetMap = {}
+
+  if(!isEmpty(scripts)) {
+    assetMap.scripts = scripts
+  }
+
+  if(!isEmpty(styles)) {
+    assetMap.styles = styles
+  }
+
+  if(!isEmpty(files)) {
+    assetMap.other = files
+  }
+
+  return assetMap
+}
+
+/**
+ * Generates manifest file for system component build
+ */
+module.exports = (Object, FileDescriptor) => {
+  let componentEntries = extractAllEntriesFromFileDescriptor(FileDescriptor)
+
+  return mapValues(componentEntries, mapComponentEntryFiles)
+}
