21846e9ec63d262b566eeff1c6b9144fec1bee78
gstephen <gemma.stephen@visitscotland.com>
2020-08-14 17:26:57 +0100

    Reworking header components

---
+++ frontend/src/components/patterns/header-new/components/mega-nav/MegaNavTopMenuItem.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable -->
+<template>
+    <li class="vs-mega-nav__item">
+        <VsButton
+            class="vs-mega-nav__button"
+            variant="transparent"
+        >
+            <slot />
+        </VsButton>
+    </li>
+</template>
+
+<script>
+
+/**
+ *  Mega nav top level menu button
+ */
+
+import VsButton from '@components/elements/button/Button';
+
+export default {
+    name: 'VsMegaNavTopMenuItem',
+    status: 'prototype',
+    release: '0.1.0',
+    components: {
+        VsButton,
+    },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/type";
+
+.vs-mega-nav__button{
+    text-transform: none;
+    position: relative;
+}
+</style>
+
+<docs>
+  ```jsx
+  ```
+</docs>
---
+++ frontend/src/components/patterns/header-new/header.new.example.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable -->
+<template>
+    <VsHeaderNew>
+        <template slot="global-navbar">
+            <div
+                class="navbar bg-primary-purple"
+                style="height: 35px"
+            >
+                <VsContainer>
+                    <VsRow>
+                        <VsCol cols="12">
+                            <span style="color: white">Global Nav</span>
+                        </VsCol>
+                    </VsRow>
+                </VsContainer>
+            </div>
+        </template>
+        <template slot="mega-nav">
+            <VsMegaNav>
+                <template slot="mega-nav-top-menu-items">
+                    <VsMegaNavTopMenuItem>
+                        Testing 1
+                       
+                    </VsMegaNavTopMenuItem>
+
+                    <VsMegaNavTopMenuItem>
+                        Testing 2
+                    </VsMegaNavTopMenuItem>
+                    <VsMegaNavTopMenuItem>
+                        Testing 3
+                    </VsMegaNavTopMenuItem>
+                </template>
+            </VsMegaNav>
+        </template>
+    </VsHeaderNew>
+</template>
