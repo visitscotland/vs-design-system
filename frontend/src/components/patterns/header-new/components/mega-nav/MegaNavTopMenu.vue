56a2b79b784150518db069e94c2c72dc4c4cdea7
gstephen <gemma.stephen@visitscotland.com>
2020-08-14 17:26:57 +0100

    Reworking header components

---
+++ frontend/src/components/patterns/header-new/components/mega-nav/MegaNavTopMenu.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable -->
+<template>
+    <VsList 
+        unstyled
+        class="vs-mega-nav-top-menu"
+    >
+        <slot />
+    </VsList>
+</template>
+
+<script>
+
+/**
+ *  Mega nav top level menu list
+ */
+
+import VsList from '@components/elements/list/List';
+
+export default {
+    name: 'VsMegaNavTopMenu',
+    status: 'prototype',
+    release: '0.1.0',
+    components: {
+        VsList,
+    },
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/type";
+
+.vs-mega-nav-top-menu {
+    width: 100%;
+    display: flex;
+}
+
+</style>
+
+<docs>
+  ```jsx
+
+  ```
+</docs>
