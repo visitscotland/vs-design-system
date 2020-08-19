32608fb9df2bbe3b2efcf488aae9aa183f1c6797
Jose Calcines <jose.calcines@visitscotland.com>
2020-08-19 15:55:09 +0100

    FIX (freemarker) Removed design system files

--- repository-data/webfiles/src/main/resources/site/design-system/components/275af75c6b39f0e1eb2c.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[70],{"7Asr":function(n,a,e){"use strict";var o=e("oqxP");e.d(a,"a",function(){return o.default})}}]);
--- repository-data/webfiles/src/main/resources/site/design-system/components/6e6213361b081c8496fc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_name_=window.webpackJsonp_name_||[]).push([[9],{ewvW:function(n,e,o){var w=o("HYAF");n.exports=function(n){return Object(w(n))}}}]);
