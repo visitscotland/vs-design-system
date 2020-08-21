09ff16134e78706bc27394e0849ef87fff524aae
gstephen <gemma.stephen@visitscotland.com>
2020-08-21 18:06:05 +0100

    Working on mobile dropdown nav

---
+++ frontend/src/components/patterns/header-new/components/mega-nav/mobile-menu/MegaNavMobileListItem.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable -->
+<template>
+    <BNavItem href="#">
+        <slot />
+    </BNavItem>
+</template>
+
+<script>
+import { BNavItem } from 'bootstrap-vue';
+
+export default {
+    name: 'VsMegaNavMobileListItem',
+    status: 'prototype',
+    release: '0.1.0',
+    components: {
+        BNavItem,
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
+
+<docs>
+  ```jsx
+
+  ```
+</docs>
