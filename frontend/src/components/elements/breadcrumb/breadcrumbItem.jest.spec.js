0136ca73f2042f6f394c46b7a545e67a9ae43db8
Marcos Theofilo <marcos.theofilo@visistscotland.com>
2020-09-02 11:44:14 +0100

    VS-1067 TEST (Breadcrumb) Refactoring unit tests

--- frontend/src/components/elements/breadcrumb/breadcrumbItem.jest.spec.js
+++ frontend/src/components/elements/breadcrumb/breadcrumbItem.jest.spec.js
@@ -1,43 +1,62 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
+import { BBreadcrumbItem } from 'bootstrap-vue';
+
 import VsBreadcrumbItem from './BreadcrumbItem';
 
-const factoryShallowMount = (propsData) => shallowMount(VsBreadcrumbItem, {
+const factoryMount = (propsData) => mount(VsBreadcrumbItem, {
     propsData: {
+        text: 'Some Text',
         ...propsData,
     },
 });
 
-describe('VsBreadcrumbItem', () => {
-    it('should render a b-breadcrumb-item', () => {
-        const wrapper = factoryShallowMount();
-        expect(wrapper.is('b-breadcrumb-item-stub')).toBe(true);
-        expect(wrapper.html()).toContain(
-            '<b-breadcrumb-item-stub target="_self" event="click" routertag="a" ariacurrent="page" class="vs-breadcrumb-item"></b-breadcrumb-item-stub>',
-        );
+describe('<VsBreadcrumbItem />', () => {
+    it('should render a <b-breadcrumb-item />', () => {
+        const wrapper = factoryMount();
+
+        const breadcrumbItem = wrapper.find(BBreadcrumbItem);
+        expect(breadcrumbItem.exists()).toBe(true);
     });
 
-    it('should accept an active property', () => {
-        const wrapper = factoryShallowMount({
-            active: true,
-        });
-        expect(wrapper.attributes('active')).toBe('true');
-        wrapper.setProps({
-            active: 'false',
-        });
-        expect(wrapper.attributes('active')).toBe('false');
-    });
+    describe(':props', () => {
+        it(':active - `active` prop should be `false` by default', () => {
+            const wrapper = factoryMount();
 
-    it('should accept a text property', () => {
-        const wrapper = factoryShallowMount({
-            text: 'Things to See and Do',
+            expect(wrapper.classes('active')).toBe(false);
         });
-        expect(wrapper.attributes('text')).toBe('Things to See and Do');
-    });
 
-    it('should accept an href property', () => {
-        const wrapper = factoryShallowMount({
-            href: 'https://www.visitscotland.com/see-do/itineraries/',
+        it(':active - should accept and render an `active` property', () => {
+            const wrapper = factoryMount({
+                active: true,
+            });
+
+            expect(wrapper.classes('active')).toBe(true);
         });
-        expect(wrapper.attributes('href')).toBe('https://www.visitscotland.com/see-do/itineraries/');
+
+        it(':text - should accept and render a `text` property', () => {
+            const wrapper = factoryMount({
+                text: 'Things to See and Do',
+            });
+
+            expect(wrapper.text()).toBe('Things to See and Do');
+        });
+
+        it(':href - should accept and render an `href` property', () => {
+            const wrapper = factoryMount({
+                href: 'https://www.visitscotland.com/see-do/itineraries/',
+            });
+            const link = wrapper.find('a');
+
+            expect(link.attributes('href')).toBe('https://www.visitscotland.com/see-do/itineraries/');
+        });
+
+        it(':aria-current - `aria-current` prop should be `page` by default', () => {
+            const wrapper = factoryMount({
+                active: true,
+            });
+            const span = wrapper.find('span');
+
+            expect(span.attributes('aria-current')).toBe('page');
+        });
     });
 });
