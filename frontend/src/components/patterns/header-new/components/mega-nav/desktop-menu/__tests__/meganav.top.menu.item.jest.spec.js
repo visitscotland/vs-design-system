2e9020954f895ccac16073a3a9e40e66e785b3ea
gstephen <gemma.stephen@visitscotland.com>
2020-09-10 15:18:21 +0100

    Addint __tests__ folder and data attributes

--- frontend/src/components/patterns/header-new/components/mega-nav/desktop-menu/__tests__/meganav.top.menu.item.jest.spec.js
+++ frontend/src/components/patterns/header-new/components/mega-nav/desktop-menu/__tests__/meganav.top.menu.item.jest.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount, mount } from '@vue/test-utils';
-import VsMegaNavTopMenuItem from './MegaNavTopMenuItem';
+import VsMegaNavTopMenuItem from '../MegaNavTopMenuItem';
 
 const factoryMount = (values) => mount(VsMegaNavTopMenuItem, {
     propsData: {
@@ -14,27 +14,27 @@ const factoryMount = (values) => mount(VsMegaNavTopMenuItem, {
 describe('VsMegaNavTopMenuItem', () => {
     it('should render a component with the class `.vs-mega-nav__top-menu__item`', () => {
         const wrapper = shallowMount(VsMegaNavTopMenuItem);
-        expect(wrapper.classes()).toContain('vs-mega-nav__top-menu__item');
+        expect(wrapper.attributes('data-test')).toBe('vs-mega-nav__top-menu__item');
     });
 
     describe(':props', () => {
         it('should not render CTA link by default when no props have been provided', () => {
             const wrapper = factoryMount();
-            expect(wrapper.find('.vs-mega-nav__cta-link').exists()).toBeFalsy();
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').exists()).toBeFalsy();
         });
 
         it('should not render CTA link if only `ctaText` prop has been provided', () => {
             const wrapper = factoryMount({
                 ctaText: 'Explore the Map of Scotland',
             });
-            expect(wrapper.find('.vs-mega-nav__cta-link').exists()).toBeFalsy();
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').exists()).toBeFalsy();
         });
 
         it('should not render CTA link if only `href` prop has been provided', () => {
             const wrapper = factoryMount({
                 href: 'https://www.visitscotland.com/destinations',
             });
-            expect(wrapper.find('.vs-mega-nav__cta-link').exists()).toBeFalsy();
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').exists()).toBeFalsy();
         });
 
         it('should render CTA link if both `ctaText` and `href` props have been provided', () => {
@@ -42,14 +42,30 @@ describe('VsMegaNavTopMenuItem', () => {
                 ctaText: 'Explore the Map of Scotland',
                 href: 'https://www.visitscotland.com/destinations',
             });
-            expect(wrapper.find('.vs-mega-nav__cta-link').exists()).toBeTruthy();
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').exists()).toBeTruthy();
+        });
+
+        it('should display CTA link text `Explore the Map of Scotland` when passed as `ctaText` prop', () => {
+            const wrapper = factoryMount({
+                ctaText: 'Explore the Map of Scotland',
+                href: 'https://www.visitscotland.com/destinations',
+            });
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').text()).toBe('Explore the Map of Scotland');
+        });
+
+        it('should display the URL `https://www.visitscotland.com/destinations` on the cta link when passed as `href` prop', () => {
+            const wrapper = factoryMount({
+                ctaText: 'Explore the Map of Scotland',
+                href: 'https://www.visitscotland.com/destinations',
+            });
+            expect(wrapper.find('[data-test="vs-mega-nav__cta-link"]').attributes().href).toBe('https://www.visitscotland.com/destinations');
         });
     });
 
     describe(':slots', () => {
         it('renders content inserted in a buttonContent slot', () => {
             const wrapper = factoryMount();
-            expect(wrapper.find('.dropdown-toggle').text()).toBe('Dropdown Toggle');
+            expect(wrapper.find('[data-test="vs-mega-nav__dropdown"]').find('.dropdown-toggle').text()).toBe('Dropdown Toggle');
         });
 
         it('renders content inserted in a dropdownContent slot', () => {
