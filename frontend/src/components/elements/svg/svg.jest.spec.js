eb5c28d2def9986988dbdb95f03a68acef6b679d
Deepa Madheswaran <deepa.madheswaran@vsweb.visitscotland.net>
2020-09-29 16:32:19 +0100

    Fix lint errors

--- frontend/src/components/elements/svg/svg.jest.spec.js
+++ frontend/src/components/elements/svg/svg.jest.spec.js
@@ -1,8 +1,8 @@
 import { extend, trim } from 'lodash';
 import { shallowMount } from '@vue/test-utils';
+import svgContext from '@/utils/svg-context';
 import VsSvg from './Svg';
 import mockSvg from '../../../../test/unit/mocks/svgMock';
-import svgContext from '@/utils/svg-context';
 
 let svgPath;
 let wrapper;
