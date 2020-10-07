5bb47df4b83ec59df597fa6afa87ccb8cf7471f8
Deepa Madheswaran <deepa.madheswaran@vsweb.visitscotland.net>
2020-10-07 16:09:32 +0100

    VS-2163 - Added Accordionitem tests

--- frontend/src/components/patterns/accordion/_tests_/accordionitem.jest.spec.js
+++ frontend/src/components/patterns/accordion/_tests_/accordionitem.jest.spec.js
@@ -14,8 +14,6 @@ describe('<VsAccordionItem />', () => {
             controlId: '1234',
         });
 
-        console.log(wrapper.html());
-
         const AccordionItem = wrapper.find('bcard-stub.vs-accordion__item');
         const AccordionItemHeader = wrapper.find('bcardheader-stub.vs-accordion__item__card-header');
         const AccordionItemTitle = wrapper.find('h4.vs-accordion__item__title');
@@ -36,6 +34,37 @@ describe('<VsAccordionItem />', () => {
             const AccordionItemBody = wrapper.find('.vs-accordion__item__panel');
 
             expect(AccordionItemBody.attributes('id')).toBe('1234');
+        });
+
+        it(':itemBreakPoint assigns accordion class name', () => {
+            const wrapper = factoryShallowMount({
+                itemBreakPoint: 'sm',
+                controlId: '1234',
+            });
+
+            const AccordionToggle = wrapper.find('vsaccordiontoggle-stub');
+
+            expect(AccordionToggle.classes()).toContain('d-sm-none');
+        });
+
+        it(':openByDefault - to show accordion open or closed by default', () => {
+            const wrapper = factoryShallowMount({
+                openByDefault: 'false',
+                controlId: '1234',
+            });
+
+            const AccordionItemBody = wrapper.find('vsaccordiontoggle-stub');
+            expect(AccordionItemBody.attributes('visible')).toBe('false');
+        });
+
+        it(':variant assigns variant to VsAccordionToggle', () => {
+            const wrapper = factoryShallowMount({
+                variant: 'secondary',
+                controlId: '1234',
+            });
+
+            const AccordionItemBody = wrapper.find('vsaccordiontoggle-stub');
+            expect(AccordionItemBody.attributes('variant')).toBe('secondary');
         });
     });
 });
