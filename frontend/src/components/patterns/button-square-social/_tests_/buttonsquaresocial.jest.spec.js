df38e518df83f4873b80c6eb48c17ac624ccbcb1
Deepa Madheswaran <deepa.madheswaran@vsweb.visitscotland.net>
2020-10-09 16:27:48 +0100

    VS-2163 - Added tests

--- frontend/src/components/patterns/button-square-social/_tests_/buttonsquaresocial.jest.spec.js
+++ frontend/src/components/patterns/button-square-social/_tests_/buttonsquaresocial.jest.spec.js
@@ -10,10 +10,37 @@ const factoryShallowMount = (propsData) => shallowMount(VsButtonSquareSocial, {
 
 describe('<VsButtonSquareSocial />', () => {
     it('should render ButtonSquareSocial', () => {
-        const wrapper = factoryShallowMount();
+        const wrapper = factoryShallowMount({
+            icon: 'facebook',
+            href: '#facebook.com',
+        });
 
         const ButtonSquareSocial = wrapper.find('vslink-stub.button-square-social');
 
         expect(ButtonSquareSocial.exists()).toBe(true);
+    });
+
+    describe(':props', () => {
+        it(':icon - Social icon to show', () => {
+            const wrapper = factoryShallowMount({
+                icon: 'facebook',
+                href: '#facebook.com',
+            });
+
+            const vsLink = wrapper.find('vsicon-stub');
+
+            expect(vsLink.attributes('name')).toBe('facebook');
+        });
+
+        it(':href - Social the external URL the button will point to', () => {
+            const wrapper = factoryShallowMount({
+                icon: 'facebook',
+                href: '#facebook.com',
+            });
+
+            const vsLink = wrapper.find('vslink-stub.button-square-social');
+
+            expect(vsLink.attributes('href')).toBe('#facebook.com');
+        });
     });
 });
