7d6e05c53fb6594c45bd95755b8de44312eea353
jhaddrill <james.haddrill@visitscotland.com>
2020-10-12 15:37:31 +0100

    VS-1984 FIX (StretchedLinkCard) fix linting errors

--- frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
 import VsStretchedLinkCard from '../StretchedLinkCard';
 
-const factoryShallowMount = (values) => shallowMount(VsStretchedLinkCard, {
-     propsData: {
-        link: 'https://www.visitscotland.com/'
-     }
+const factoryShallowMount = () => shallowMount(VsStretchedLinkCard, {
+    propsData: {
+        link: 'https://www.visitscotland.com/',
+    },
 });
 
 describe('VsStretchedLinkCard', () => {
@@ -17,6 +17,6 @@ describe('VsStretchedLinkCard', () => {
         it('should render the component with the link prop passed as the stretched link href', () => {
             const wrapper = factoryShallowMount();
             expect(wrapper.find('.stretched-link').attributes().href).toBe('https://www.visitscotland.com/');
-        })
+        });
     });
 });
