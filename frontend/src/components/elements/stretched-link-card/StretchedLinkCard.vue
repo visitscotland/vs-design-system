7f0d05725d2d97176d06d2581d53b6fa9c8b8628
jhaddrill <james.haddrill@visitscotland.com>
2020-10-12 15:36:24 +0100

    VS-1984 FEAT (StretchedLinkCard) create StretchedLinkCard component

---
+++ frontend/src/components/elements/stretched-link-card/StretchedLinkCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="card">
+        <!-- @slot Contains the image for the card  -->
+        <slot name="stretched-card-image" />
+
+        <div class="card-body">
+            <VsHeading
+                level="4"
+                class="card-title"
+            >
+                <a
+                    :href="link"
+                    class="stretched-link"
+                >
+                    <!-- @slot Contains header content for the card  -->
+                    <slot name="stretched-card-header" />
+                </a>
+            </VsHeading>
+            <!-- @slot Contains body content for the card  -->
+            <slot name="stretched-card-content" />
+        </div>
+    </div>
+</template>
+
+<script>
+import VsHeading from '@components/elements/heading/Heading';
+/**
+ * The Stretched Link Card is a block that stretches its nested link across its whole area
+ * meaning that the whole block is clickable
+ */
+export default {
+    name: 'VsStretchedLinkCard',
+    status: 'prototype',
+    release: '0.0.1',
+    props: {
+        /**
+        * The link that the component will use
+        */
+        link: {
+            type: String,
+            required: true,
+        },
+    },
+    components: {VsHeading},
+};
+</script>
+
+<docs>
+  ```
+    <vs-stretched-link-card>
+        <vs-img
+            slot="stretched-card-image"
+            src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+            alt="Claire standing stones"
+            fluid
+        />
+
+        <template slot="stretched-card-header">
+            A Title Would Go Here
+        </template>
+
+        <vs-rich-text-wrapper slot="stretched-card-content">
+            The content for the card goes here
+        </vs-rich-text-wrapper>
+    </vs-stretched-link-card>
+  ```
+</docs>
---
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils';
+import VsStretchedLinkCard from '../StretchedLinkCard';
+
+const factoryShallowMount = (values) => shallowMount(VsStretchedLinkCard, {
+     propsData: {
+        link: 'https://www.visitscotland.com/'
+     }
+});
+
+describe('VsStretchedLinkCard', () => {
+    it('should render an element with class stretched-link', () => {
+        const wrapper = factoryShallowMount();
+        expect(wrapper.find('.stretched-link').exists()).toBe(true);
+    });
+
+    describe(':props', () => {
+        it('should render the component with the link prop passed as the stretched link href', () => {
+            const wrapper = factoryShallowMount();
+            expect(wrapper.find('.stretched-link').attributes().href).toBe('https://www.visitscotland.com/');
+        })
+    });
+});
