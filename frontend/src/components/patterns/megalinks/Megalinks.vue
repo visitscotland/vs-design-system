9efeaffa297371eff5c8e599bfc5cef8a972cdb4
jhaddrill <james.haddrill@visitscotland.com>
2020-10-16 14:35:05 +0100

    VS-1984 FIX (Megalinks) tidy up syntax

--- frontend/src/components/patterns/megalinks/Megalinks.vue
+++ frontend/src/components/patterns/megalinks/Megalinks.vue
@@ -13,11 +13,11 @@
                             class="vs-megalinks__heading"
                         >
                             <!-- @slot Slot to contain optional title -->
-                            <slot name="vs-multilinks-title" />
+                            <slot name="vs-megalinks-title" />
                         </VsHeading>
 
                         <!-- @slot Slot to contain optional intro content -->
-                        <slot name="vs-multilinks-intro" />
+                        <slot name="vs-megalinks-intro" />
                     </div>
                 </VsCol>
             </VsRow>
@@ -53,9 +53,18 @@ export default {
 
 <style lang="scss">
     .vs-megalinks {
+        // make panels in a row equal height
+        .row {
+            display: flex;
+        }
+
+        .row > [class*='col-'] {
+            display: flex;
+        }
+
         .vs-megalinks__intro {
             text-align: center;
-            padding-bottom: $spacer-9;
+            margin-bottom: $spacer-4;
         }
 
         .vs-megalinks__heading {
@@ -65,18 +74,25 @@ export default {
         .vs-megalinks__intro-content {
             margin-top: $spacer-6;
         }
+
+        @include media-breakpoint-up(lg) {
+            .vs-megalinks__intro {
+                text-align: center;
+                margin-bottom: $spacer-10;
+            }
+        }
     }
 </style>
 
 <docs>
     ```js
-    <vs-megalinks>
-        <template slot="vs-multilinks-title">
+    <VsMegalinks>
+        <template slot="vs-megalinks-title">
             Find your experiences in the highest village of Scotland
         </template>
         <VsRichTextWrapper
             variant="lead"
-            slot="vs-multilinks-intro"
+            slot="vs-megalinks-intro"
             class="mt-6"
         >
 
@@ -86,7 +102,92 @@ export default {
             inventore quidem, nesciunt explicabo repudiandae.
         </VsRichTextWrapper>
 
-        THIS IS WHERE THE LINKS WOULD GO
-    </vs-megalinks>
+        <VsContainer>
+            <VsRow>
+                <VsCol
+                    cols="12"
+                    md="6"
+                    lg="12"
+                >
+                    <vs-megalink-multi-image
+                        featured
+                        imgSrc="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        imgAlt="This is the alt text"
+                        linkType="internal"
+                    >
+                        <template slot="vs-multi-image-header">
+                            The Edinburgh International Festival and summer festival
+                        </template>
+                        <template slot="vs-multi-image-content">
+                            <p>Right across the country, you’ll find amazing places to eat and drink
+                            from local markets to renowned restaurants.</p>
+                        </template>
+                    </vs-megalink-multi-image>
+                </VsCol>
+                <VsCol
+                    cols="12"
+                    md="6"
+                    lg="4"
+                >
+                    <vs-megalink-multi-image
+                        imgSrc="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        imgAlt="This is the alt text 1"
+                        linkType="external"
+                    >
+                        <template slot="vs-multi-image-header">
+                            Count 7,000 shining stars in the iconic galloway forest
+                        </template>
+                        <template slot="vs-multi-image-content">
+                            <p>Right across the country, you’ll find amazing
+                            places to eat and drink from local markets to renowned
+                            restaurants. Here are some recomm…</p>
+                        </template>
+                    </vs-megalink-multi-image>
+                </VsCol>
+                <VsCol
+                    cols="12"
+                    md="6"
+                    lg="4"
+                >
+                    <vs-megalink-multi-image
+                        imgSrc="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        imgAlt="This is the alt text 2"
+                        linkType="external"
+                    >
+                        <template slot="vs-multi-image-header">
+                            Count 7,000 shining stars in the iconic galloway forest
+                        </template>
+                        <template slot="vs-multi-image-content">
+                            <p>Right across the country, you’ll find amazing places to eat and drink
+                            from local markets to renowned restaurants. Here are some recomm…</p>
+                            <p>Right across the country, you’ll find amazing places to eat and drink
+                            from local markets to renowned restaurants. Here are some recomm…</p>
+                            <p>Right across the country, you’ll find amazing places to eat and drink
+                            from local markets to renowned restaurants. Here are some recomm…</p>
+                        </template>
+                    </vs-megalink-multi-image>
+                </VsCol>
+                <VsCol
+                    cols="12"
+                    md="6"
+                    lg="4"
+                >
+                    <vs-megalink-multi-image
+                        imgSrc="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        linkType="download"
+                    >
+                        <template slot="vs-multi-image-header">
+                            Soar through the air on a boat of Falkirk Wheel (PDF 3MB)
+                        </template>
+                        <template slot="vs-multi-image-content">
+                            <p>Right across the country, you’ll find amazing
+                            places to eat and drink from local markets to renowned
+                            restaurants. Here are some recomm…</p>
+                        </template>
+                    </vs-megalink-multi-image>
+                </VsCol>
+            </VsRow>
+        </VsContainer>
+    </VsMegalinks>
     ```
 </docs>
