ef6ce34717b90e060e2e97eb0799e700410c4e1d
Jose Calcines <jose.calcines@visitscotland.com>
2020-10-20 12:48:26 +0100

    FIX (frontend) Resolve issue with formatting

--- frontend/src/components/patterns/header/components/global-menu/GlobalMenuLanguageItem.vue
+++ frontend/src/components/patterns/header/components/global-menu/GlobalMenuLanguageItem.vue
@@ -1,71 +1,71 @@
 <template>
-  <VsDropdownItem
-      class="vs-global-menu__languages__item"
-      :href="languageLink"
-  >
-    {{ languageName }}
-  </VsDropdownItem>
+    <VsDropdownItem
+        class="vs-global-menu__languages__item"
+        :href="languageLink"
+    >
+        {{ languageName }}
+    </VsDropdownItem>
 </template>
 
 <script>
 import VsDropdownItem from '../../../dropdown/DropdownItem';
 
 export default {
-  name: 'VsGlobalMenuLanguageItem',
-  status: 'prototype',
-  release: '0.1.0',
-  components: {
-    VsDropdownItem,
-  },
-  props: {
-    languageName: {
-      type: String,
-      default: null,
+    name: 'VsGlobalMenuLanguageItem',
+    status: 'prototype',
+    release: '0.1.0',
+    components: {
+        VsDropdownItem,
     },
-    languageLink: {
-      type: String,
-      default: null,
+    props: {
+        languageName: {
+            type: String,
+            default: null,
+        },
+        languageLink: {
+            type: String,
+            default: null,
+        },
     },
-  },
 };
 </script>
 
 <style lang="scss">
 .vs-global-menu__languages__item {
-  &:not(:last-of-type) {
-    border-bottom: 1px solid $color-purple-tint-3;
-  }
-
-  .dropdown-item {
-    padding: $spacer-4;
-    color: white;
-    text-decoration: none;
-
-    &:hover {
-      background: $color-purple-shade-2;
+    &:not(:last-of-type) {
+        border-bottom: 1px solid $color-purple-tint-3;
     }
 
-    &:focus {
-      outline: 3px solid $color-purple-tint-5;
-      outline-offset: -3px;
-      background: $color-purple;
+    .dropdown-item {
+        padding: $spacer-4;
+        color: white;
+        text-decoration: none;
+
+        &:hover {
+            background: $color-purple-shade-2;
+        }
+
+        &:focus {
+            outline: 3px solid $color-purple-tint-5;
+            outline-offset: -3px;
+            background: $color-purple;
+        }
     }
-  }
 }
 
 @include no-js {
-  .vs-global-menu__languages__item {
-    display: block;
-  }
+    .vs-global-menu__languages__item {
+        display: block;
+    }
 
-  @include media-breakpoint-up(sm) {
-    display: inline;
-  }
+    @include media-breakpoint-up(sm) {
+        display: inline;
+    }
 }
 </style>
 
 <docs>
-```
-<VsGlobalMenuLanguageItem language-name="English" languageLink="site/"/>
-```
+  ```
+    <VsGlobalMenuLanguageItem language-name="English" languageLink="site/"/>
+  ```
 </docs>
