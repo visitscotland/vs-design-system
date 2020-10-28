34b285fada500e05d688a2d45f15b422c23bf5e9
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2020-10-28 08:25:59 +0000

    FEAT (images) Images validator for link images used in Stop, ListicleItem and SharedLink, if the product associated is not a DMS prodcuct, then an Image is required

---
+++ cms/src/main/java/com/visitscotland/brmx/validator/LinkImageValidator.java
@@ -0,0 +1,46 @@
+package com.visitscotland.brmx.validator;
+
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import java.util.Optional;
+
+/**
+ * jcr:Name = visitscotland:scotland-image-validator
+
+ */
+public class LinkImageValidator implements Validator<Node> {
+    public Optional<Violation> validate(final ValidationContext context, final Node document) {
+        try {
+            Node node = document.hasNode("visitscotland:linkTypes")? document.getNode("visitscotland:linkTypes"): document.hasNode("visitscotland:products")? document.getNode("visitscotland:products"):document.getNode("visitscotland:product");
+            if (node != null) {
+                //Make sure that for Share links and stops, if the product is not dms, an image is provided or/and selected
+                if (!node.hasProperty("visitscotland:product")) {
+                    if (document.hasNode("visitscotland:image")) {
+                        if(document.getNode("visitscotland:image").getProperty("hippo:docbase").getValue().getString().equals("cafebabe-cafe-babe-cafe-babecafebabe")) {
+                            return Optional.of(context.createViolation());
+                        }
+                    }else
+                        //Images for Listicle that allows CMS images but also Instagram images
+                        if (document.hasNode("visitscotland:images")){
+                            Node images = document.getNode("visitscotland:images");
+                            if (!images.hasProperty("visitscotland:caption") && ((!images.hasProperty("hippo:docbase"))
+                                    || (images.hasProperty("hippo:docbase") && images.getProperty("hippo:docbase").getValue().getString().equals("cafebabe-cafe-babe-cafe-babecafebabe")))) {
+                                return Optional.of(context.createViolation());
+                            }
+                            }else{
+                                return Optional.of(context.createViolation());
+                            }
+                }
+            }
+        } catch (RepositoryException e) {
+            return Optional.of(context.createViolation());
+        }
+
+        return Optional.empty();
+    }
+}
+
