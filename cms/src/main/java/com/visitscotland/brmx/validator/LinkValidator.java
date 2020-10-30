0efa31a62dc07ab94d6ab9eaad9808ff4687ebe2
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2020-10-30 11:45:05 +0000

    FEAT (Validator) Message for megalinks

--- cms/src/main/java/com/visitscotland/brmx/validator/LinkValidator.java
+++ cms/src/main/java/com/visitscotland/brmx/validator/LinkValidator.java
@@ -1,12 +1,14 @@
 package com.visitscotland.brmx.validator;
 
 import com.visitscotland.brmx.translation.plugin.SessionFactory;
+import org.apache.commons.lang.StringUtils;
 import org.onehippo.cms.services.validation.api.ValidationContext;
 import org.onehippo.cms.services.validation.api.Validator;
 import org.onehippo.cms.services.validation.api.Violation;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.hippoecm.repository.api.HippoNodeType.HIPPO_DOCBASE;
@@ -18,7 +20,7 @@ import static org.hippoecm.repository.api.HippoNodeType.HIPPO_DOCBASE;
 //TODO investigate if we can create unit test for this validator
 public class LinkValidator implements Validator<Node> {
 
-    private static final String EMPTY_IMAGE = "cafebabe-cafe-babe-cafe-babecafebabe";
+    private static final String EMPTY_DOCUMENT = "cafebabe-cafe-babe-cafe-babecafebabe";
     private SessionFactory sessionFactory;
 
     public LinkValidator() {
@@ -27,11 +29,17 @@ public class LinkValidator implements Validator<Node> {
 
     public Optional<Violation> validate(final ValidationContext context, final Node document) {
         try {
-             if(!document.getProperty(HIPPO_DOCBASE).getValue().getString().equals(EMPTY_IMAGE)) {
+             if(!document.getProperty(HIPPO_DOCBASE).getValue().getString().equals(EMPTY_DOCUMENT)) {
                  Node linkNode = sessionFactory.getJcrSession().getNodeByIdentifier(document.getProperty(HIPPO_DOCBASE).getString());
                  Node childNode = linkNode.getNode(linkNode.getName());
-                 if (!childNode.isNodeType("visitscotland:Page") && !childNode.isNodeType("visitscotland:SharedLink")){
-                     return Optional.of(context.createViolation());
+                 if (document.getParent().isNodeType("visitscotland:Day")) {
+                     if (!childNode.isNodeType("visitscotland:Stop")) {
+                         return Optional.of(context.createViolation("stop"));
+                     }
+                 }else{
+                     if (!childNode.isNodeType("visitscotland:Page") && !childNode.isNodeType("visitscotland:SharedLink")){
+                         return Optional.of(context.createViolation());
+                     }
                  }
                }
         } catch (RepositoryException e) {
