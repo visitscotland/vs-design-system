4f71d68e91bbac0282657a4cb79a716e89c652b2
Marcos Theofilo <marcos.theofilo@visistscotland.com>
2020-11-06 11:43:40 +0000

    VS-1661 FEAT (iKnow Partner) iKnow component created

---
+++ frontend/src/components/patterns/iknow/IknowPartnerItem.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="vs-iknow-partner-item">
+        <VsHeading level="2">
+            Help and advice
+        </VsHeading>
+        <VsSvg
+            class="vs-iknow-partner-item__logo"
+            path="iKnowScotland"
+        />
+        <p class="vs-iknow-partner-item__text">
+            You can also get advice from our accredited
+            <a href="#">iKnow partners in Edinburgh</a>,
+            with excellent local expertise,
+            travel advice and insider tips about must-see
+            attractions and great accommodation in the local area.
+            <VsSvg
+                class="vs-iknow-partner-item__coo"
+                path="highland-cow"
+            />
+        </p>
+    </div>
+</template>
+
+<script>
+import VsHeading from '@components/elements/heading/Heading';
+import VsSvg from '@components/elements/svg/Svg';
+
+export default {
+    name: 'VsIknowPartnerItem',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        VsHeading,
+        VsSvg,
+    },
+};
+</script>
+
+<style lang="scss">
+.vs-iknow-partner-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: $spacer-8;
+
+    @include media-breakpoint-up(xl) {
+        padding: $spacer-11;
+    }
+
+    &__logo {
+        display: inline-block;
+        max-height: 30px;
+        max-width: 105px;
+        margin-bottom: $spacer-8;
+
+        @include media-breakpoint-up(xl) {
+            max-height: 40px;
+            max-width: 138px;
+        }
+    }
+
+    &__text {
+        font-size: $display3-size;
+        margin-bottom: 0;
+
+        @include media-breakpoint-up(xl) {
+            text-align: center;
+            max-width: 620px;
+            position: relative;
+        }
+    }
+
+    &__coo {
+        display: block;
+        width: 100%;
+        height: auto;
+        max-width: 115px;
+        max-height: 100px;
+        margin: $spacer-8 auto 0;
+
+        @include media-breakpoint-up(xl) {
+            max-height: 210px;
+            max-width: 185px;
+            position: absolute;
+            bottom: -5.5rem;
+            left: -14rem;
+        }
+    }
+}
+</style>
+
+<docs>
+```jsx
+    <vs-iknow-partner-item />
+```
+</docs>
