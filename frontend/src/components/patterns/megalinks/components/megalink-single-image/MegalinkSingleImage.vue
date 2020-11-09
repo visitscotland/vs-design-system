1052d5d3d9a8b924a561566dae919b31c433855d
jhaddrill <james.haddrill@visitscotland.com>
2020-11-09 09:08:04 +0000

    VS-1983 (Megalinks components/ftl) cms integration

--- frontend/src/components/patterns/megalinks/components/megalink-single-image/MegalinkSingleImage.vue
+++ frontend/src/components/patterns/megalinks/components/megalink-single-image/MegalinkSingleImage.vue
@@ -1,54 +1,52 @@
 <template>
-    <VsCol cols="12">
-        <section
-            class="megalink-single-image"
-            :class="alternate ? 'megalink-single-image--alternate' : ''"
+    <section
+        class="megalink-single-image"
+        :class="alternate ? 'megalink-single-image--alternate' : ''"
+    >
+        <VsImageWithCaption
+            mobile-overlap
+            alt-text=""
+            :text-align="alternate ? 'left' : 'right'"
+            image-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+            class="megalink-single-image__image"
         >
-            <VsImageWithCaption
-                mobile-overlap
-                alt-text=""
-                :text-align="alternate ? 'left' : 'right'"
-                image-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
-                class="megalink-single-image__image"
+            <template slot="caption">
+                <!-- @slot Slot for image caption -->
+                <slot name="vsSingleImageCaption" />
+            </template>
+            <template slot="credit">
+                <!-- @slot Slot for image credit -->
+                <slot name="vsSingleImageCredit" />
+            </template>
+        </VsImageWithCaption>
+        <div class="megalink-single-image__content">
+            <VsHeading
+                level="2"
+                v-if="title"
             >
-                <template slot="caption">
-                    <!-- @slot Slot for image caption -->
-                    <slot name="vsSingleImageCaption" />
-                </template>
-                <template slot="credit">
-                    <!-- @slot Slot for image credit -->
-                    <slot name="vsSingleImageCredit" />
-                </template>
-            </VsImageWithCaption>
-            <div class="megalink-single-image__content">
-                <VsHeading
-                    level="2"
-                    v-if="title"
+                {{ title }}
+            </VsHeading>
+            <VsRichTextWrapper variant="lead">
+                <!-- @slot Slot for content -->
+                <slot name="vsSingleImageContent" />
+            </VsRichTextWrapper>
+
+            <ul class="megalink-single-image__link-list">
+                <!-- @slot Slot for links list -->
+                <slot name="vsSingleImageLinks" />
+            </ul>
+
+            <div class="megalink-single-image__button">
+                <VsButton
+                    :href="buttonLink"
+                    v-if="buttonLink"
                 >
-                    {{ title }}
-                </VsHeading>
-                <VsRichTextWrapper variant="lead">
-                    <!-- @slot Slot for content -->
-                    <slot name="vsSingleImageContent" />
-                </VsRichTextWrapper>
-
-                <ul class="megalink-single-image__link-list">
-                    <!-- @slot Slot for links list -->
-                    <slot name="vsSingleImageLinks" />
-                </ul>
-
-                <div class="megalink-single-image__button">
-                    <VsButton
-                        :href="buttonLink"
-                        v-if="buttonLink"
-                    >
-                        <!-- @slot Slot for button text -->
-                        <slot name="vsSingleImageButtonText" />
-                    </VsButton>
-                </div>
+                    <!-- @slot Slot for button text -->
+                    <slot name="vsSingleImageButtonText" />
+                </VsButton>
             </div>
-        </section>
-    </VsCol>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -95,8 +93,8 @@ export default {
 
 <style lang="scss">
     .megalink-single-image {
-        margin: 0 -10px;
-        width: 100%;
+        margin: 0 -16px;
+        min-width: 100%;
 
         .megalink-single-image__title {
             margin-bottom: 0;
@@ -143,13 +141,14 @@ export default {
         }
 
         @include media-breakpoint-up(lg) {
+            width: 100%;
             margin: 0 0 $spacer-11;
             display: flex;
             flex-direction: column;
 
             .megalink-single-image__content {
                 width: 50%;
-                padding: $spacer-9;
+                padding: $spacer-9 $spacer-12 $spacer-9 $spacer-9;
                 margin: 0;
             }
 
@@ -175,96 +174,100 @@ export default {
 <docs>
     ```js
     <VsMegalinks>
-        <vs-megalink-single-image buttonLink="www.visitscotland.com">
-            <template slot="vsSingleImageCaption">An image of Scotland</template>
-            <template slot="vsSingleImageCredit">@2020 Credit here</template>
-            <template slot="vsSingleImageContent">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer et eros at est dignissim interdum. Fusce nisl metus,
-                    pharetra eu feugiat vitae, porttitor eget est. Vivamus
-                    condimentum urna vel ante tempor, a eleifend neque ultricies.
-                    Morbi convallis, felis id semper vulputate, nisl est porta quam,
-                    luctus vehicula sapien orci quis urna. Suspendisse accumsan leo
-                    diam, nec faucibus neque pulvinar vitae. Duis non rutrum felis,
-                    ut pretium purus. Nullam hendrerit quam vitae ipsum aliquam
-                    fermentum. Fusce gravida eu est in convallis.
-                </p>
-            </template>
-            <template slot="vsSingleImageLinks">
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-            </template>
-            <template slot="vsSingleImageButtonText">
-                This is the button
-            </template>
-        </vs-megalink-single-image>
-        <vs-megalink-single-image
-            title="This is the second component heading"
-            buttonLink="www.visitscotland.com"
-            alternate
-        >
-            <template slot="vsSingleImageCaption">An image of Scotland</template>
-            <template slot="vsSingleImageCredit">@2020 Credit here</template>
-            <template slot="vsSingleImageContent">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer et eros at est dignissim interdum. Fusce nisl metus,
-                    pharetra eu feugiat vitae, porttitor eget est. Vivamus
-                    condimentum urna vel ante tempor, a eleifend neque ultricies.
-                    Morbi convallis, felis id semper vulputate, nisl est porta quam,
-                    luctus vehicula sapien orci quis urna. Suspendisse accumsan leo
-                    diam, nec faucibus neque pulvinar vitae. Duis non rutrum felis,
-                    ut pretium purus. Nullam hendrerit quam vitae ipsum aliquam
-                    fermentum. Fusce gravida eu est in convallis.
-                </p>
-            </template>
-            <template slot="vsSingleImageLinks">
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-                <li class="megalink-single-image__link-list-item">
-                    <VsLink
-                        href="www.visitscotland.com"
-                    >
-                        This is a link here
-                    </VsLink>
-                </li>
-            </template>
-            <template slot="vsSingleImageButtonText">
-                This is the button
-            </template>
-        </vs-megalink-single-image>
+        <vs-col cols="12">
+            <vs-megalink-single-image buttonLink="www.visitscotland.com">
+                <template slot="vsSingleImageCaption">An image of Scotland</template>
+                <template slot="vsSingleImageCredit">@2020 Credit here</template>
+                <template slot="vsSingleImageContent">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Integer et eros at est dignissim interdum. Fusce nisl metus,
+                        pharetra eu feugiat vitae, porttitor eget est. Vivamus
+                        condimentum urna vel ante tempor, a eleifend neque ultricies.
+                        Morbi convallis, felis id semper vulputate, nisl est porta quam,
+                        luctus vehicula sapien orci quis urna. Suspendisse accumsan leo
+                        diam, nec faucibus neque pulvinar vitae. Duis non rutrum felis,
+                        ut pretium purus. Nullam hendrerit quam vitae ipsum aliquam
+                        fermentum. Fusce gravida eu est in convallis.
+                    </p>
+                </template>
+                <template slot="vsSingleImageLinks">
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                </template>
+                <template slot="vsSingleImageButtonText">
+                    This is the button
+                </template>
+            </vs-megalink-single-image>
+        </vs-col>
+        <vs-col cols="12">
+            <vs-megalink-single-image
+                title="This is the second component heading"
+                buttonLink="www.visitscotland.com"
+                alternate
+            >
+                <template slot="vsSingleImageCaption">An image of Scotland</template>
+                <template slot="vsSingleImageCredit">@2020 Credit here</template>
+                <template slot="vsSingleImageContent">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Integer et eros at est dignissim interdum. Fusce nisl metus,
+                        pharetra eu feugiat vitae, porttitor eget est. Vivamus
+                        condimentum urna vel ante tempor, a eleifend neque ultricies.
+                        Morbi convallis, felis id semper vulputate, nisl est porta quam,
+                        luctus vehicula sapien orci quis urna. Suspendisse accumsan leo
+                        diam, nec faucibus neque pulvinar vitae. Duis non rutrum felis,
+                        ut pretium purus. Nullam hendrerit quam vitae ipsum aliquam
+                        fermentum. Fusce gravida eu est in convallis.
+                    </p>
+                </template>
+                <template slot="vsSingleImageLinks">
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                    <li class="megalink-single-image__link-list-item">
+                        <VsLink
+                            href="www.visitscotland.com"
+                        >
+                            This is a link here
+                        </VsLink>
+                    </li>
+                </template>
+                <template slot="vsSingleImageButtonText">
+                    This is the button
+                </template>
+            </vs-megalink-single-image>
+        </vs-col>
     </VsMegalinks>
     ```
 </docs>
