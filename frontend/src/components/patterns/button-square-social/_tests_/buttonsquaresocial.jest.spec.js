99075a61a43e879c7c51011626497ad8029de878
Michael Prosser <michael.prosser@visitscotland.com>
2020-11-10 12:46:37 +0000

    Cut code reuse in VsButtonSquareSocial tests

--- frontend/src/components/patterns/button-square-social/_tests_/buttonsquaresocial.jest.spec.js
+++ frontend/src/components/patterns/button-square-social/_tests_/buttonsquaresocial.jest.spec.js
@@ -8,13 +8,16 @@ const factoryShallowMount = (propsData) => shallowMount(VsButtonSquareSocial, {
     },
 });
 
+let wrapper;
+beforeEach(() => {
+    wrapper = factoryShallowMount({
+        icon: 'facebook',
+        href: '#facebook.com',
+    });
+});
+
 describe('VsButtonSquareSocial', () => {
     it('should render ButtonSquareSocial', () => {
-        const wrapper = factoryShallowMount({
-            icon: 'facebook',
-            href: '#facebook.com',
-        });
-
         const ButtonSquareSocial = wrapper.find('vslink-stub.button-square-social');
 
         expect(ButtonSquareSocial.exists()).toBe(true);
@@ -22,22 +25,12 @@ describe('VsButtonSquareSocial', () => {
 
     describe(':props', () => {
         it(':icon - Social icon to show', () => {
-            const wrapper = factoryShallowMount({
-                icon: 'facebook',
-                href: '#facebook.com',
-            });
-
             const vsLink = wrapper.find('vsicon-stub');
 
             expect(vsLink.attributes('name')).toBe('facebook');
         });
 
         it(':href - Social the external URL the button will point to', () => {
-            const wrapper = factoryShallowMount({
-                icon: 'facebook',
-                href: '#facebook.com',
-            });
-
             const vsLink = wrapper.find('vslink-stub.button-square-social');
 
             expect(vsLink.attributes('href')).toBe('#facebook.com');
