eda1941b6d092296b8e8b0afe9532182fcdeb54f
Michael Prosser <michael.prosser@visitscotland.com>
2020-11-11 16:04:48 +0000

    Add remaining img tests for fluid props

--- frontend/src/components/elements/img/__tests__/img.jest.spec.js
+++ frontend/src/components/elements/img/__tests__/img.jest.spec.js
@@ -1,10 +1,21 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 
 import VsImg from '../Img';
 
 const slotText = 'Image text';
 
 const factoryShallowMount = (propsData) => shallowMount(VsImg, {
+    slots: {
+        default: slotText,
+    },
+    propsData: {
+        ...propsData,
+        src: 'https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm',
+        alt: 'Claire standing stones',
+    }
+});
+
+const factoryMount = (propsData) => mount(VsImg, {
     slots: {
         default: slotText,
     },
@@ -24,19 +35,36 @@ describe('VsButton', () => {
 
     describe(':props', () => {
         it(':should accept and render a `src` property', () => {
-            expect(true).toBe(false);
+            const wrapper = factoryShallowMount();
+
+            expect(wrapper.attributes('src')).toBe('https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm');
         });
 
         it(':should accept and render an `alt` property', () => {
-            expect(true).toBe(false);
+            const wrapper = factoryShallowMount();
+
+            expect(wrapper.attributes('alt')).toBe('Claire standing stones');
         });
 
-        it(':should accept and render a `fluid` property', () => {
-            expect(true).toBe(false);
+        it(':should set an `img-fluid` class if the `fluid` property is truthy', () => {
+            const wrapper = factoryMount({
+                fluid: true
+            });
+
+            expect(wrapper.classes('img-fluid')).toBe(true);
         });
 
-        it(':should accept and render a `fluid-grow` property', () => {
-            expect(true).toBe(false);
+        it(':should set an `img-fluid` class and a `w-100` class if the `fluid-grow` property is truthy', () => {
+            const wrapper = factoryMount({
+                fluidGrow: true
+            });
+
+            expect([
+                wrapper.classes('img-fluid'),
+                wrapper.classes('w-100')
+            ]).toEqual([
+                true, true
+            ]);
         });
     });
 
