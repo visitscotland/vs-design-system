d7163c8feb2508f446a7c4f1e43d726f0806bf2d
jhaddrill <james.haddrill@visitscotland.com>
2020-11-11 10:18:07 +0000

    VS-1984 FIX (component tests) replace references to classes in unit test with data-test attributes

--- frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
@@ -11,13 +11,13 @@ const factoryShallowMount = () => shallowMount(VsStretchedLinkCard, {
 describe('VsStretchedLinkCard', () => {
     it('should render an element with class stretched-link', () => {
         const wrapper = factoryShallowMount();
-        expect(wrapper.find('.stretched-link').exists()).toBe(true);
+        expect(wrapper.find('[data-test="stretched-link"]').exists()).toBe(true);
     });
 
     describe(':props', () => {
         it('should render the component with the link prop passed as the stretched link href', () => {
             const wrapper = factoryShallowMount();
-            expect(wrapper.find('.stretched-link').attributes().href).toBe('https://www.visitscotland.com/');
+            expect(wrapper.find('[data-test="stretched-link"]').attributes().href).toBe('https://www.visitscotland.com/');
         });
     });
 });
