74457239646426824fec8d825f106114154c19c5
jhaddrill <james.haddrill@visitscotland.com>
2020-11-25 08:43:40 +0000

    VS-2319 FEAT (carousel) create slide component and further customisation

--- frontend/src/components/patterns/carousel/Carousel.vue
+++ frontend/src/components/patterns/carousel/Carousel.vue
@@ -1,29 +1,131 @@
 <template>
-    <Splide :options="splideOptions">
-        <SplideSlide>
-            <img
-                alt=""
-                src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+    <VsContainer>
+        <VsRow>
+            <VsCol
+                cols="12"
             >
-        </SplideSlide>
-        <SplideSlide>
-            <img
-                alt=""
-                src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
-            >
-        </SplideSlide>
-        <SplideSlide>
-            <img
-                alt=""
-                src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
-            >
-        </SplideSlide>
-    </splide>
+                <section class="carousel">
+                    <Splide
+                        :options="splideOptions"
+                    >
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+
+                        <VsCarouselSlide
+                            link-url="www.visitscotland.com"
+                            link-type="external"
+                            img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        >
+                            <template slot="vsCarouselSlideHeading">
+                                Count 7,000 shining stars in the iconic
+                                galloway forest
+                            </template>
+                            <template slot="vsCarouselSlideContent">
+                                <p>
+                                    Right across the country, you’ll find amazing
+                                    places to eat and drink from local markets to renowned
+                                    restaurants. Here are some recomm…
+                                </p>
+                            </template>
+                        </VsCarouselSlide>
+                    </Splide>
+                </section>
+            </VsCol>
+        </VsRow>
+    </VsContainer>
 </template>
 
 <script>
-// import VsStretchedLinkCard from '@components/elements/stretched-link-card/StretchedLinkCard';
-import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import { Splide } from '@splidejs/vue-splide';
+import VsCarouselSlide from '@components/patterns/carousel/components/CarouselSlide';
+// import leftArrow from '../../../assets/svg/icons/family.svg';
 
 /**
 * Multi purpose carousel component to use
@@ -32,17 +134,30 @@ import { Splide, SplideSlide } from '@splidejs/vue-splide';
 
 export default {
     name: 'VsCarousel',
-    status: 'prototype',
+    status: 'protolink-type',
     release: '0.0.1',
     components: {
-        // VsStretchedLinkCard,
         Splide,
-        SplideSlide,
+        VsCarouselSlide,
     },
     data() {
         return {
             splideOptions: {
-                perPage: 2,
+                perPage: 4,
+                gap: '24px',
+                arrowPath: 'M20.527,73.811,22.3,72.043a1.072,1.072,0,0,0,0-1.515L8.444,56.677H38.929A1.071,1.071,0,0,0,40,55.606v-2.5a1.071,1.071,0,0,0-1.071-1.071H8.444L22.3,38.183a1.072,1.072,0,0,0,0-1.515L20.527,34.9a1.072,1.072,0,0,0-1.515,0L.314,53.6a1.072,1.072,0,0,0,0,1.515l18.7,18.7A1.071,1.071,0,0,0,20.527,73.811Z',
+                breakpoints: {
+                    992: {
+                        perPage: 3,
+                    },
+                    768: {
+                        perPage: 2,
+                    },
+                    576: {
+                        perPage: 1,
+                        width: '80%',
+                    },
+                },
             },
         };
     },
@@ -51,6 +166,69 @@ export default {
 
 <style lang="scss">
     @import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+    .carousel {
+        .splide {
+            margin: 0 auto;
+        }
+
+        .splide__arrow[disabled] {
+            display: none;
+        }
+
+        .splide__track {
+            overflow: visible;
+        }
+
+        .splide__pagination {
+            position: relative;
+            transform: none;
+            width: 100%;
+            left: auto;
+            bottom: auto;
+        }
+
+        .splide__arrow {
+            border-radius: 0;
+            background: $color-pink;
+            opacity: 1;
+            top: 40%;
+
+            svg {
+                transform: translateY(-28%);
+                fill: $color-white;
+                height: 56px;
+            }
+        }
+
+        .splide__arrow--prev {
+            left: 0;
+            transform: translateX(-50%);
+        }
+
+        .splide__arrow--next {
+            right: 0;
+            transform: translateX(50%);
+
+            svg {
+                transform: translateY(-28%) scaleX(-1);
+            }
+        }
+
+        .splide__pagination__page {
+            width: 10px;
+            height: 10px;
+            color: $color-gray-tint-4;
+
+            &.is-active {
+                transform: none;
+                background: $color-black;
+                opacity: 1;
+                width: 14px;
+                height: 14px;
+            }
+        }
+    }
 </style>
 
 <docs>
---
+++ frontend/src/components/patterns/carousel/components/CarouselSlide.vue
@@ -0,0 +1,86 @@
+<template>
+    <SplideSlide>
+        <VsStretchedLinkCard
+            :link="linkUrl"
+            :type="linkType"
+            class="carousel-slide"
+            data-test="carousel-slide-featured"
+        >
+            <VsImg
+                slot="stretchedCardImage"
+                :src="imgSrc"
+                :alt="imgAlt"
+                class="carousel-slide__img"
+            />
+            <span
+                slot="stretchedCardHeader"
+                class="carousel-slide__title"
+                data-test="carousel-slide__title"
+            ><!-- @slot Slot to contain heading --><slot name="vsCarouselSlideHeading" />
+            </span>
+        </VsStretchedLinkCard>
+    </SplideSlide>
+</template>
+
+<script>
+import { SplideSlide } from '@splidejs/vue-splide';
+import VsStretchedLinkCard from '@components/elements/stretched-link-card/StretchedLinkCard';
+
+/**
+* Slide for carousel
+*/
+
+export default {
+    name: 'VsCarouselSlide',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        SplideSlide,
+        VsStretchedLinkCard,
+    },
+    props: {
+        /**
+        * The image to use in the component
+        */
+        imgSrc: {
+            required: true,
+            type: String,
+        },
+        /**
+        * The image alt text to use in the component
+        */
+        imgAlt: {
+            required: false,
+            type: String,
+            default: '',
+        },
+        /**
+        * The type of link. This will set the icon.
+        * `external, internal, download`
+        */
+        linkType: {
+            type: String,
+            required: true,
+            validator: (value) => value.match(/(external|internal|download)/),
+        },
+        /**
+        * The link destination
+        */
+        linkUrl: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+    .splide__slide {
+        opacity: 0.5;
+        transition: opacity 0.2s ease;
+
+        &.is-visible {
+            opacity: 1;
+        }
+    }
+</style>
