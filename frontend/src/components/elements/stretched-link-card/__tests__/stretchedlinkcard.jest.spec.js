ea30835fa7758b9255afbdae0d32d1b76cb617aa
jhaddrill <james.haddrill@visitscotland.com>
2020-12-02 12:33:48 +0000

    VS-2318 FEAT (Megalinks, StretchedLink) move styles and markup into stretched link component

--- frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkcard.jest.spec.js
@@ -1,10 +1,14 @@
 import { shallowMount } from '@vue/test-utils';
 import VsStretchedLinkCard from '../StretchedLinkCard';
 
+const imgUrl = 'https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm';
+
 const factoryShallowMount = () => shallowMount(VsStretchedLinkCard, {
     propsData: {
         link: 'https://www.visitscotland.com/',
         type: 'external',
+        imgSrc: imgUrl,
+        imgAlt: 'Image alt',
     },
 });
 
@@ -18,6 +22,18 @@ describe('VsStretchedLinkCard', () => {
         it('should render the component with the link prop passed as the stretched link href', () => {
             const wrapper = factoryShallowMount();
             expect(wrapper.find('[data-test="stretched-link"]').attributes().href).toBe('https://www.visitscotland.com/');
+        });
+        it('should not render an image if not src attribute is set', async() => {
+            const wrapper = shallowMount(VsStretchedLinkCard, {
+                propsData: {
+                    link: 'https://www.visitscotland.com/',
+                    type: 'external',
+                    imgSrc: '',
+                    imgAlt: 'Image alt',
+                },
+            });
+
+            await expect(wrapper.find('[data-test="stretched-link__img"]').exists()).toBe(false);
         });
     });
 });
