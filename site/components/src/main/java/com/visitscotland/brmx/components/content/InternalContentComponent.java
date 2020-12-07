1963fc992503ec3ff8f51c70724ef5c17c4b12ca
Jose Calcines <jose.calcines@visitscotland.com>
2020-12-07 11:06:29 +0000

    VS-2371 FEAT (Header & Footer) Fix erratic behaviour in the legacy implementation

--- site/components/src/main/java/com/visitscotland/brmx/components/content/InternalContentComponent.java
+++ site/components/src/main/java/com/visitscotland/brmx/components/content/InternalContentComponent.java
@@ -1,98 +1,29 @@
 package com.visitscotland.brmx.components.content;
 
-import com.visitscotland.brmx.beans.mapping.LocalizedURL;
 import com.visitscotland.brmx.services.ResourceBundleService;
 import com.visitscotland.brmx.utils.HippoUtilsService;
-import com.visitscotland.brmx.utils.Language;
-import com.visitscotland.brmx.utils.Properties;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.onehippo.cms7.essentials.components.CommonComponent;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 
 public class InternalContentComponent extends CommonComponent {
 
-    public static final String PARAM_SSO = "sso";
-    public static final String PARAM_ROOT_PATH = "root-path";
-    public static final String PARAM_EXTERNAL = "external";
-
-    public static final String PATH_PLACEHOLDER = "[PATH-PLACEHOLDER]";
-
-    public static final String FULLY_QUALIFIED_URLS = "fullyQualified";
-    public static final String SSO_URL = "returnUrl";
-    public static final String GLOBAL_MENU_URLS = "placeholerLocalizedURLs";
-
-
     ResourceBundleService bundle;
     HippoUtilsService utils;
+    InternalParameterProcessor processor;
 
     public InternalContentComponent() {
         bundle = new ResourceBundleService();
         utils = new HippoUtilsService();
+        processor = new InternalParameterProcessor(bundle, utils);
     }
 
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) {
         super.doBeforeRender(request, response);
 
-        addLocalizedURLs(request);
-        processParameters(request);
-    }
-
-    void processParameters(HstRequest request) {
-        final StringBuilder returnUrl = new StringBuilder();
-        String external = utils.getParameterFromUrl(request, PARAM_EXTERNAL);
-        String rootPath = utils.getParameterFromUrl(request, PARAM_ROOT_PATH);
-        String sso = utils.getParameterFromUrl(request, PARAM_SSO);
-
-        if (rootPath != null) {
-            returnUrl.append(rootPath).append("/").append(PATH_PLACEHOLDER);
-            request.getRequestContext().setModel(FULLY_QUALIFIED_URLS, "true");
-        } else if (external != null) {
-            returnUrl.append(composeNonExistingURL(request.getLocale(), request));
-            request.getRequestContext().setModel(FULLY_QUALIFIED_URLS, "true");
-        } else {
-            returnUrl.append("/").append(PATH_PLACEHOLDER);
-        }
-
-        if (sso != null) {
-            returnUrl.append("?id=").append(sso);
-        }
-
-        request.setModel(SSO_URL, returnUrl.toString());
-    }
-
-    void addLocalizedURLs(HstRequest request) {
-        List<LocalizedURL> translatedURLs = new ArrayList<>(Language.values().length);
-
-        for (Language language : Language.values()) {
-            LocalizedURL lan = new LocalizedURL();
-            lan.setLocale(language.getLocale());
-            lan.setLanguage(language.getLocale().getLanguage());
-            lan.setDisplayName(bundle.getResourceBundle("universal", lan.getLanguage(), request.getLocale()));
-            lan.setUrl(composeNonExistingURL(language.getLocale(), request));
-
-            translatedURLs.add(lan);
-        }
-        request.getRequestContext().setModel(GLOBAL_MENU_URLS, translatedURLs);
-    }
-
-    /**
-     * Composes a non existing URL with the Placeholder.
-     */
-    private String composeNonExistingURL(Locale locale, HstRequest request) {
-        String languagePath = "";
-
-        if (locale != null && Language.getLanguageForLocale(locale).getCMSPathVariable().length() != 0) {
-            languagePath = "/" + Language.getLanguageForLocale(locale).getCMSPathVariable();
-        }
-
-        return request.getRequestContext().getBaseURL().getHostName() +
-                request.getRequestContext().getBaseURL().getContextPath() +
-                languagePath + "/" + PATH_PLACEHOLDER;
+        processor.addLocalizedURLs(request);
+        processor.processParameters(request);
     }
 }
--- site/components/src/test/java/com/visitscotland/brmx/components/content/InternalContentComponentTest.java
+++ site/components/src/test/java/com/visitscotland/brmx/components/content/InternalContentComponentTest.java
@@ -1,12 +1,10 @@
 package com.visitscotland.brmx.components.content;
 
-import com.visitscotland.brmx.beans.Destination;
 import com.visitscotland.brmx.beans.mapping.LocalizedURL;
 import com.visitscotland.brmx.services.ResourceBundleService;
 import com.visitscotland.brmx.utils.HippoUtilsService;
 import com.visitscotland.brmx.utils.Language;
 import org.hippoecm.hst.core.component.HstRequest;
-import org.hippoecm.hst.mock.core.component.MockHstRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,16 +17,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class InternalContentComponentTest {
+class InternalParameterProcessorTest {
 
-    @Mock(answer = Answers.CALLS_REAL_METHODS)
-    InternalContentComponent component;
+    InternalParameterProcessor component;
 
     @Mock
     HippoUtilsService utils;
@@ -45,36 +41,35 @@ class InternalContentComponentTest {
 
     @BeforeEach
     void init() {
-        component.utils = this.utils;
-        component.bundle = this.bundle;
+        component = new InternalParameterProcessor(bundle, utils);
     }
 
     @Test
     @DisplayName("VS-443 - processParameters - No query parameters defined")
-    void processParameters(){
+    void processParameters() {
         ArgumentCaptor<String> ssoUrl = ArgumentCaptor.forClass(String.class);
 
-        when(request.setModel(eq(InternalContentComponent.SSO_URL), ssoUrl.capture())).thenReturn(null);
+        when(request.setModel(eq(InternalParameterProcessor.SSO_URL), ssoUrl.capture())).thenReturn(null);
 
         component.processParameters(request);
 
-        Assertions.assertEquals("/"+ InternalContentComponent.PATH_PLACEHOLDER, ssoUrl.getValue());
+        Assertions.assertEquals("/" + InternalParameterProcessor.PATH_PLACEHOLDER, ssoUrl.getValue());
     }
 
     @Test
     @DisplayName("VS-2357 - processParameters - external query parameter specification")
-    void processParameters_external(){
+    void processParameters_external() {
         //Verify tha populates the fullQualifiedUrls attribute on the request CONTEXT
         //When there is no parameters the SSO_URL is host + /[PATH-PLACEHOLDER]
         ArgumentCaptor<String> fullyQualified = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<String> ssoUrl = ArgumentCaptor.forClass(String.class);
 
-        when(utils.getParameterFromUrl(request, InternalContentComponent.PARAM_EXTERNAL)).thenReturn("true");
-        when(request.getRequestContext().setModel(eq(InternalContentComponent.FULLY_QUALIFIED_URLS), fullyQualified.capture())).thenReturn(null);
+        when(utils.getParameterFromUrl(request, InternalParameterProcessor.PARAM_EXTERNAL)).thenReturn("true");
+        when(request.getRequestContext().setModel(eq(InternalParameterProcessor.FULLY_QUALIFIED_URLS), fullyQualified.capture())).thenReturn(null);
         when(request.getRequestContext().getBaseURL().getHostName()).thenReturn("http://visitscotland.com");
         when(request.getRequestContext().getBaseURL().getContextPath()).thenReturn("/site");
         when(request.getLocale()).thenReturn(Locale.UK);
-        when(request.setModel(eq(InternalContentComponent.SSO_URL), ssoUrl.capture())).thenReturn(null);
+        when(request.setModel(eq(InternalParameterProcessor.SSO_URL), ssoUrl.capture())).thenReturn(null);
 
 
         component.processParameters(request);
@@ -86,52 +81,117 @@ class InternalContentComponentTest {
 
     @Test
     @DisplayName("VS-2357 - processParameters - root-path query parameter specification")
-    void processParameters_rootPath(){
+    void processParameters_rootPath() {
         //Verify tha populates the fullQualifiedUrls attribute on the request CONTEXT
         //When there is no parameters the SSO_URL is rootpath + /[PATH-PLACEHOLDER]
         ArgumentCaptor<String> fullyQualified = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<String> ssoUrl = ArgumentCaptor.forClass(String.class);
 
         when(utils.getParameterFromUrl(eq(request), any())).thenReturn(null);
-        when(utils.getParameterFromUrl(request, InternalContentComponent.PARAM_ROOT_PATH)).thenReturn("http://visitscotlan.com/ws/tours");
-        when(request.getRequestContext().setModel(eq(InternalContentComponent.FULLY_QUALIFIED_URLS), fullyQualified.capture())).thenReturn(null);
-        when(request.setModel(eq(InternalContentComponent.SSO_URL), ssoUrl.capture())).thenReturn(null);
+        when(utils.getParameterFromUrl(request, InternalParameterProcessor.PARAM_ROOT_PATH)).thenReturn("http://visitscotlan.com/");
+        when(request.getRequestContext().setModel(eq(InternalParameterProcessor.FULLY_QUALIFIED_URLS), fullyQualified.capture())).thenReturn(null);
+        when(request.setModel(eq(InternalParameterProcessor.SSO_URL), ssoUrl.capture())).thenReturn(null);
 
         component.processParameters(request);
 
         Assertions.assertEquals("true", fullyQualified.getValue());
-        Assertions.assertEquals("http://visitscotlan.com/ws/tours/[PATH-PLACEHOLDER]", ssoUrl.getValue());
+        Assertions.assertEquals("http://visitscotlan.com/[PATH-PLACEHOLDER]", ssoUrl.getValue());
     }
 
     @Test
     @DisplayName("VS-2358 - processParameters - sso query parameter specification")
-    void processParameters_sso(){
+    void processParameters_sso() {
         //Verify that DOES NOT populate the fullQualifiedUrls attribute on the request CONTEXT
         //When there is no parameters the SSO_URL is /[PATH-PLACEHOLDER]?id={sso}
         ArgumentCaptor<String> ssoUrl = ArgumentCaptor.forClass(String.class);
 
         when(utils.getParameterFromUrl(eq(request), any())).thenReturn(null);
-        when(utils.getParameterFromUrl(request, InternalContentComponent.PARAM_SSO)).thenReturn("jcalcines");
+        when(utils.getParameterFromUrl(request, InternalParameterProcessor.PARAM_SSO)).thenReturn("jcalcines");
 
-        when(request.setModel(eq(InternalContentComponent.SSO_URL), ssoUrl.capture())).thenReturn(null);
+        when(request.setModel(eq(InternalParameterProcessor.SSO_URL), ssoUrl.capture())).thenReturn(null);
 
         component.processParameters(request);
 
-        verify(request.getRequestContext(), never()).setModel(eq(InternalContentComponent.FULLY_QUALIFIED_URLS), any());
+        verify(request.getRequestContext(), never()).setModel(eq(InternalParameterProcessor.FULLY_QUALIFIED_URLS), any());
         Assertions.assertEquals("/[PATH-PLACEHOLDER]?id=jcalcines", ssoUrl.getValue());
     }
 
     @Test
     @DisplayName("VS-2360 - addlocalizedUrls - verify that localized URLs have the placeholders")
-    void addLocalizedUrls(){
+    void addLocalizedUrls() {
         ArgumentCaptor<List<LocalizedURL>> urls = ArgumentCaptor.forClass(List.class);
 
-        when(request.getRequestContext().setModel(eq(InternalContentComponent.GLOBAL_MENU_URLS), urls.capture())).thenReturn(null);
+        when(request.getRequestContext().setModel(eq(InternalParameterProcessor.GLOBAL_MENU_URLS), urls.capture())).thenReturn(null);
 
         component.addLocalizedURLs(request);
 
         Assertions.assertEquals(Language.values().length, urls.getValue().size());
-        Assertions.assertTrue(urls.getValue().get(0).getUrl().contains(InternalContentComponent.PATH_PLACEHOLDER));
+        Assertions.assertTrue(urls.getValue().get(0).getUrl().contains(InternalParameterProcessor.PATH_PLACEHOLDER));
 
+    }
+
+    @Test
+    @DisplayName("VS-2357 - processParameters - root-path behaves as the legacy project does")
+    void getDomain_rootPath_mimicLegacyProject() {
+
+        Assertions.assertEquals("http://demo.visitscotland.com/[PATH-PLACEHOLDER]",
+                testRootPath("http://demo.visitscotland.com"));
+        Assertions.assertEquals("http://demo.visitscotland.com/[PATH-PLACEHOLDER]",
+                testRootPath("http://demo.visitscotland.com/"));
+        Assertions.assertEquals("http://demo.visitscotland.com/[PATH-PLACEHOLDER]",
+                testRootPath("http://demo.visitscotland.com/tours"));
+        Assertions.assertEquals("/[PATH-PLACEHOLDER]", testRootPath("demo.visitscotland.com"));
+        Assertions.assertEquals("/[PATH-PLACEHOLDER]", testRootPath("visitscotland"));
+
+        // root-path=http://demo.visitscotland.com/ -- http://demo.visitscotland.com
+        // root-path=http://demo.visitscotland.com -- http://demo.visitscotland.com
+        // root-path=http://demo.visitscotland.com/tours -- http://demo.visitscotland.com
+        // root-path=demo.visitscotland.com -- no root-path -->  nothing (Log)
+        // root-path=hola -- no root-path -->  nothing (Log)
+
+    }
+
+    //
+
+
+    @Test
+    @DisplayName("VS-2357 - processParameters - root-path fixes issues from the legacy project")
+    void getDomain_rootPath_fixIssuesInLegacyProject() {
+
+        Assertions.assertEquals("http://demo.visitscotland.com:8080/[PATH-PLACEHOLDER]",
+                testRootPath("http://demo.visitscotland.com:8080"));
+        Assertions.assertEquals("http://user@demo.visitscotland.com:8080/[PATH-PLACEHOLDER]",
+                testRootPath("http://user@demo.visitscotland.com:8080"));
+        Assertions.assertEquals("//demo.visitscotland.com/[PATH-PLACEHOLDER]",
+                testRootPath("//demo.visitscotland.com"));
+        Assertions.assertEquals("scheme://demo.visitscotland.com/[PATH-PLACEHOLDER]",
+                testRootPath("scheme://demo.visitscotland.com"));
+
+
+        // root-path=http://demo.visitscotland.com:8080 -- http://demo.visitscotland.com:8080
+        // root-path=http://test@demo.visitscotland.com:8080 -- http://test@demo.visitscotland.com:8080
+        // root-path=//demo.visitscotland.com/tours -- //demo.visitscotland.com
+        // root-path=ftp://test@demo.visitscotland.com:8080 -- ftp://test@demo.visitscotland.com:8080
+
+        // root-path=demo.visitscotland.com -- no root-path --> Bad Request?
+        // root-path=hola -- no root-path --> Bad Request?
+    }
+
+    /**
+     * Mocks a request and process the parameter rootPath.
+     *
+     * @return The resultant returnURL for that parameter
+     */
+    String testRootPath(String rootPath) {
+        HstRequest request = mock(HstRequest.class, Answers.RETURNS_DEEP_STUBS);
+        ArgumentCaptor<String> ssoUrl = ArgumentCaptor.forClass(String.class);
+
+        when(utils.getParameterFromUrl(eq(request), any())).thenReturn(null);
+        when(utils.getParameterFromUrl(request, InternalParameterProcessor.PARAM_ROOT_PATH)).thenReturn(rootPath);
+        when(request.setModel(eq(InternalParameterProcessor.SSO_URL), ssoUrl.capture())).thenReturn(null);
+
+        component.processParameters(request);
+
+        return ssoUrl.getValue();
     }
 }
