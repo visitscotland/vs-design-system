4cb00128fc8f5764e4d6f5ca8ad43dfbbe44943c
jhaddrill <james.haddrill@visitscotland.com>
2020-12-09 10:41:32 +0000

    VS-2308 FEAT (Carousel, CarouselSlide) a 'vs-' to data-test attributes

--- frontend/src/components/patterns/carousel/components/CarouselSlide.vue
+++ frontend/src/components/patterns/carousel/components/CarouselSlide.vue
@@ -10,7 +10,7 @@
             <span
                 slot="stretchedCardHeader"
                 class="vs-carousel-slide__title"
-                data-test="carousel-slide__title"
+                data-test="vs-carousel-slide__title"
             >
                 <!-- @slot Slot to contain heading -->
                 <slot name="vsCarouselSlideHeading" />
