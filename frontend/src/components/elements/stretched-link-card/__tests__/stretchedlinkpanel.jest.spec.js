1d514ce1f047bcda8b6f645952651bbe3b9b10ce
jhaddrill <james.haddrill@visitscotland.com>
2020-12-10 15:27:50 +0000

    VS-2321 (FIX) add vs- prefix to classes

--- frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkpanel.jest.spec.js
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkpanel.jest.spec.js
@@ -3,23 +3,22 @@ import VsStretchedLinkPanels from '../components/StretchedLinkPanels';
 
 describe('VsStretchedLinkCard', () => {
     describe(':props', () => {
-        it('should render a `.stretched-link-panel--days` class if the days prop is defined', () => {
+        it('should render a `.vs-stretched-link-panel--days` class if the days prop is defined', () => {
             const wrapper = shallowMount(VsStretchedLinkPanels, {
                 propsData: {
                     days: '10',
                 },
             });
-            expect(wrapper.find('[data-test="stretched-link-panel--days"]').exists()).toBe(true);
+            expect(wrapper.find('[data-test="vs-stretched-link-panel--days"]').exists()).toBe(true);
         });
 
         it('should render an element with the text prop content if it is defined', () => {
             const wrapper = shallowMount(VsStretchedLinkPanels, {
                 propsData: {
-                    text: 'Panel text',
+                    transport: 'Panel text',
                 },
             });
-            expect(wrapper.find('[data-test="stretched-link-panel__icon"]').text()).toBe('Panel text');
-            expect(wrapper.find('[data-test="stretched-link-panel--days"]').exists()).toBe(false);
+            expect(wrapper.find('[data-test="vs-stretched-link-panel__icon"]').text()).toBe('Panel text');
         });
     });
 });
