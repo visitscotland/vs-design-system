d6f99585800051ed65f4fa77950cb20d619c93a8
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2020-12-10 12:16:23 +0000

    VS-2382 FEAT (External document)  update test

--- cms/src/test/java/com/visitscotland/brmx/validator/ExternalDocumentValidatorTest.java
+++ cms/src/test/java/com/visitscotland/brmx/validator/ExternalDocumentValidatorTest.java
@@ -1,19 +1,15 @@
 package com.visitscotland.brmx.validator;
 
-import com.visitscotland.brmx.beans.ExternalDocument;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.onehippo.cms.services.validation.api.ValidationContext;
 import org.onehippo.cms.services.validation.api.Violation;
 
-import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -24,9 +20,8 @@ class ExternalDocumentValidatorTest {
     ValidationContext context;
 
     @Test
-    @DisplayName("If the length of the field is bigger than the max allowed, an error should be displayed")
+    @DisplayName("The URL is not valid if it does not contain pdf extension")
     void externalDocumentIncorrect() throws RepositoryException {
-        Node node = Mockito.mock(Node.class, RETURNS_DEEP_STUBS);
         ExternalDocumentValidator validator = new ExternalDocumentValidator();
 
         when(context.createViolation()).thenReturn(mock(Violation.class));
