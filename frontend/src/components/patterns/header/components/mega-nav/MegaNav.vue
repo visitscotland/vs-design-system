d5632ef95e57e9b1ae618e8cf65a8059ee3e0cf0
Gemma <gemma.stephen@visitscotland.com>
2020-12-11 15:43:26 +0000

    Tidying up files/folders

--- frontend/src/components/patterns/header/components/mega-nav/MegaNav.vue
+++ frontend/src/components/patterns/header/components/mega-nav/MegaNav.vue
@@ -81,8 +81,8 @@ import {
     VsCol, VsRow, VsContainer,
 } from '@components/elements/layout';
 import VsSvgLink from '@components/patterns/svg-link/SvgLink';
-import VsMegaNavDropdown from '@components/patterns/header/components/mega-nav/MegaNavDropdown';
-import VsMegaNavTopMenu from '@components/patterns/header/components/mega-nav/desktop-menu/MegaNavTopMenu';
+import VsMegaNavDropdown from '@components/patterns/header/components/mega-nav/components/MegaNavDropdown';
+import VsMegaNavTopMenu from '@components/patterns/header/components/mega-nav/components/MegaNavTopMenu';
 import VsIcon from '@components/elements/icon/Icon';
 
 /**
