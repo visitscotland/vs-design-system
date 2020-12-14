fa434c22525eca0433e3ec317406928c5cb12d92
jhaddrill <james.haddrill@visitscotland.com>
2020-12-14 09:33:21 +0000

    VS-2321 FEAT (Carousel, StretchedLink) add props for 'days' label

--- frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkpanel.jest.spec.js
+++ frontend/src/components/elements/stretched-link-card/__tests__/stretchedlinkpanel.jest.spec.js
@@ -1,35 +1,28 @@
 import { shallowMount } from '@vue/test-utils';
 import VsStretchedLinkPanels from '../components/StretchedLinkPanels';
 
+const factoryShallowMount = () => shallowMount(VsStretchedLinkPanels, {
+    propsData: {
+        days: '10',
+        transport: 'Transport text',
+        daysLabel: 'days',
+    },
+});
+
 describe('VsStretchedLinkCard', () => {
     describe(':props', () => {
         it('should render a `.vs-stretched-link-panel--days` class if the days prop is defined', () => {
-            const wrapper = shallowMount(VsStretchedLinkPanels, {
-                propsData: {
-                    days: '10',
-                },
-            });
+            const wrapper = factoryShallowMount();
             expect(wrapper.find('[data-test="vs-stretched-link-panel--days"]').text()).toContain('10');
         });
 
         it('should render an element with the text prop content if it is defined', () => {
-            const wrapper = shallowMount(VsStretchedLinkPanels, {
-                propsData: {
-                    transport: 'Panel text',
-                },
-            });
-            expect(wrapper.find('[data-test="vs-stretched-link-panel__icon"]').text()).toBe('Panel text');
+            const wrapper = factoryShallowMount();
+            expect(wrapper.find('[data-test="vs-stretched-link-panel__icon"]').text()).toBe('Transport text');
         });
-    });
 
-    describe(':slots', () => {
         it('should render the contnet of the `stretchedLinkPanelDays` slot', () => {
-            const wrapper = shallowMount(VsStretchedLinkPanels, {
-                slots: {
-                    stretchedLinkPanelDays: 'days',
-                },
-            });
-
+            const wrapper = factoryShallowMount();
             expect(wrapper.find('[data-test="vs-stretched-link-panel--days"]').text()).toContain('days');
         });
     });
--- frontend/src/components/patterns/carousel/Carousel.vue
+++ frontend/src/components/patterns/carousel/Carousel.vue
@@ -149,6 +149,13 @@ export default {
                 display: none;
             }
 
+            &__slide {
+                img {
+                        align-self: flex-start;
+                        max-width: 100%;
+                }
+            }
+
             &__arrow {
                 border-radius: 0;
                 background: $color-pink;
