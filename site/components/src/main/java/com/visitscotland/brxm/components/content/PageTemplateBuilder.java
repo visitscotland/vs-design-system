fbf756e594facaf5030dfb77d5ad010c706dd4e8
jhaddrill <james.haddrill@visitscotland.com>
2021-01-21 11:59:51 +0000

    VS-2015 FIX (PageTEmplateBuilder.java) fix error in file

--- site/components/src/main/java/com/visitscotland/brxm/components/content/PageTemplateBuilder.java
+++ site/components/src/main/java/com/visitscotland/brxm/components/content/PageTemplateBuilder.java
@@ -76,8 +76,6 @@ public class PageTemplateBuilder {
                     }
                     iKnowModule.setHippoBean(item);
                     page.modules.add(iKnowModule);
-                } else if (item instanceof LongContent){
-                    page.modules.add(createLongContent(request, (LongContent) item));
                 } else if (item instanceof Article){
                     page.modules.add(createLongContent(request, (Article) item));
                 }
@@ -174,50 +172,6 @@ public class PageTemplateBuilder {
             FlatLongContentSection flcs = new FlatLongContentSection();
             flcs.setCopy(section.getCopy());
             //TODO Convert MediaItem into image
-           //flcs.setImage(new FlatImage(section.getMediaItem(), request.getLocale()));
-
-            if (section.getMediaItem() instanceof Image) {
-                Image cmsImage = (Image) section.getMediaItem();
-                if (cmsImage != null) {
-                    FlatImage flatImage = new FlatImage(cmsImage,request.getLocale());
-                   flcs.setImage(flatImage);
-                }
-            }
-
-            // TODO Are we going to include Quotes?
-            if (section.getQuote()!= null){
-                flcs.setQuote(section.getQuote().getQuote());
-                flcs.setQuoteAuthorName(section.getQuote().getAuthor());
-                flcs.setQuoteAuthorTitle(section.getQuote().getRole());
-                if (section.getQuote().getImage() != null) {
-                    flcs.setQuoteImage(new FlatImage(section.getQuote().getImage(), request.getLocale()));
-                }
-                //TODO ADD Rethink about CTA
-//                flcs.setQuoteLink(section.getQuote().getProduct());
-            }
-            sections.add(flcs);
-        }
-        module.setSections(sections);
-
-        return module;
-    }
-
-    //TODO convert into factory
-    private Module createLongContent(HstRequest request, LongContent doc){
-        LongContentModule module = new LongContentModule();
-        List<FlatLongContentSection> sections = new ArrayList<>();
-        //TODO add media
-        if (doc.getImage() != null) {
-            module.setImage(new FlatImage(doc.getImage(), Locale.UK));
-        }
-        module.setTitle(doc.getTitle());
-        module.setIntroduction(doc.getIntroduction());
-        module.setHippoBean(doc);
-
-        for (LongContentSection section: doc.getparagraphs()){
-            FlatLongContentSection flcs = new FlatLongContentSection();
-            flcs.setCopy(section.getCopy());
-            //TODO Convert MediaItem into image
             //flcs.setImage(new FlatImage(section.getMediaItem(), request.getLocale()));
 
             if (section.getMediaItem() instanceof Image) {
@@ -245,7 +199,6 @@ public class PageTemplateBuilder {
 
         return module;
     }
-
 
     /**
      * Controls the configuration of the page.
