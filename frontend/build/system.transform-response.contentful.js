7da493b3959ee4b8a413ef01c574541fc769721d
Michael Prosser <michael.prosser@visitscotland.com>
2021-02-22 15:29:54 +0000

    VS-1547 FIX (Lint) Apply auto fixes for lint errors in build folder

--- frontend/build/system.transform-response.contentful.js
+++ frontend/build/system.transform-response.contentful.js
@@ -1,52 +1,52 @@
-const documentToHtmlString = require("@contentful/rich-text-html-renderer").documentToHtmlString
-const _ = require("lodash")
+const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
+const _ = require('lodash');
 
 function transformRawResponse(raw) {
-  const instance = _.get(raw, "items.0")
-  const relatedEntries = _.get(raw, "includes.Entry")
+    const instance = _.get(raw, 'items.0');
+    const relatedEntries = _.get(raw, 'includes.Entry');
 
-  return {
-    title: _getContentfulFieldValue(instance, "title"),
-    sections: _extractContentfulEntrySections(instance, relatedEntries),
-  }
+    return {
+        title: _getContentfulFieldValue(instance, 'title'),
+        sections: _extractContentfulEntrySections(instance, relatedEntries),
+    };
 }
 
 function _getContentfulFieldValue(contentfulEntry, fieldPath) {
-  return _.get(contentfulEntry, "fields." + fieldPath)
+    return _.get(contentfulEntry, `fields.${ fieldPath}`);
 }
 
 function _extractContentfulEntrySections(contentfulEntry, relatedEntries) {
-  return _.filter(
-    _.map(
-      _getContentfulFieldValue(contentfulEntry, "sections"),
-      _.partial(_parseContentfulSection, _, relatedEntries)
-    )
-  )
+    return _.filter(
+        _.map(
+            _getContentfulFieldValue(contentfulEntry, 'sections'),
+            _.partial(_parseContentfulSection, _, relatedEntries)
+        )
+    );
 }
 
 function _parseContentfulSection(sectionLinkObj, relatedEntries) {
-  const section = _.find(
-    relatedEntries,
-    _.matchesProperty("sys.id", _.get(sectionLinkObj, "sys.id"))
-  )
+    const section = _.find(
+        relatedEntries,
+        _.matchesProperty('sys.id', _.get(sectionLinkObj, 'sys.id'))
+    );
 
-  if (!section) {
-    return null
-  }
+    if (!section) {
+        return null;
+    }
 
-  const fields = _.get(section, "fields")
+    const fields = _.get(section, 'fields');
 
-  if (fields.content) {
-    fields.content = documentToHtmlString(fields.content)
-  }
+    if (fields.content) {
+        fields.content = documentToHtmlString(fields.content);
+    }
 
-  if (fields.sections) {
-    fields.sections = _extractContentfulEntrySections(section, relatedEntries)
-  }
+    if (fields.sections) {
+        fields.sections = _extractContentfulEntrySections(section, relatedEntries);
+    }
 
-  return fields
+    return fields;
 }
 
 module.exports = {
-  transformRawResponse,
-}
+    transformRawResponse,
+};
