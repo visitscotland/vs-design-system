54e99f9d13b45da32c4a317dbaa998f8a925fad7
Michael Prosser <michael.prosser@visitscotland.com>
2021-02-22 15:29:54 +0000

    VS-1547 FIX (Lint) Apply auto fixes for lint errors in build folder

--- frontend/build/system.remote.utils.js
+++ frontend/build/system.remote.utils.js
@@ -1,234 +1,236 @@
 #! /usr/bin/env node
-const fs = require("fs")
-const rm = require("rimraf")
-const path = require("path")
+const fs = require('fs');
+const rm = require('rimraf');
+const path = require('path');
 
-const _ = require("lodash")
-const ora = require("ora")
-const chalk = require("chalk")
-const requestPromise = require("request-promise-native")
-const Turndown = require("turndown")
-const turndownTablesPlugin = require("turndown-plugin-gfm").tables
+const _ = require('lodash');
+const ora = require('ora');
+const chalk = require('chalk');
+const requestPromise = require('request-promise-native');
+const Turndown = require('turndown');
+const turndownTablesPlugin = require('turndown-plugin-gfm').tables;
 
-const getConfig = require("vue-styleguidist/scripts/config")
-const StyleguidistError = require("react-styleguidist/lib/scripts/utils/error")
+const getConfig = require('vue-styleguidist/scripts/config');
+const StyleguidistError = require('react-styleguidist/lib/scripts/utils/error');
 
-const KEY_REMOTE_CONFIG = "remoteProfiles"
-const ARG_REMOTE_PROFILE = "remote-profile"
+const KEY_REMOTE_CONFIG = 'remoteProfiles';
+const ARG_REMOTE_PROFILE = 'remote-profile';
 
 const defaultRemoteConfig = {
-  tempPath: "./.tmp/",
-}
+    tempPath: './.tmp/',
+};
 
-const turndownService = _getTurndownService()
+const turndownService = _getTurndownService();
 
 const defaultRequestOptions = {
-  json: true,
-}
+    json: true,
+};
 
-let remoteConfig = null
-let spinner
+let remoteConfig = null;
+let spinner;
 
 function getRemoteConfig(config, argv) {
+    const selectedProfileName = _.get(argv, ARG_REMOTE_PROFILE, process.env.VS_DS_REMOTE_PROFILE);
 
-  const selectedProfileName = _.get(argv, ARG_REMOTE_PROFILE, process.env.VS_DS_REMOTE_PROFILE)
+    remoteConfig = _extractRemoteConfig(config, selectedProfileName);
 
-  remoteConfig = _extractRemoteConfig(config, selectedProfileName)
+    if (!remoteConfig) {
+        return Promise.resolve(getConfig(config));
+    }
 
-  if (!remoteConfig) {
-    return Promise.resolve(getConfig(config))
-  }
+    remoteConfig = _applyRemoteConfigDefaults(remoteConfig);
 
-  remoteConfig = _applyRemoteConfigDefaults(remoteConfig)
+    const requestOptions = _makeRequestOptions(remoteConfig);
 
-  const requestOptions = _makeRequestOptions(remoteConfig)
+    spinner = ora(`Getting remote config from ${ requestOptions.uri }...`);
 
-  spinner = ora("Getting remote config from " + requestOptions.uri + "...")
+    spinner.start();
 
-  spinner.start()
+    return requestPromise(requestOptions)
+        .then(_.partial(_mergeConfig, _, config))
+        .then(_.partial(_processSections, _, _.get(remoteConfig, 'tempPath')))
+        .then(_addPrivateComponents)
+        .then((mergedConfig) => {
+            spinner.stop();
 
-  return requestPromise(requestOptions)
-    .then(_.partial(_mergeConfig, _, config))
-    .then(_.partial(_processSections, _, _.get(remoteConfig, "tempPath")))
-    .then(_addPrivateComponents)
-    .then(function(mergedConfig) {
-      spinner.stop()
+            console.log(chalk.cyan('Remote config merged!'));
 
-      console.log(chalk.cyan("Remote config merged!"))
+            return mergedConfig;
+        })
+        .catch((err) => {
+            spinner.stop();
 
-      return mergedConfig
-    })
-    .catch(function(err) {
-      spinner.stop()
+            console.log(chalk.red(`Problem encountered getting remote config from ${ requestOptions.uri}`));
+            console.log(err);
 
-      console.log(chalk.red("Problem encountered getting remote config from " + requestOptions.uri))
-      console.log(err)
+            // return the original static config on error
+            console.log(chalk.cyan('Ignoring remote config'));
 
-      // return the original static config on error
-      console.log(chalk.cyan("Ignoring remote config"))
+            return config;
 
-      return config
-
-      // throw err
-    })
-    .then(getConfig)
+            // throw err
+        })
+        .then(getConfig);
 }
 
 function cleanup(docsConfig) {
-  const sectionMarkdownFiles = _.map(_.get(docsConfig, "sections"), _makeSectionMarkdownFileName)
+    const sectionMarkdownFiles = _.map(_.get(docsConfig, 'sections'), _makeSectionMarkdownFileName);
 
-  const tempPath = _.get(_applyRemoteConfigDefaults(remoteConfig), "tempPath")
+    const tempPath = _.get(_applyRemoteConfigDefaults(remoteConfig), 'tempPath');
 
-  _.each(sectionMarkdownFiles, function(fileName) {
-    rm(path.join(tempPath, fileName), function(err) {
-      if (err) {
-        console.log(chalk.red("Cleanup: failed to remove temp markdown file", fileName, ":", err))
-      }
-    })
-  })
+    _.each(sectionMarkdownFiles, (fileName) => {
+        rm(path.join(tempPath, fileName), (err) => {
+            if (err) {
+                console.log(chalk.red('Cleanup: failed to remove temp markdown file', fileName, ':', err));
+            }
+        });
+    });
 }
 
 function _makeRequestOptions(remoteConfig) {
-  let options = {
-    uri: _makeRemoteUriFromRemoteConfig(remoteConfig),
-  }
+    const options = {
+        uri: _makeRemoteUriFromRemoteConfig(remoteConfig),
+    };
 
-  return _.defaults(options, _.get(remoteConfig, "requestOptions"), defaultRequestOptions)
+    return _.defaults(options, _.get(remoteConfig, 'requestOptions'), defaultRequestOptions);
 }
 
 function _makeRemoteUriFromRemoteConfig(config) {
-  const uri = _.get(config, "uriBase")
-  let queryString = null
+    const uri = _.get(config, 'uriBase');
+    let queryString = null;
 
-  if (!uri) {
-    throw new StyleguidistError("Remote config is missing uriBase")
-  }
+    if (!uri) {
+        throw new StyleguidistError('Remote config is missing uriBase');
+    }
 
-  queryString = _.join(
-    _.map(_.get(config, "uriParams"), (value, key) => {
-      return key + "=" + value
-    }),
-    "&"
-  )
+    queryString = _.join(
+        _.map(_.get(config, 'uriParams'), (value, key) => `${key }=${ value}`),
+        '&'
+    );
 
-  return uri + (queryString ? "?" + queryString : "")
+    return uri + (queryString ? `?${ queryString}` : '');
 }
 
 function _mergeConfig(remoteConfig, localConfig) {
-  return _.assign({}, localConfig, remoteConfig)
+    return _.assign({
+    }, localConfig, remoteConfig);
 }
 
 function _processSections(parentObject, tempOutputPath) {
-  const sections = _.get(parentObject, "sections")
+    const sections = _.get(parentObject, 'sections');
 
-  if (!sections) {
-    return parentObject
-  }
+    if (!sections) {
+        return parentObject;
+    }
 
-  _prepOutputDir(tempOutputPath)
+    _prepOutputDir(tempOutputPath);
 
-  parentObject.sections = _.map(sections, _.partial(_processSection, _, tempOutputPath))
+    parentObject.sections = _.map(sections, _.partial(_processSection, _, tempOutputPath));
 
-  return parentObject
+    return parentObject;
 }
 
 function _processSection(section, tempOutputPath) {
-  // process child sections first
-  section = _processSections(section, tempOutputPath)
+    // process child sections first
+    section = _processSections(section, tempOutputPath);
 
-  return _.merge({}, section, {
-    content: _processSectionContent(section, tempOutputPath),
-  })
+    return _.merge({
+    }, section, {
+        content: _processSectionContent(section, tempOutputPath),
+    });
 }
 
 function _processSectionContent(section, tempOutputPath) {
-  const outputMarkdownPath = tempOutputPath + _makeSectionMarkdownFileName(section)
-  const htmlContent = _.get(section, "content")
-  const markdownContent = htmlContent ? turndownService.turndown(htmlContent) : ""
+    const outputMarkdownPath = tempOutputPath + _makeSectionMarkdownFileName(section);
+    const htmlContent = _.get(section, 'content');
+    const markdownContent = htmlContent ? turndownService.turndown(htmlContent) : '';
 
-  fs.writeFileSync(outputMarkdownPath, markdownContent, "utf8")
+    fs.writeFileSync(outputMarkdownPath, markdownContent, 'utf8');
 
-  return outputMarkdownPath
+    return outputMarkdownPath;
 }
 
 function _makeSectionMarkdownFileName(section) {
-  return _.kebabCase(_.get(section, "name")) + ".md"
+    return `${_.kebabCase(_.get(section, 'name')) }.md`;
 }
 
 function _getTurndownService() {
-  const turndownService = new Turndown()
+    const turndownService = new Turndown();
 
-  turndownService.use(turndownTablesPlugin)
+    turndownService.use(turndownTablesPlugin);
 
-  turndownService.addRule("transform-code", {
-    filter: ["code", "pre"],
-    replacement(content) {
-      return "```\n" + content + "\n```"
-    },
-  })
+    turndownService.addRule('transform-code', {
+        filter: ['code', 'pre'],
+        replacement(content) {
+            return `\`\`\`\n${ content }\n\`\`\``;
+        },
+    });
 
-  return turndownService
+    return turndownService;
 }
 
 function _prepOutputDir(tempOutputPath) {
-  if (!fs.existsSync(tempOutputPath)) {
-    fs.mkdirSync(tempOutputPath, { recursive: true })
-  }
+    if (!fs.existsSync(tempOutputPath)) {
+        fs.mkdirSync(tempOutputPath, {
+            recursive: true,
+        });
+    }
 }
 
 function _applyRemoteConfigDefaults(remoteConfig) {
-  return _.defaultsDeep({}, remoteConfig, defaultRemoteConfig)
+    return _.defaultsDeep({
+    }, remoteConfig, defaultRemoteConfig);
 }
 
 function _addPrivateComponents(mergedConfig) {
-  if (!mergedConfig.sections || !_.isArray(mergedConfig.sections)) {
-    mergedConfig.sections = []
-  }
+    if (!mergedConfig.sections || !_.isArray(mergedConfig.sections)) {
+        mergedConfig.sections = [];
+    }
 
-  mergedConfig.sections.push({
-    name: "Private Components",
-    exampleMode: "hide",
-    usageMode: "hide",
-    components: ["./src/**/*.vue", "./docs/components/**/*.vue"],
-  })
+    mergedConfig.sections.push({
+        name: 'Private Components',
+        exampleMode: 'hide',
+        usageMode: 'hide',
+        components: ['./src/**/*.vue', './docs/components/**/*.vue'],
+    });
 
-  return mergedConfig
+    return mergedConfig;
 }
 
 function _extractRemoteConfig(config, profileName) {
-  const remoteConfig = _.get(config, KEY_REMOTE_CONFIG)
+    const remoteConfig = _.get(config, KEY_REMOTE_CONFIG);
 
-  _.unset(config, KEY_REMOTE_CONFIG)
+    _.unset(config, KEY_REMOTE_CONFIG);
 
-  if(_.isEmpty(remoteConfig)) {
-    console.log(
-      chalk.red("No remote config defined")
-    )
+    if (_.isEmpty(remoteConfig)) {
+        console.log(
+            chalk.red('No remote config defined')
+        );
 
-    return null
-  }
+        return null;
+    }
 
-  if (!profileName) {
-    profileName = _.first(_.keys(remoteConfig))
-    console.log(chalk.red("No profile chosen, falling back to first profile in config"))
-  } else if (!_.has(remoteConfig, profileName)) {
-    console.log(
-      chalk.red(
+    if (!profileName) {
+        profileName = _.first(_.keys(remoteConfig));
+        console.log(chalk.red('No profile chosen, falling back to first profile in config'));
+    } else if (!_.has(remoteConfig, profileName)) {
+        console.log(
+            chalk.red(
         `Profile ${profileName} not included in remote config - 
         add at least one profile to the "remoteProfile" key of the config. 
         Falling back to first profile in config`
-      )
-    )
+            )
+        );
 
-    profileName = _.first(_.keys(remoteConfig))
-  }
+        profileName = _.first(_.keys(remoteConfig));
+    }
 
-  console.log(chalk.cyan("Selected remote profile - " + profileName + "."))
+    console.log(chalk.cyan(`Selected remote profile - ${ profileName }.`));
 
-  return _.get(remoteConfig, profileName)
+    return _.get(remoteConfig, profileName);
 }
 
 module.exports = {
-  getRemoteConfig,
-  cleanup,
-}
+    getRemoteConfig,
+    cleanup,
+};
