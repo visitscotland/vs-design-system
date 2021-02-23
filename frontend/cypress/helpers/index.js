795a0b13a91cbeaa5eae2e0827e201ca5d73e844
Michael Prosser <michael.prosser@visitscotland.com>
2021-02-23 10:10:13 +0000

    VS-1547 FIX (Lint) Apply cypress lint fixes

--- frontend/cypress/helpers/index.js
+++ frontend/cypress/helpers/index.js
@@ -1,9 +1,5 @@
-// const vueHelpers = require("./vue").default
-// const { getToken, getThemeColours }
-
-export const vueHelper = require('./vue').default;
-
-export const { getToken, getThemeColours } = require('./tokens').default;
+const vueHelper = require('./vue').default;
+const { getToken, getThemeColours } = require('./tokens').default;
 
 export default {
     getToken,
