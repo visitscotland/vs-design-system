b781e74352af12bb9a500bdcf770a274129399c3
jhaddrill <james.haddrill@visitscotland.com>
2021-03-08 12:01:05 +0000

    VS-2308 FEAT (Carousel component) updates to carousel code

--- frontend/src/components/patterns/carousel/components/CarouselSlide.vue
+++ frontend/src/components/patterns/carousel/components/CarouselSlide.vue
@@ -1,40 +1,47 @@
 <template>
-    <SplideSlide>
-        <VsStretchedLinkCard
-            :link="linkUrl"
-            :type="linkType"
-            :img-src="imgSrc"
-            :img-alt="imgAlt"
-            class="vs-carousel-slide"
-        >
-            <VsStretchedLinkPanels
-                v-if="days && transport"
-                :days="days"
-                :transport="transport"
-                slot="stretchedCardPanels"
-                :days-label="daysLabel"
-            />
-
-            <template slot="stretchedCardCategory">
-                {{ category }}
-            </template>
-
-            <span
-                slot="stretchedCardHeader"
-                class="vs-carousel-slide__title"
-                data-test="vs-carousel-slide__title"
+    <VsCol
+        :cols="slideCols.xs"
+        :sm="slideCols.sm"
+        :lg="slideCols.md"
+        :xl="slideCols.lg"
+    >
+        <div class="vs-carousel-slide">
+            <VsStretchedLinkCard
+                :link="linkUrl"
+                :type="linkType"
+                :img-src="imgSrc"
+                :img-alt="imgAlt"
+                class="vs-carousel-slide__card"
             >
-                <!-- @slot Slot to contain heading -->
-                <slot name="vsCarouselSlideHeading" />
-            </span>
-        </VsStretchedLinkCard>
-    </SplideSlide>
+                <VsStretchedLinkPanels
+                    v-if="days && transport"
+                    :days="days"
+                    :transport="transport"
+                    slot="stretchedCardPanels"
+                    :days-label="daysLabel"
+                />
+
+                <template slot="stretchedCardCategory">
+                    {{ category }}
+                </template>
+
+                <span
+                    slot="stretchedCardHeader"
+                    class="vs-carousel-slide__title"
+                    data-test="vs-carousel-slide__title"
+                >
+                    <!-- @slot Slot to contain heading -->
+                    <slot name="vsCarouselSlideHeading" />
+                </span>
+            </VsStretchedLinkCard>
+        </div>
+    </VsCol>
 </template>
 
 <script>
-import { SplideSlide } from '@splidejs/vue-splide';
 import VsStretchedLinkCard from '@components/elements/stretched-link-card/StretchedLinkCard';
 import VsStretchedLinkPanels from '@components/elements/stretched-link-card/components/StretchedLinkPanels';
+import { VsCol } from '@components/elements/layout';
 
 /**
 * Slide for carousel
@@ -47,9 +54,9 @@ export default {
     status: 'prototype',
     release: '0.0.1',
     components: {
-        SplideSlide,
         VsStretchedLinkCard,
         VsStretchedLinkPanels,
+        VsCol,
     },
     props: {
         /**
@@ -111,22 +118,31 @@ export default {
             default: '',
         },
     },
+    data() {
+        return {
+            parentData: this.$parent.slidesPerPage,
+        };
+    },
+    inject: ['slideCols'],
 };
 </script>
 
 <style lang="scss">
-    .splide__slide {
-        opacity: 0.5;
-        transition: opacity 0.2s ease;
-
-        &.is-visible {
-            opacity: 1;
-        }
-    }
-
     .vs-carousel-slide {
-        height: 100%;
-        padding: $spacer-2 $spacer-2 $spacer-6;
+        max-width: 100%;
+        flex-shrink: 0;
+
+        .vs-carousel-slide__card {
+            opacity: 0.5;
+            transition: opacity 0.3s ease;
+            transition-delay: 0.6s;
+            padding: $spacer-2;
+            max-width: 100%;
+
+            &--active {
+                opacity: 1;
+            }
+        }
 
         .card-title {
             margin-bottom: $spacer-0;
@@ -138,6 +154,133 @@ export default {
 
             .vs-heading__sub-heading {
                 display: none;
+            }
+        }
+    }
+
+    .no-js .vs-carousel-slide {
+        .vs-stretched-link-card {
+            opacity: 1;
+            display: flex;
+            flex-direction: row;
+            padding: $spacer-2;
+            border: none;
+            height: 100%;
+            transition: box-shadow 800ms;
+
+            &:hover {
+                box-shadow: 10px 10px 20px $color-gray-tint-4;
+
+                .vs-stretched-link-card__title {
+                    text-decoration: underline;
+                }
+            }
+
+            .stretched-link {
+                text-decoration: none;
+            }
+
+            .card-body {
+                background: none;
+                padding: 0;
+                align-self: flex-start;
+                width: 66%;
+                text-align: left;
+            }
+
+            &__img {
+                width: 33%;
+                align-self: flex-start;
+                margin-right: $spacer-4;
+            }
+
+            &__title {
+                font-size: $font-size-sm;
+                letter-spacing: .05rem;
+                line-height: $line-height-m;
+                color: $color-base-text;
+                text-decoration: none;
+            }
+
+            &__category {
+                margin-bottom: $spacer-2;
+            }
+
+            .stretched-link {
+                letter-spacing: 0;
+            }
+
+            .card-title {
+                display: flex;
+                margin-bottom: 0;
+            }
+
+            &__content {
+                display: none;
+
+                p {
+                    display: -webkit-box;
+                    -webkit-line-clamp: 3;
+                    -webkit-box-orient: vertical;
+                    overflow: hidden;
+                }
+            }
+
+            &::after {
+                content: '';
+                position: absolute;
+                left: $spacer-2;
+                bottom: 0;
+                height: 1px;
+                width: calc(100% - 16px);
+                background: $color-gray-tint-5;
+                display: inline-block !important;
+            }
+
+            .vs-stretched-link-panels {
+                right: auto;
+                left: 33%;
+                transform: translateX(-100%);
+
+                &__panel {
+                    display: none;
+                    width: 36px;
+                    height: 36px;
+
+                    &--days {
+                        display: flex;
+                    }
+                }
+
+                 &__days {
+                     font-size: $font-size-base;
+                     margin-bottom: 0;
+                 }
+            }
+        }
+
+        @include media-breakpoint-up(sm) {
+            .vs-stretched-link-card {
+                &__content {
+                    display: block;
+                }
+            }
+        }
+
+        @include media-breakpoint-up(lg) {
+            .vs-stretched-link-card {
+                &__title {
+                    font-size: $small-font-size;
+                }
+
+                &__content {
+                    margin: $spacer-2 0 0;
+                    line-height: $line-height-s;
+                }
+            }
+
+            .vs-carousel-slide {
+                width: calc(50% - 24px) !important;
             }
         }
     }
