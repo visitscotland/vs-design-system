dffcdef5f8dec13563ef02730ae02d9b47311a74
jhaddrill <james.haddrill@visitscotland.com>
2021-03-15 09:26:49 +0000

    VS-2308 FEAT (Carousel) update carousel tests

--- frontend/src/components/patterns/carousel/__tests__/carousel.jest.spec.js
+++ frontend/src/components/patterns/carousel/__tests__/carousel.jest.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
 import VsCarousel from '../Carousel';
 
-const splide = {
-    index: 8,
-    length: 10,
-};
-
-const factoryShallowMount = (slotsData) => shallowMount(VsCarousel, {
+const factoryShallowMount = (slotsData, propsData) => shallowMount(VsCarousel, {
+    propsData: {
+        ...propsData,
+    },
     data() {
         return {
             currentSlide: 3,
@@ -15,31 +13,68 @@ const factoryShallowMount = (slotsData) => shallowMount(VsCarousel, {
     ...slotsData,
 });
 
+/* eslint-disable */
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {}
+    };
+};
+/* eslint-enable */
+
 describe('VsCarousel', () => {
-    it('should change the currentSlide prop when the currentSlide method is called', () => {
-        const wrapper = factoryShallowMount();
-        wrapper.vm.carouselMoved(splide);
-        expect(wrapper.vm.currentSlide).toBe(splide.index + 1);
-    });
+    describe(':slots', () => {
+        it('renders the mobile pagination correctly', async() => {
+            const wrapper = factoryShallowMount({
+                slots: {
+                    vsCarouselOf: 'of',
+                },
+            });
 
-    it('should change the totalSlides prop when the initialiseCustomOptions is called', () => {
-        const wrapper = factoryShallowMount();
-        wrapper.vm.initialiseCustomOptions(splide);
-        expect(wrapper.vm.totalSlides).toBe(splide.length);
-    });
+            await wrapper.setData({
+                currentPage: 1,
+                totalSlides: 10,
+                currentWidth: 'lg',
+            });
 
-    it('renders the mobile pagination correctly', async() => {
-        const wrapper = factoryShallowMount({
-            slots: {
-                vsCarouselOf: 'of',
-            },
+            const pagintationText = wrapper.find('[data-test="vs-carousel__mobile-pagination"]').text();
+
+            const formattedPagination = pagintationText.replace(/\s\s+/g, ' ');
+
+            expect(formattedPagination).toBe('2 of 10');
         });
+    });
+
+    it('only renders the previous control if the prop is set to true', async() => {
+        const wrapper = factoryShallowMount();
 
         await wrapper.setData({
-            currentSlide: 4,
-            totalSlides: 10,
+            prevDisabled: false,
         });
 
-        expect(wrapper.find('[data-test="vs-carousel__mobile-pagination"]').text()).toBe('4 of 10');
+        expect(wrapper.find('.vs-carousel__control--prev').exists()).toBe(true);
+
+        await wrapper.setData({
+            prevDisabled: true,
+        });
+
+        expect(wrapper.find('.vs-carousel__control--prev').exists()).toBe(false);
+    });
+
+    it('only renders the next control if the prop is set to true', async() => {
+        const wrapper = factoryShallowMount();
+
+        await wrapper.setData({
+            nextDisabled: false,
+        });
+
+        expect(wrapper.find('.vs-carousel__control--next').exists()).toBe(true);
+
+        await wrapper.setData({
+            nextDisabled: true,
+        });
+
+        expect(wrapper.find('.vs-carousel__control--next').exists()).toBe(false);
     });
 });
