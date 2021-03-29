7a3e92230f9f18d89945a7c4d4f67ced8be4e95e
Michael Prosser <michael.prosser@visitscotland.com>
2021-03-29 17:03:47 +0100

    VS-1415 FEAT (Address) Set up folder structure for address component

---
+++ frontend/src/components/patterns/address/Address.vue
@@ -0,0 +1,35 @@
+<template>
+    <div
+        class="vs-address"
+        data-test="vs-address"
+    >
+        Address
+    </div>
+</template>
+
+<script>
+
+/**
+ * Address
+ *
+ * @displayName Address
+ */
+export default {
+    name: 'VsAddress',
+    props: {
+    },
+};
+</script>
+
+<style lang="scss">
+</style>
+
+<docs>
+  ```js
+
+    <h3>Basic Address</h3>
+    <VsAddress>
+    </VsAddress>
+
+  ```
+</docs>
