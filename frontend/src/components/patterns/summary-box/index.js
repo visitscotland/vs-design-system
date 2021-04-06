ea86fed0cf644fe82a23f7d2994c1674a5b42058
Michael Prosser <michael.prosser@visitscotland.com>
2021-04-06 15:55:06 +0100

    VS-1026 FIX (SummaryBox) Refactor - merge the distance display and distance label components into one list item

--- frontend/src/components/patterns/summary-box/index.js
+++ frontend/src/components/patterns/summary-box/index.js
@@ -1,5 +1,3 @@
-import VsSummaryBoxDistanceDisplay from './components/SummaryBoxDistanceDisplay';
-import VsSummaryBoxDistanceLabel from './components/SummaryBoxDistanceLabel';
 import VsSummaryBoxIconWithLabel from './components/SummaryBoxIconWithLabel';
 import VsSummaryBoxLabel from './components/SummaryBoxLabel';
 import VsSummaryBoxList from './SummaryBoxList';
@@ -7,11 +5,10 @@ import VsSummaryBoxListItem from './components/SummaryBoxListItem';
 import VsSummaryBoxDisplay from './components/SummaryBoxDisplay';
 
 export {
-    VsSummaryBoxDistanceDisplay,
-    VsSummaryBoxDistanceLabel,
     VsSummaryBoxIconWithLabel,
     VsSummaryBoxLabel,
     VsSummaryBoxList,
     VsSummaryBoxListItem,
+    VsSummaryBoxDistanceListItem,
     VsSummaryBoxDisplay,
 };
