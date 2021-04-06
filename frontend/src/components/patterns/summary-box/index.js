47ca0ec11c62748b78612c06d5670d0bba26b1ab
Michael Prosser <michael.prosser@visitscotland.com>
2021-04-06 16:00:19 +0100

    VS-1026 FIX (SummaryBox) Refactor - remove store now distance elements are all in one component

--- frontend/src/components/patterns/summary-box/index.js
+++ frontend/src/components/patterns/summary-box/index.js
@@ -3,12 +3,13 @@ import VsSummaryBoxLabel from './components/SummaryBoxLabel';
 import VsSummaryBoxList from './SummaryBoxList';
 import VsSummaryBoxListItem from './components/SummaryBoxListItem';
 import VsSummaryBoxDisplay from './components/SummaryBoxDisplay';
+import VsSummaryBoxDistanceListItem from './components/SummaryBoxDistanceListItem';
 
 export {
     VsSummaryBoxIconWithLabel,
     VsSummaryBoxLabel,
     VsSummaryBoxList,
     VsSummaryBoxListItem,
-    VsSummaryBoxDistanceListItem,
     VsSummaryBoxDisplay,
+    VsSummaryBoxDistanceListItem,
 };
