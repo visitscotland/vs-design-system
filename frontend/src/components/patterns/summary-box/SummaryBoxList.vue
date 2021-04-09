11b425d5eb85aca0495514a4487448e72d94ab28
Michael Prosser <michael.prosser@visitscotland.com>
2021-04-09 17:01:01 +0100

    VS-1026 FIX (SummaryBox) Bring component up to modern standards

--- frontend/src/components/patterns/summary-box/SummaryBoxList.vue
+++ frontend/src/components/patterns/summary-box/SummaryBoxList.vue
@@ -1,9 +1,11 @@
 <template>
     <div
-        class="vs-summary-box__wrapper"
+        class="vs-summary-box"
         data-test="vs-summary-box-list"
     >
         <ul class="vs-summary-box__list list-unstyled d-flex flex-wrap p-2 p-sm-3">
+            <!-- Default slot, should load in a set of 4 SummaryBoxListItem and
+            SummaryBoxDistanceListItem elements -->
             <slot />
         </ul>
     </div>
@@ -19,29 +21,28 @@ export default {
     name: 'VsSummaryBoxList',
     status: 'prototype',
     release: '0.0.1',
-    components: {
-    },
-    props: {
-    },
 };
 </script>
 
 <style lang="scss">
-.vs-summary-box__list {
-    background-color: $color-yellow;
-    width: 350px;
-    @include media-breakpoint-up(lg) {
-        width: 400px;
-    }
-}
-.vs-summary-box__wrapper {
+.vs-summary-box {
     display: flex;
     justify-content: center;
+
     @include media-breakpoint-up(lg) {
         position: absolute;
         top: 0;
         width: calc(100% - #{$spacer-8});
         z-index: 1000;
+    }
+}
+
+.vs-summary-box__list {
+    background-color: $color-yellow;
+    width: 350px;
+
+    @include media-breakpoint-up(lg) {
+        width: 400px;
     }
 }
 </style>
