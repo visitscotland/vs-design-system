cad698cc4a7ef2967c09993534d3aada3888ccf4
Jose Calcines <jose.calcines@visitscotland.com>
2021-04-28 11:08:53 +0100

    VS-2502 (DMS) Fixed static analysis issues related to DMSDataService

--- site/components/src/main/java/com/visitscotland/brxm/dms/DMSConstants.java
+++ site/components/src/main/java/com/visitscotland/brxm/dms/DMSConstants.java
@@ -88,10 +88,9 @@ public class DMSConstants {
     }
 
     //Endpoints
-    //TODO Remove %s at the beginning of the constants
     public static final String PRODUCT_SEARCH = "/info/%s/search-results?";
     public static final String PRODUCT_SEARCH_DATA_MAP = "/data/product-search/map?";
-    public static final String VS_DMS_PRODUCT_CARD = "%s/data/products/card?";
+    public static final String VS_DMS_PRODUCT_CARD = "/data/products/card?";
     public static final String META_PRODUCT_LIST = "/data/meta/%s/list?%s";
     public static final String META_LOCATIONS = "/data/meta/location/list?full";
 
