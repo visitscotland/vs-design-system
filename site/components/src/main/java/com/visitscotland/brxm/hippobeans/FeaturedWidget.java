6f948da8c3547aba09c6448be70c5cf88a95e16d
Jose Calcines <jose.calcines@visitscotland.com>
2021-05-14 17:29:12 +0100

    VS-2144 FEAT (Navigation) Change MegaLinks Item to CMSLink

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/FeaturedWidget.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/FeaturedWidget.java
@@ -1,6 +1,8 @@
 package com.visitscotland.brxm.hippobeans;
 
+import com.visitscotland.brxm.hippobeans.capabilities.Linkable;
 import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 
 import java.util.List;
@@ -13,7 +15,7 @@ import java.util.List;
 public class FeaturedWidget extends Widget {
 
     @HippoEssentialsGenerated(internalName = "visitscotland:items", allowModifications = false)
-    public List<MegalinkItem> getItems() {
-        return getLinkedBeans("visitscotland:items", MegalinkItem.class);
+    public <T extends HippoBean, Linkable> List<T> getItems() {
+        return (List<T>) getLinkedBeans("visitscotland:items", HippoBean.class);
     }
 }
