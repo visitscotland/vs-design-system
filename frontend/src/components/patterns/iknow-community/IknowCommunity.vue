a086d79dcf7238d0cce4e4e7cf28283ae8364a37
Michael Prosser <michael.prosser@visitscotland.com>
2021-05-26 16:07:35 +0100

    VS-1693 FEAT (iknow community) Add framework of iknow community component

---
+++ frontend/src/components/patterns/iknow-community/IknowCommunity.vue
@@ -0,0 +1,36 @@
+<template>
+    <div
+        class="vs-iknow-community"
+        data-test="vs-iknow-community"
+    >
+        <h2>iKnow Community</h2>
+    </div>
+</template>
+
+<script>
+
+/**
+ * Filler
+ *
+ * @displayName iKnow Community
+ */
+export default {
+    name: 'VsIknowCommunity',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+    },
+};
+</script>
+
+<style lang="scss">
+.vs-iknow {
+}
+</style>
+
+<docs>
+```jsx
+    <VsIknowCommunity>
+    </VsIknowCommunity>
+```
+</docs>
