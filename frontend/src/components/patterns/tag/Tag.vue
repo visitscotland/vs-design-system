2824506902f364bf4f8fabf4d1519f18112731fd
Michael Prosser <michael.prosser@visitscotland.com>
2021-05-27 10:45:03 +0100

    VS-1693 FEAT (iknow community) Implement tag component

---
+++ frontend/src/components/patterns/tag/Tag.vue
@@ -0,0 +1,75 @@
+<template>
+    <div
+        class="vs-tag mr-2"
+    >
+        <VsLink
+            :href="href"
+        >
+            <!-- @slot Holds the tag text (text expected) -->
+            <slot />
+        </VsLink>
+    </div>
+</template>
+
+<script>
+import VsLink from '@components/elements/link/Link';
+
+/**
+ * A styled tag with a link
+ *
+ * @displayName Tag
+ */
+export default {
+    name: 'VsTag',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        VsLink,
+    },
+    props: {
+    },
+};
+</script>
+
+<style lang="scss">
+.vs-tag {
+    display: inline-block;;
+    border: 2px solid $color-pink;
+    border-radius: $border-radius-pill;
+    transition: $transition-base;
+
+    .vs-link {
+        display: block;
+        text-decoration: none;
+        padding: $spacer-1 $spacer-4;
+    }
+
+    &:hover {
+        background-color: $color-pink;
+
+        .vs-link {
+            color: $color-white;
+        }
+    }
+}
+</style>
+
+<docs>
+```jsx
+    <VsTag
+        href="#"
+    >
+        Activities
+    </VsTag>
+    <VsTag
+        href="#"
+    >
+        Castles
+    </VsTag>
+    <VsTag
+        href="#"
+    >
+        Things to do
+    </VsTag>
+```
+</docs>
