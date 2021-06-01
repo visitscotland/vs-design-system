2569b4dcdaed7cb656acb82d7bcd3eb7ff38fb41
jhaddrill <james.haddrill@visitscotland.com>
2021-06-01 12:30:39 +0100

    VS-2584 FEAT (MegaNavFeaturedItem) add slots and styles

--- frontend/src/components/patterns/header/components/mega-nav/components/MegaNavFeaturedItem.vue
+++ frontend/src/components/patterns/header/components/mega-nav/components/MegaNavFeaturedItem.vue
@@ -1,19 +1,21 @@
 <template>
     <VsStretchedLinkCard
         link="https://visitscotland.com"
-        type="external"
+        type="internal"
         img-src="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
         img-alt="This is the alt text"
         class="vs-mega-nav-featured-item"
     >
         <template slot="stretchedCardHeader">
-            A Title Would Go Here
+            <slot name="vs-featured-item-header" />
         </template>
 
         <template slot="stretchedCardContent">
-            <p>The content for the card goes here</p>
+            <slot name="vs-featured-item-content" />
+        </template>
 
-            <p>A second line of content</p>
+        <template slot="stretchedCardLink">
+            <slot name="vs-featured-item-link" />
         </template>
     </VsStretchedLinkCard>
 </template>
@@ -49,6 +51,54 @@ export default {
 
         .card-title {
             font-size: $small-font-size;
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+        }
+
+        .vs-stretched-link-card__content {
+            display: none;
+        }
+
+        @include media-breakpoint-up(md) {
+            display: flex;
+            flex-direction: row-reverse;
+
+            .card-body {
+                position: initial;
+                width: auto;
+                padding: $spacer-8;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+
+            .vs-stretched-link-card__img {
+                width: 245px;
+                height: auto;
+            }
+        }
+
+        @include media-breakpoint-up(lg) {
+            flex-direction: column;
+
+            .card-body {
+                padding: $spacer-5 $spacer-3;
+            }
+
+            .vs-stretched-link-card__img {
+                width: 100%;
+            }
+
+            .vs-stretched-link-card__content {
+                display: block;
+                margin: $spacer-0;
+            }
+
+            .vs-stretched-link-card__link {
+                align-self: flex-end;
+            }
         }
     }
 </style>
@@ -57,8 +107,21 @@ export default {
     ```jsx
         <VsContainer>
             <VsRow>
-                <VsCol md="4">
-                    <VsMegaNavFeaturedItem />
+                <VsCol cols="12" lg="4">
+                    <VsMegaNavFeaturedItem>
+                        <template slot="vs-featured-item-header">
+                            From our home to yours – see Scotland virtually
+                        </template>
+
+                        <template slot="vs-featured-item-content">
+                            <p>The content for the card goes here</p>
+                            <p>A second line of content</p>
+                        </template>
+
+                        <template slot="vs-featured-item-link">
+                            A link to a page
+                        </template>
+                    </VsMegaNavFeaturedItem>
                 </VsCol>
             </VsRow>
         </VsContainer>
