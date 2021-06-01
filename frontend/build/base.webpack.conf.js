73e6ebe287832bbd774a82b119ea96c2834c5115
Michael Prosser <michael.prosser@visitscotland.com>
2021-06-01 17:16:25 +0100

    VS-2620 FIX (iknow community) Inline svg styles to prevent class name collisions

--- frontend/build/base.webpack.conf.js
+++ frontend/build/base.webpack.conf.js
@@ -87,6 +87,11 @@ module.exports = {
                                     {
                                         removeViewBox: false,
                                     },
+                                    {
+                                        inlineStyles: {
+                                            onlyMatchedOnce: false,
+                                        },
+                                    },
                                 ],
                             },
                         },
