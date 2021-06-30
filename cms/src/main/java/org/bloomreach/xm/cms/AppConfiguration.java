48e1f364bcca8a96d5152edff489c6edfe0bdf01
Jose Calcines <jose.calcines@visitscotland.com>
2021-06-30 16:18:58 +0100

    VS-2663 (BrXM Upgrade) (Console) Configure SpringBoot Actuator

---
+++ cms/src/main/java/org/bloomreach/xm/cms/AppConfiguration.java
@@ -0,0 +1,11 @@
+package org.bloomreach.xm.cms;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages = {"com.visitscotland.brxm.cms.config"} )
+public class AppConfiguration {
+
+}
