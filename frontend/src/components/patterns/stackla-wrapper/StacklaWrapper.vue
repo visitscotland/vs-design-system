4f7cc693c292de0e1b736a5f14b82100cf9f268c
Michael Prosser <michael.prosser@visitscotland.com>
2021-07-08 12:01:41 +0100

    VS-2683 FEAT (Stackla) - Integrate stackla widget into vue component to handle no-js and no-cookies

---
+++ frontend/src/components/patterns/stackla-wrapper/StacklaWrapper.vue
@@ -0,0 +1,61 @@
+<template>
+    <div
+        data-test="vs-stackla"
+    >
+        <VsContainer>
+            <VsRow>
+                <VsCol>
+                    <!--
+                        @slot Takes the dom element for the stackla widget, the script should be
+                        loaded separately further down the page as if it is passed into the vue
+                        template it will not be executed
+                    -->
+                    <slot name="stacklaWidget" />
+                </VsCol>
+            </VsRow>
+        </VsContainer>
+    </div>
+</template>
+
+<script>
+import VsContainer from '@components/elements/layout/Container';
+import VsCol from '@components/elements/layout/Col';
+import VsRow from '@components/elements/layout/Row';
+
+/**
+ * This component acts as a wrapper for a stackla widget embed and provides
+ * no-js and no-cookies functionality for that widget
+ *
+ * @displayName Stackla Wrapper
+ */
+export default {
+    name: 'VsStacklaWrapper',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        VsContainer,
+        VsRow,
+        VsCol,
+    },
+};
+</script>
+
+<docs>
+```jsx
+    <VsModuleWrapper>
+        <template slot="vsModuleWrapperHeading">
+            Your Pictures Of Scottish Castles
+        </template>
+
+        <template slot="vsModuleWrapperIntro">
+            Share your snaps with us by using #ScottishCastle or #VisitScotland
+        </template>
+
+        <VsStacklaWrapper>
+            <template slot="stacklaWidget">
+                Stackla Script Goes Here
+            </template>
+        </VsStacklaWrapper>
+    </VsModuleWrapper>
+```
+</docs>
