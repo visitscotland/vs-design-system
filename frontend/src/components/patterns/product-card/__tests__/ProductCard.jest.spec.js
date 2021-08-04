b87ac34f85f82893febcac65afc9db1b71a1f0de
Michael Prosser <michael.prosser@visitscotland.com>
2021-08-04 16:58:57 +0100

    VS-2719 FEAT (Canned search) - Correct lint errors in tests

--- frontend/src/components/patterns/product-card/__tests__/ProductCard.jest.spec.js
+++ frontend/src/components/patterns/product-card/__tests__/ProductCard.jest.spec.js
@@ -1,83 +1,84 @@
 import { mount } from '@vue/test-utils';
 import VsProductCard from '../ProductCard';
 
-const imgSrc = 'https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm',
-    imgAlt = 'Arden House',
-    title = 'Arden House',
-    location = 'Callander, Loch Lomond, The Trossachs',
-    stars = {
-        min: 4,
-        max: 4,
-        gold: false
+const imgSrc = 'https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm';
+const imgAlt = 'Arden House';
+const title = 'Arden House';
+const location = 'Callander, Loch Lomond, The Trossachs';
+const stars = {
+    min: 4,
+    max: 4,
+    gold: false,
+};
+const categories = ['Rural', 'Village Location', 'Mountains area'];
+const description = 'Test product description';
+const detailLink = {
+    link: '/info/accommodation/arden-house-p572561',
+    label: 'View Details',
+    type: 'internal',
+};
+const websiteLink = {
+    link: 'http://www.ardenhouse.org.uk',
+    label: 'Visit website',
+    type: 'external',
+};
+const priceIntro = 'Price from';
+const price = '£110';
+const priceOutro = 'Per room per night (breakfast inc.)';
+const badgeOne = 'B and B';
+const badgeTwo = 'Offer';
+const badgeThree = 'We are open';
+const goodToGoLogo = 'Good to go';
+const safeTravelsLogo = 'Safe travels';
+const awards = [
+    {
+        id: 'qatasteourbest',
+        name: 'Taste Our Best',
+        image: 'http://visitscotlandpreview.nmdemo.net/wsimgs/awards/TOB-updated-white-back_286148508_714060445.jpg',
     },
-    categories = ["Rural", "Village Location", "Mountains area"],
-    description = "Test product description",
-    detailLink = {
-        link: '/info/accommodation/arden-house-p572561',
-        label: 'View Details',
-        type: 'internal'
+    {
+        id: 'qag2s',
+        name: 'Green Tourism Silver',
+        image: 'http://visitscotlandpreview.nmdemo.net/wsimgs/awards/GT_silver_200x200_275519812.png',
+        type: 'GREEN_TOURISM',
     },
-    websiteLink = {
-        link: "http://www.ardenhouse.org.uk",
-        label: 'Visit website',
-        type: 'external'
-    },
-    priceIntro = "Price from",
-    price = "£110",
-    priceOutro = "Per room per night (breakfast inc.)",
-    badgeOne = "B and B",
-    badgeTwo = "Offer",
-    badgeThree = "We are open",
-    goodToGoLogo = "Good to go",
-    safeTravelsLogo = "Safe travels",
-    awards = [
-        {
-           "id":"qatasteourbest",
-           "name":"Taste Our Best",
-           "image":"http://visitscotlandpreview.nmdemo.net/wsimgs/awards/TOB-updated-white-back_286148508_714060445.jpg"
-        },
-        {
-          "id": "qag2s",
-          "name": "Green Tourism Silver",
-          "image": "http://visitscotlandpreview.nmdemo.net/wsimgs/awards/GT_silver_200x200_275519812.png",
-          "type": "GREEN_TOURISM"
-        }
-    ];
+];
 
 const factoryMount = (propsData) => mount(VsProductCard, {
     propsData: {
-        imgSrc: imgSrc,
-        imgAlt: imgAlt,
-        title: title,
-        location: location, 
-        stars: stars,
-        categories: categories,
-        description: description,
-        detailLink: detailLink,
-        websiteLink: websiteLink,
-        priceIntro: priceIntro,
-        price: price,
-        priceOutro: priceOutro,
-        badgeOne: badgeOne,
-        badgeTwo: badgeTwo,
-        badgeThree: badgeThree,
-        goodToGoLogo: goodToGoLogo,
-        safeTravelsLogo: safeTravelsLogo,
-        awards: awards,
-        ...propsData
+        imgSrc,
+        imgAlt,
+        title,
+        location,
+        stars,
+        categories,
+        description,
+        detailLink,
+        websiteLink,
+        priceIntro,
+        price,
+        priceOutro,
+        badgeOne,
+        badgeTwo,
+        badgeThree,
+        goodToGoLogo,
+        safeTravelsLogo,
+        awards,
+        ...propsData,
     },
 });
 
-const originalWarn = console.warn.bind(console.warn)
+/* eslint-disable */
+const originalWarn = console.warn.bind(console.warn);
 
 beforeAll(() => {
-    console.warn = (msg) => 
-        !msg.toString().includes('tooltip - The provided target is no valid HTML element.') && originalWarn(msg)
-})
+    console.warn = (msg) => !msg.toString().includes('tooltip - The provided target is no valid HTML element.') && originalWarn(msg);
+});
 
 afterAll(() => {
-  console.warn = originalWarn
-})
+    console.warn = originalWarn;
+});
+/* eslint-enable */
 
 let wrapper;
 beforeEach(() => {
@@ -93,33 +94,33 @@ describe('VsProductCard', () => {
         describe(':img', () => {
             it('should render a `vs-img` with the `src` provided in `imgSrc`', () => {
                 const mainProdImg = wrapper.find('[data-test="vs-product-card__img"]');
-                
+
                 expect(mainProdImg.props().src).toBe(imgSrc);
             });
 
             it('should render a `vs-img` with the `alt` provided in `imgAlt`', () => {
                 const mainProdImg = wrapper.find('[data-test="vs-product-card__img"]');
-                
+
                 expect(mainProdImg.props().alt).toBe(imgAlt);
             });
         });
 
         it('should render the content of the `title` property as a vs-heading', () => {
             const prodTitle = wrapper.find('[data-test="vs-product-card__title"]');
-            
+
             expect(prodTitle.html()).toContain(title);
         });
 
         describe(':link', () => {
             it('should render a `vs-link` with the `href` provided in `detailLink.link`', () => {
                 const prodLink = wrapper.find('[data-test="vs-product-card__link"]');
-                
+
                 expect(prodLink.props().href).toBe(detailLink.link);
             });
 
             it('should render a `vs-link` with the `type` provided in `detailLink.type`', () => {
                 const prodLink = wrapper.find('[data-test="vs-product-card__link"]');
-                
+
                 expect(prodLink.props().type).toBe(detailLink.type);
             });
 
@@ -130,7 +131,7 @@ describe('VsProductCard', () => {
 
         it('should render the content of the `location` property as a vs-heading', () => {
             const prodTitle = wrapper.find('[data-test="vs-product-card__location"]');
-            
+
             expect(prodTitle.html()).toContain(location);
         });
 
@@ -139,13 +140,13 @@ describe('VsProductCard', () => {
                 const testStars = {
                     min: 4,
                     max: 4,
-                    gold: false
+                    gold: false,
                 };
 
                 wrapper.setProps({
                     stars: testStars,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 expect(wrapper.findAll('.vs-product-card__star').length).toBe(4);
@@ -155,13 +156,13 @@ describe('VsProductCard', () => {
                 const testStars = {
                     min: 2,
                     max: 4,
-                    gold: false
+                    gold: false,
                 };
 
                 wrapper.setProps({
                     stars: testStars,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 expect(wrapper.findAll('.vs-product-card__star').length).toBe(1);
@@ -171,13 +172,13 @@ describe('VsProductCard', () => {
                 const testStars = {
                     min: 2,
                     max: 4,
-                    gold: false
+                    gold: false,
                 };
 
                 wrapper.setProps({
                     stars: testStars,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 expect(wrapper.find('.vs-product-card__stars').html()).toContain('2-4');
@@ -187,13 +188,13 @@ describe('VsProductCard', () => {
                 const testStars = {
                     min: 4,
                     max: 4,
-                    gold: false
+                    gold: false,
                 };
 
                 wrapper.setProps({
                     stars: testStars,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 const star = wrapper.find('.vs-product-card__star');
@@ -205,13 +206,13 @@ describe('VsProductCard', () => {
                 const testStars = {
                     min: 4,
                     max: 4,
-                    gold: true
+                    gold: true,
                 };
 
                 wrapper.setProps({
                     stars: testStars,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 const star = wrapper.find('.vs-product-card__star');
@@ -222,7 +223,7 @@ describe('VsProductCard', () => {
 
         describe(':categories', () => {
             it('should render each item within the `categories` property', () => {
-                const cardCategories = wrapper.find('[data-test="vs-product-card__categories"]'); 
+                const cardCategories = wrapper.find('[data-test="vs-product-card__categories"]');
 
                 for (let x = 0; x < categories.length; x++) {
                     expect(cardCategories.html()).toContain(categories[x]);
@@ -230,15 +231,15 @@ describe('VsProductCard', () => {
             });
 
             it('should not render additional `categories` beyond three', async() => {
-                const testCats = ["a", "b", "c", "d", "e"];
+                const testCats = ['a', 'b', 'c', 'd', 'e'];
 
                 wrapper.setProps({
                     categories: testCats,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
-                const cardCategories = wrapper.find('[data-test="vs-product-card__categories"]'); 
+                const cardCategories = wrapper.find('[data-test="vs-product-card__categories"]');
 
                 for (let x = 3; x < categories.length; x++) {
                     expect(cardCategories.html()).not.toContain(categories[x]);
@@ -253,13 +254,13 @@ describe('VsProductCard', () => {
         describe(':websiteLink', () => {
             it('should render a `vs-link` with the `href` provided in `websiteLink.link`', () => {
                 const prodLink = wrapper.find('[data-test="vs-product-card__website-link"]');
-                
+
                 expect(prodLink.props().href).toBe(websiteLink.link);
             });
 
             it('should render a `vs-link` with the `type` provided in `websiteLink.type`', () => {
                 const prodLink = wrapper.find('[data-test="vs-product-card__website-link"]');
-                
+
                 expect(prodLink.props().type).toBe(websiteLink.type);
             });
 
@@ -300,12 +301,12 @@ describe('VsProductCard', () => {
             });
 
             it('should not render the good to go vs-tooltip if no `goodToGoLogo` property is provided', async() => {
-                const testGtG = "";;
+                const testGtG = ''; ;
 
                 wrapper.setProps({
                     goodToGoLogo: testGtG,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 const tooltip = wrapper.find('[data-test="vs-product-card__good-to-go"]');
@@ -320,12 +321,12 @@ describe('VsProductCard', () => {
             });
 
             it('should not render the safe travels vs-tooltip if no `safeTravelsLogo` property is provided', async() => {
-                const testSt = "";
+                const testSt = '';
 
                 wrapper.setProps({
                     safeTravelsLogo: testSt,
                 });
-    
+
                 await wrapper.vm.$nextTick();
 
                 const tooltip = wrapper.find('[data-test="vs-product-card__safe-travels"]');
