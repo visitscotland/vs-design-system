9fee06df4d2c5510ea489fbcfba352e0a8eddf2f
Michael Prosser <michael.prosser@visitscotland.com>
2021-08-20 12:55:55 +0100

    VS-2719 FEAT (Canned search) - Move logos into a separate component

--- frontend/src/components/patterns/canned-search/index.js
+++ frontend/src/components/patterns/canned-search/index.js
@@ -1,7 +1,13 @@
 import VsCannedSearch from './CannedSearch';
 import VsCannedSearchProductCard from './components/CannedSearchProductCard';
+import VsCannedSearchStars from './components/CannedSearchStars';
+import VsCannedSearchCategories from './components/CannedSearchCategories';
+import VsCannedSearchLogos from './components/CannedSearchLogos';
 
 export default {
     VsCannedSearch,
     VsCannedSearchProductCard,
+    VsCannedSearchStars,
+    VsCannedSearchCategories,
+    VsCannedSearchLogos,
 };
