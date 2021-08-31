7c1e08aab020af3e98f8788c9b5e86b428f6cd63
Gemma <gemma.stephen@visitscotland.com>
2021-08-31 17:09:25 +0100

    VS-2722 FIX (Travel Panel) Fixing no-js UI issues for Accordions and Tabs

--- frontend/src/components/patterns/accordion/components/AccordionItem.vue
+++ frontend/src/components/patterns/accordion/components/AccordionItem.vue
@@ -181,10 +181,6 @@ export default {
 }
 
 @include no-js {
-    .vs-accordion-toggle .vs-icon {
-        display: none;
-    }
-
     .vs-accordion-item .vs-accordion-item__panel {
         display: block!important;
     }
