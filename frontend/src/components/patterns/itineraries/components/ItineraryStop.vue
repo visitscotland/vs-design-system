5e4736d07943ee02f00338903eebd4af328ff3da
jhaddrill <james.haddrill@visitscotland.com>
2021-09-01 15:45:10 +0100

    VS-2783 FIX (ItineraryStop.vue) remove css change

--- frontend/src/components/patterns/itineraries/components/ItineraryStop.vue
+++ frontend/src/components/patterns/itineraries/components/ItineraryStop.vue
@@ -164,10 +164,6 @@ export default {
             padding: $spacer-4 $spacer-0;
             margin-right: $spacer-6;
         }
-
-        .vs-address {
-            display: flex;
-        }
     }
 
     &__address,
