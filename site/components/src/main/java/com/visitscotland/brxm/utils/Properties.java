c6bb33fb87b03a4fa1a7452ac3e5b0b3e00d9771
Jose Calcines <jose.calcines@visitscotland.com>
2021-09-03 21:40:14 +0100

    VS-2770 WIP (Configuration) (Properties) (DMS) Read Properties from Configuration

--- site/components/src/main/java/com/visitscotland/brxm/utils/Properties.java
+++ site/components/src/main/java/com/visitscotland/brxm/utils/Properties.java
@@ -2,6 +2,7 @@ package com.visitscotland.brxm.utils;
 
 import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.utils.Contract;
+import org.hippoecm.hst.container.RequestContextProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,26 +16,33 @@ public class Properties {
 
     private static final Logger logger = LoggerFactory.getLogger(Properties.class.getName());
 
-    static final String BUNDLE_ID = "config.cms";
+    static final String DEFAULT_ID = "config.cms";
 
     static final String INSTAGRAM_API = "instagram.api";
     static final String INSTAGRAM_ACCESS_TOKEN ="instagram.accesstoken";
     static final String INSTAGRAM_APP_ID ="instagram.app-id";
     static final String INSTAGRAM_URL ="instagram.post-url";
-    static final String CMS_BASE_PATH = "links.cms-base-path-url";
     static final String HELPDESK_EMAIL = "helpdesk-email";
-    static final String DMS_HOST = "links.vs-dms-products.url";
-    static final String DMS_MAP_DEFAULT_DISTANCE = "dms.default-distance";
-    static final String DMS_DATA_HOST = "dms-data.url";
+    static final String IKNOW_COMMUNITY_URL = "iknow-community.url";
+    static final String IKNOW_COMMUNITY_TAGGED_DISCUSSION = "iknow-community.tagged-discussion";
+
+    //Environment
+    static final String USE_RELATIVE_URLS = "links.use-relative-urls";
+    static final String INTERNAL_SITES = "links.internal-sites";
+    static final String CMS_BASE_PATH = "links.cms-base-path-url";
+
+    // DMS Properties
+    static final String DMS_DATA_HOST = "dms-data.private-url";
+    static final String DMS_DATA_PUBLIC_HOST = "dms-data.public-url";
     static final String DMS_DATA_ENCODING = "dms-data.encoding";
     static final String DMS_DATA_API_KEY = "dms-data.api-key";
     static final String DMS_DATA_TIMEOUT = "dms-data.timeout";
     static final String DMS_DATA_TRIES = "dms-data.tries";
     static final String DMS_DATA_SLEEP_TIME = "dms-data.sleep-time";
-    static final String IKNOW_COMMUNITY_URL = "iknow-community.url";
-    static final String IKNOW_COMMUNITY_TAGGED_DISCUSSION = "iknow-community.tagged-discussion";
-    static final String USE_RELATIVE_URLS = "links.use-relative-urls";
-    static final String INTERNAL_SITES = "links.internal-sites";
+    static final String DMS_HOST = "links.vs-dms-products.url";
+    static final String DMS_MAP_DEFAULT_DISTANCE = "dms.default-distance";
+
+
 
 
     private final ResourceBundleService bundle;
@@ -167,11 +175,23 @@ public class Properties {
         return Boolean.parseBoolean(getProperty(key));
     }
 
+    /**
+     * Calculates the properties document defined in the environment (i.e. /hst:visitscotland/hst:hosts/dev-localhost/localhost/)
+     * and when none is defined returns the default one.
+     *
+     * @return Resource Bundle id for the configuration
+     */
+    private String getEnvironmentProperties(){
+        String bundleId = RequestContextProvider.get().getResolvedMount().getMount().getProperty("visitscotland:cmsProperties");
+        return bundleId != null ? bundleId : DEFAULT_ID;
+    }
+
     private String getProperty(String key){
-        String value = bundle.getResourceBundle(BUNDLE_ID, key, Locale.UK);
+        String bundleId = getEnvironmentProperties();
+        String value = bundle.getResourceBundle(bundleId, key, Locale.UK);
 
         if (Contract.isEmpty(value)){
-            logger.warn("The property {} hasn't been set in the resourceBundle {}", key, BUNDLE_ID);
+            logger.warn("The property {} hasn't been set in the resourceBundle {}", key, bundleId);
         } else if (value.startsWith("$")){
             return getEnvironmentVariable(value.substring(1));
         } else {
