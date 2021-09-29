31925de78f67b02cf89b1e270c548378d303bbaa
Ben Banerjee-Richards <Ben.Banerjee-Richards@visitscotland.com>
2021-09-29 15:58:17 +0100

    VS-1998 (Translation Fallback) Remove import that breaks compilation

--- site/components/src/main/java/com/visitscotland/brxm/utils/CustomLocalizationValve.java
+++ site/components/src/main/java/com/visitscotland/brxm/utils/CustomLocalizationValve.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.jstl.core.Config;
 import java.util.Collections;
 import java.util.Locale;
 
