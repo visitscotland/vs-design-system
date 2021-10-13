5adc2225d7c31d3eb5f1a532c8d1e42ab6766784
jhaddrill <james.haddrill@visitscotland.com>
2021-10-13 13:55:59 +0100

    VS-2757 FEAT (Psr.vue) add PSR component

--- frontend/src/components/elements/psr/Psr.vue
+++ frontend/src/components/elements/psr/Psr.vue
@@ -2,38 +2,77 @@
     <div
         class="vs-psr"
         id="inline-search-container"
+        v-bind="prefilAttrs"
     />
 </template>
 
 <script>
 /**
- * Prototypes are wrappers used to visually communicate parts
- * of the product or actions that are still unavailable.
+ * The PSR widget is embedded from an external source.
+ * It can be customised with data attributes.
  *
- * This is a development only tool to help visualise
- * components/functionalities to be finished.
- *
- * @displayName Prototype
+ * @displayName PSR
  */
 
 export default {
     name: 'VsPsr',
     status: 'prototype',
     release: '0.0.1',
+    props: {
+        /**
+         * Pre-filled fields
+         */
+        prefilled: {
+            type: Array,
+            default() {
+                return [];
+            },
+        },
+    },
+    computed: {
+        /* Return an array of objects defining data attributes */
+        prefilAttrs() {
+            const dataAttrs = [];
+
+            this.prefilled.forEach((element) => {
+                const attrObj = {
+                };
+                const keyName = Object.keys(element);
+                const attrKey = `data-${keyName}`;
+                attrObj[attrKey] = element[keyName];
+
+                dataAttrs.push(
+                    attrObj,
+                );
+            });
+
+            return dataAttrs;
+        },
+    },
     mounted() {
-        const recaptchaScript = document.createElement('script');
-        recaptchaScript.setAttribute('src', 'https://develop.visitscotland.com/api/dev/ui/product-search/static/js/bundle.js');
-        document.head.appendChild(recaptchaScript);
+        const psrScriptEl = document.createElement('script');
+        psrScriptEl.setAttribute('src', 'https://develop.visitscotland.com/api/dev/ui/product-search/static/js/bundle.js');
+        document.head.appendChild(psrScriptEl);
     },
 };
 </script>
 
 <style lang="scss">
 
+@import "https://develop.visitscotland.com/api/dev/ui/product-search/css/main.css";
+
+.no-js {
+    .vs-psr {
+        display: none;
+    }
+}
+
 </style>
 
 <docs>
 ```jsx
-    <h4>Originals</h4>
+    <vs-psr
+        :prefilled="[{'subSearchType': 'cate'},{'loc': 'Inverary'}]"
+    />
 ```
 </docs>
