b29e7801b59a28bbcc8e7cf2bf6a36172c7e346b
Gemma <gemma.stephen@visitscotland.com>
2021-10-14 18:07:35 +0100

    VS-2857 MAINT (ImageWithCaption) Updating styles and adding unit tests

--- frontend/src/components/patterns/caption/Caption.vue
+++ frontend/src/components/patterns/caption/Caption.vue
@@ -10,25 +10,34 @@
                 class="order-2 order-sm-1"
                 :class="[!showMap ? 'align-self-center' : '']"
             >
-                <div class="caption-info">
-                    <p class="image-caption">
+                <div class="vs-caption__caption-info">
+                    <p
+                        class="vs-caption__image-caption"
+                        v-if="!!this.$slots.caption"
+                    >
+                        <!-- @slot Slot to display caption  -->
                         <slot name="caption" />
                     </p>
 
-                    <p class="image-credit">
+                    <p
+                        class="vs-caption__image-credit"
+                        v-if="!!this.$slots.credit"
+                    >
+                        <!-- @slot Slot to display credit  -->
                         <slot name="credit" />
                     </p>
 
-                    <slot name="social-link" />
+                    <!-- @slot Slot to display social media credit link  -->
+                    <slot name="socialLink" />
                 </div>
             </VsCol>
             <VsCol
                 class="col-12 col-sm-auto order-1
                 order-sm-2 pl-sm-0 align-self-end
                 align-self-sm-start"
-                v-if="showMap && variant !== isLargeCaption"
+                v-if="showMap && isLargeCaption"
             >
-                <div class="map-wrapper pt-3 pt-sm-2 pb-sm-2 pr-sm-4 mx-auto">
+                <div class="vs-caption__map-wrapper pt-3 pt-sm-2 pb-sm-2 pr-sm-4 mx-auto">
                     <VsImageLocationMap
                         :latitude="latitude"
                         :longitude="longitude"
@@ -109,10 +118,10 @@ export default {
         captionClasses() {
             return [
                 {
-                    'large-caption-wrapper': this.isLargeCaption,
-                    'fullwidth-caption-wrapper': !this.isLargeCaption,
+                    'vs-caption--large': this.isLargeCaption,
+                    'vs-caption--fullwidth': !this.isLargeCaption,
                 },
-                `caption-wrapper--${this.textAlign}`,
+                `vs-caption--${this.textAlign}`,
             ];
         },
     },
@@ -124,31 +133,9 @@ export default {
     background-color: $color-gray-shade-6;
     color: $color-white;
 
-    .image-caption,
-    .image-credit {
-        font-size: $small-font-size;
-        line-height: $line-height-standard;
-    }
-
-    .image-caption {
-        font-weight: $font-weight-semi-bold;
-    }
-
-    .image-credit {
-        font-weight: $font-weight-light;
-        margin-bottom: $spacer-0;
-    }
-
-    &.large-caption-wrapper,
-    &.fullwidth-caption-wrapper {
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 2;
-        display: flex;
-        text-align: center;
+    &--large,
+    &--fullwidth {
+        position: relative;
 
         @include media-breakpoint-up(sm) {
             display: block;
@@ -157,33 +144,33 @@ export default {
         > .row {
             margin: 0 auto;
         }
+        .vs-caption{
+            &__image-caption {
+                margin-bottom: $spacer-2;
+            }
 
-        .image-caption {
-            margin-bottom: $spacer-2;
-        }
-
-        .map-wrapper {
-            max-width: 54px;
+            &__map-wrapper {
+                max-width: 54px;
+            }
         }
     }
 
-    &.fullwidth-caption-wrapper{
+    &--fullwidth{
         @include media-breakpoint-up(sm) {
-            position: relative;
             width: 100%;
             height: auto;
             min-height: 64px;
             text-align: left;
         }
-        .caption-info{
+        .vs-caption__caption-info{
             padding: $spacer-3 0;
         }
     }
 
-    &.large-caption-wrapper {
+    &--large {
+        text-align: center;
+
         @include media-breakpoint-up(sm) {
-            bottom: -48px;
-            top: auto;
             width: 310px;
             height: auto;
             min-height: 96px;
@@ -193,23 +180,40 @@ export default {
                 margin: 0 -16px;
             }
 
-            .image-caption {
-                margin-bottom: $spacer-8;
-            }
+            .vs-caption{
+                &__image-caption {
+                   margin-bottom: $spacer-8;
+                }
 
-            .map-wrapper {
-                max-width: 74px;
+                &__map-wrapper {
+                    max-width: 74px;
+                }
             }
         }
 
-        .caption-info{
+        .vs-caption__caption-info{
             padding: $spacer-4;
         }
     }
 
+    &__image-caption,
+    &__image-credit {
+        font-size: $small-font-size;
+        line-height: $line-height-standard;
+    }
+
+    &__image-caption {
+        font-weight: $font-weight-semi-bold;
+    }
+
+    &__image-credit {
+        font-weight: $font-weight-light;
+        margin-bottom: $spacer-0;
+    }
+
     @include media-breakpoint-up(md) {
-        &.caption-wrapper--right {
-            p{
+        &--right {
+            .vs-caption__caption-info{
                 text-align: right;
             }
         }
@@ -219,50 +223,56 @@ export default {
 @include no-js {
     .vs-image-with-caption__caption-wrapper{
         @include media-breakpoint-down(xs) {
-            .vs-caption.large-caption-wrapper {
-                position: relative;
-                top: 0;
-                width: 100%;
-                height: auto;
-                text-align: left;
+            .vs-caption{
+                &--large {
+                    position: relative;
+                    top: 0;
+                    width: 100%;
+                    height: auto;
+                    text-align: left;
+                    display: block;
+
+                    .order-2 {
+                        order: 1;
+                    }
+
+                    .order-1 {
+                        order: 2;
+                        flex: 0 0 auto;
+                        width: auto;
+                        max-width: 100%;
+                        align-self: auto!important;
+                    }
+
+                    .vs-caption{
+                        &__map-wrapper {
+                            padding-top: 0!important;
+                        }
+
+                        &__image-caption {
+                            margin-bottom: $spacer-5;
+                        }
+
+                        &__caption-info{
+                            padding: $spacer-3 $spacer-2;
+                        }
+                    }
+                }
+            }
+        }
+
+        .vs-caption{
+            &--large,
+            &--fullwidth {
+                @include media-breakpoint-down(xs) {
+                    position: relative;
+                }
+            }
+
+            &--fullwidth {
                 display: block;
-
-                .order-2 {
-                    order: 1;
-                }
-
-                .order-1 {
-                    order: 2;
-                    flex: 0 0 auto;
-                    width: auto;
-                    max-width: 100%;
-                    align-self: auto!important;
-                }
-
-                .map-wrapper {
-                    padding-top: 0!important;
-                }
-
-                .image-caption {
-                    margin-bottom: $spacer-5;
-                }
-
-                .caption-info{
-                    padding: $spacer-3 $spacer-2;
-                }
+                text-align: left;
             }
-        }
-
-        .large-caption-wrapper,
-        .fullwidth-caption-wrapper {
-            @include media-breakpoint-down(xs) {
-                position: relative;
-            }
-        }
-
-        .fullwidth-caption-wrapper {
-            display: block;
-            text-align: left;
         }
     }
 }
@@ -270,34 +280,59 @@ export default {
 
 <docs>
   ```js
-    <h3>Fullwidth Caption Style</h3>
-    <VsCaption
-        variant="fullwidth"
-        style="max-width:500px"
-    >
-        <span slot="caption">
-            A Scottish Castle
-        </span>
 
-        <span slot="credit">
-            VisitScotland
-        </span>
-    </VsCaption>
+    <BsWrapper style="max-width: 500px">
+        <h3>Fullwidth Caption</h3>
+        <VsCaption>
+            <span slot="caption">
+                A Scottish Castle
+            </span>
 
-    <h3 style="margin-top: 3rem;">Large Caption Style</h3>
-    <VsCaption
-        :latitude="55.9485947"
-        :longitude="-3.2021022"
-        variant="large"
-        style="position: relative; bottom: 0;"
-    >
-        <span slot="caption">
-            A Scottish Castle
-        </span>
+            <span slot="credit">
+                VisitScotland
+            </span>
+        </VsCaption>
 
-        <span slot="credit">
-            VisitScotland
-        </span>
-    </VsCaption>
+        <h3 style="margin-top: 3rem;">Fullwidth Caption (Right)</h3>
+        <VsCaption text-align="right">
+            <span slot="caption">
+                A Scottish Castle
+            </span>
+
+            <span slot="credit">
+                VisitScotland
+            </span>
+        </VsCaption>
+
+        <h3 style="margin-top: 3rem;">Fullwidth Social Image</h3>
+        <VsCaption>
+            <span slot="caption">
+                A Scottish Castle
+            </span>
+
+            <VsSocialCreditLink
+                slot="socialLink"
+                credit="VisitScotland"
+                socialPostUrl="http://www.visitscotland.com"
+                source="instagram"
+            >
+            </VsSocialCreditLink>
+        </VsCaption>
+
+        <h3 style="margin-top: 3rem;">Large Caption Style</h3>
+        <VsCaption
+            latitude="55.9485947"
+            longitude="-3.2021022"
+            variant="large"
+        >
+            <span slot="caption">
+                A Scottish Castle
+            </span>
+
+            <span slot="credit">
+                VisitScotland
+            </span>
+        </VsCaption>
+    </BsWrapper>
   ```
 </docs>
