e8b1f95e545c6c3010d6bc6313245a94f1dd2ded
Ben Banerjee-Richards <Ben.Banerjee-Richards@visitscotland.com>
2021-10-20 14:38:58 +0100

    VS-2896 (Travel Information) Use resource bundle for transport types

--- site/components/src/main/java/com/visitscotland/brxm/factory/TravelInformationFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/TravelInformationFactory.java
@@ -7,12 +7,14 @@ import com.visitscotland.brxm.model.TransportType;
 import com.visitscotland.brxm.model.TravelInformationModule;
 import com.visitscotland.brxm.model.TravelInformationModuleTab;
 import com.visitscotland.brxm.model.TravelInformationTransportRowModule;
+import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.brxm.utils.HippoUtilsService;
+import com.visitscotland.utils.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Component
@@ -20,42 +22,45 @@ public class TravelInformationFactory {
 
     private final String TRAVEL_INFO_TRANSPORTS_OPTIONS = "travel-information-transports";
     private final HippoUtilsService utils;
+    private final ResourceBundleService resourceBundleService;
     private static final Logger logger = LoggerFactory.getLogger(TravelInformationFactory.class);
 
-    public TravelInformationFactory(HippoUtilsService utils) {
+    public TravelInformationFactory(HippoUtilsService utils, ResourceBundleService resourceBundleService) {
         this.utils = utils;
+        this.resourceBundleService = resourceBundleService;
     }
 
-    public TravelInformationModule getTravelInformation(TravelInformation document) {
+    public TravelInformationModule getTravelInformation(TravelInformation document, Locale locale) {
         TravelInformationModule module = new TravelInformationModule();
         module.setTitle(document.getTitle());
         module.setCopy(document.getCopy());
-        module.setGettingTo(getTravelInformationModuleTab(document.getGettingTo()));
-        module.setGettingAround(getTravelInformationModuleTab(document.getGettingAround()));
+        module.setGettingTo(getTravelInformationModuleTab(document.getGettingTo(), locale));
+        module.setGettingAround(getTravelInformationModuleTab(document.getGettingAround(), locale));
         module.setHippoBean(document);
         return module;
     }
 
-    private TravelInformationModuleTab getTravelInformationModuleTab(TravelInformationTab document) {
+    private TravelInformationModuleTab getTravelInformationModuleTab(TravelInformationTab document, Locale locale) {
         TravelInformationModuleTab module = new TravelInformationModuleTab();
         module.setTitle(document.getTitle());
         module.setTravelInformationTransportRows(document.getAccordion()
-                .stream().map(this::getTravelInformationTransportRowModule).collect(Collectors.toList()));
+                .stream().map(travelInformationTransportRow -> getTravelInformationTransportRowModule(travelInformationTransportRow, locale))
+                .collect(Collectors.toList()));
         module.setHippoBean(document);
         return module;
     }
 
-    private TravelInformationTransportRowModule getTravelInformationTransportRowModule(TravelInformationTransportRow document) {
-        Map<String, String> transportMap = utils.getValueMap(TRAVEL_INFO_TRANSPORTS_OPTIONS);
+    private TravelInformationTransportRowModule getTravelInformationTransportRowModule(TravelInformationTransportRow document, Locale locale) {
         TravelInformationTransportRowModule module = new TravelInformationTransportRowModule();
         module.setCopy(document.getCopy());
         module.setHippoBean(document);
         TransportType transportType = new TransportType();
         transportType.setKey(document.getTransport());
-        if (!transportMap.containsKey(document.getTransport())) {
+        String transportLabel = resourceBundleService.getResourceBundle(TRAVEL_INFO_TRANSPORTS_OPTIONS, document.getTransport(), locale);
+        if (transportLabel == null) {
             logger.error("Transport options {} does not contain key {}", TRAVEL_INFO_TRANSPORTS_OPTIONS, document.getTransport());
         }
-        transportType.setLabel(transportMap.getOrDefault(document.getTransport(), document.getTransport()));
+        transportType.setLabel(Contract.defaultIfNull(transportLabel, document.getTransport()));
         module.setTransport(transportType);
         return module;
     }
