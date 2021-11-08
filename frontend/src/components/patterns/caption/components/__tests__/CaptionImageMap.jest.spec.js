3029de60b07228336b98195cbc0250425ee47962
Gemma <gemma.stephen@visitscotland.com>
2021-11-08 12:05:24 +0000

    VS-2856 MAINT (DesignSystem) Move ImageLocationMap to be a Caption child component

--- frontend/src/components/patterns/caption/components/__tests__/CaptionImageMap.jest.spec.js
+++ frontend/src/components/patterns/caption/components/__tests__/CaptionImageMap.jest.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
 
-import VsImageLocationMap from '../ImageLocationMap';
+import VsCaptionImageMap from '../CaptionImageMap';
 
 const slotContent = 'Slot Content';
 
-const factoryShallowMount = (propsData) => shallowMount(VsImageLocationMap, {
+const factoryShallowMount = (propsData) => shallowMount(VsCaptionImageMap, {
     propsData: {
         ...propsData,
         latitude: '57',
@@ -22,30 +22,30 @@ beforeEach(() => {
     wrapper = factoryShallowMount();
 });
 
-describe('VsImageLocationMap', () => {
-    it('should render a component with the data-test attribute `vs-image-location-map`', () => {
-        expect(wrapper.attributes('data-test')).toBe('vs-image-location-map');
+describe('VsCaptionImageMap', () => {
+    it('should render a component with the data-test attribute `vs-caption-image-map`', () => {
+        expect(wrapper.attributes('data-test')).toBe('vs-caption-image-map');
     });
 
     describe(':props', () => {
         it('should accept and render a `mapOutlineColor` property', () => {
-            const path = wrapper.find('[data-test="vs-image-location-map__first-path"]');
+            const path = wrapper.find('[data-test="vs-caption-image-map__first-path"]');
             expect(path.attributes('stroke')).toBe('#FF0000');
         });
 
         it('should accept and render a `mapMarkerColor` property', () => {
-            const marker = wrapper.find('[data-test="vs-image-location-map__marker"]');
+            const marker = wrapper.find('[data-test="vs-caption-image-map__marker"]');
             expect(marker.attributes('fill')).toBe('#00FF00');
         });
 
         it('should accept and render a `longitude` property', () => {
-            const marker = wrapper.find('[data-test="vs-image-location-map__marker"]');
+            const marker = wrapper.find('[data-test="vs-caption-image-map__marker"]');
             // 54 is the expected x coord on the svg for a lattitude of 57
             expect(marker.attributes('cx')).toBe('54');
         });
 
         it('should accept and render a `latitude` property', () => {
-            const marker = wrapper.find('[data-test="vs-image-location-map__marker"]');
+            const marker = wrapper.find('[data-test="vs-caption-image-map__marker"]');
             // 65 is the expected y coord on the svg for a latitude of -2.2
             expect(marker.attributes('cy')).toBe('65');
         });
