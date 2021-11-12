83b2ffcc4938a7c8722067a0a022dc293e5b0cd5
Michael Prosser <michael.prosser@visitscotland.com>
2021-11-12 14:44:58 +0000

    VS-2714 FEAT (Canned search) Add missing tests for lineLimit in subHeading

--- frontend/src/components/patterns/canned-search/components/__tests__/CannedSearchSubHeading.jest.spec.js
+++ frontend/src/components/patterns/canned-search/components/__tests__/CannedSearchSubHeading.jest.spec.js
@@ -20,5 +20,15 @@ describe('VsCannedSearchSubHeading', () => {
         it('should render the content of the `subHeading` property', () => {
             expect(wrapper.html()).toContain(subHeading);
         });
+
+        it('should render a subheading with the class `text-truncate--1` if a lineLimit of 1 is provided', async() => {
+            await wrapper.setProps({lineLimit: 1});
+            expect(wrapper.classes()).toContain('text-truncate--1');
+        });
+
+        it('should render a subheading with the class `text-truncate--2` if a lineLimit of 2 is provided', async () => {
+            await wrapper.setProps({lineLimit: 2});
+            expect(wrapper.classes()).toContain('text-truncate--2');
+        });
     });
 });
