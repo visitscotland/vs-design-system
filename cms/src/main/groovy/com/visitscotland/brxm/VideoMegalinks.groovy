2928141697cc1523b891aa958985af97dd1030d1
Jose Calcines <jose.calcines@visitscotland.com>
2021-11-15 16:29:06 +0000

    VS-2933 2933 (Video) (Groovy) Create Groovy Script to Implements the changes in the content modelling

---
+++ cms/src/main/groovy/com/visitscotland/brxm/VideoMegalinks.groovy
@@ -0,0 +1,87 @@
+package com.visitscotland.brxm
+
+import org.hippoecm.repository.api.HippoWorkspace
+import org.onehippo.repository.update.BaseNodeUpdateVisitor
+
+import javax.jcr.Node
+import javax.jcr.NodeIterator
+import javax.jcr.RepositoryException
+import javax.jcr.Session
+import javax.jcr.query.Query
+import javax.jcr.query.QueryResult
+
+/**
+ * Groovy Script created for altering the structure of Megalinks due to the changes related to video.
+ *
+ * This Scripts acted also as a practice exercise for future Groovy Scripts
+ *
+ * @author jcalcines
+ * @since November 2021
+ * @version 0.0.1
+ */
+class VideoMegalinks extends BaseNodeUpdateVisitor {
+
+    void fixMegalinks(Session session) {
+        NodeIterator it = query(session,"//content/documents//element(*, visitscotland:MegalinkItem)")
+
+        while (it.hasNext()){
+            Node n = it.next()
+            log.debug n.getPath()
+            if (n.hasNode("visitscotland:link")) {
+                if (n.hasNode("visitscotland:links")) {
+                    session.removeItem(n.getNode("visitscotland:link").getPath())
+                } else {
+                    session.move(n.getNode("visitscotland:link").getPath(), n.getPath() + "/visitscotland:links")
+                }
+            }
+        }
+    }
+
+    void fixOTYML(Session session) {
+        NodeIterator it = query(session,"//content/documents//element(*, visitscotland:OTYML)")
+
+        while (it.hasNext()){
+            Node n = it.next()
+            log.debug n.getPath()
+            NodeIterator links = n.getNodes("visitscotland:links")
+            while (links.hasNext()){
+                Node link = links.next()
+                if (link.hasNode("visitscotland:link")) {
+                    log.info "Converting ${link.getPath()}"
+                    session.move(link.getPath() + "/visitscotland:link", n.getPath() + "/visitscotland:links")
+                    session.removeItem(link.getPath())
+                }
+            }
+        }
+    }
+
+    /**
+     * Executes a query and log when no results are returned
+     * @param session
+     * @param query
+     * @return
+     */
+    NodeIterator query(def session, def query){
+        QueryResult results = ((HippoWorkspace) session.getWorkspace()).getQueryManager().createQuery(query, Query.XPATH).execute();
+        if (!results.getNodes().hasNext()){
+            log.warn "No query results for ${query}"
+        } else {
+            // Note that the method size. Moves the pointer for the iterator at the end. That is why we need to
+            // invoke .getNodes() for getting the size
+            log.info "Matches = ${results.getNodes().size()}";
+        }
+        return results.getNodes();
+    }
+
+    @Override
+    boolean doUpdate(Node node) {
+        fixMegalinks(node.session);
+        fixOTYML(node.session);
+        return true;
+    }
+
+    @Override
+    boolean undoUpdate(Node node) throws RepositoryException, UnsupportedOperationException {
+        return false
+    }
+}
