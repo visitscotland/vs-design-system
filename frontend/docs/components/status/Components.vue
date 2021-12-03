7ebff92244cdadeda1b9dfbf30a9e4d969719f99
Gemma <gemma.stephen@visitscotland.com>
2021-12-03 17:09:05 +0000

    VS-2517 FEAT (DesignSystem) Adding styles for tables

--- frontend/docs/components/status/Components.vue
+++ frontend/docs/components/status/Components.vue
@@ -4,7 +4,7 @@
             <li>
                 <VsIcon
                     name="docs/ready"
-                    customColour="green"
+                    custom-colour="green"
                     size="xs"
                 />
                 <p class="mb-0">
@@ -14,7 +14,7 @@
             <li>
                 <VsIcon
                     name="docs/review"
-                    customColour="orange"
+                    custom-colour="orange"
                     size="xs"
                 />
                 <p class="mb-0">
@@ -24,7 +24,7 @@
             <li>
                 <VsIcon
                     name="docs/deprecated"
-                    customColour="red"
+                    custom-colour="red"
                     size="xs"
                 />
                 <p class="mb-0">
@@ -34,7 +34,7 @@
             <li>
                 <VsIcon
                     name="docs/prototype"
-                    customColour="blue"
+                    custom-colour="blue"
                     size="xs"
                 />
                 <p class="mb-0">
@@ -89,26 +89,26 @@
                         <VsIcon
                             v-if="component.status === 'docs/ready'"
                             name="docs/ready"
-                            customColour="green"
+                            custom-colour="green"
                             size="xs"
                         />
                         <VsIcon
                             v-if="component.status === 'under-review' ||
                                 component.status === 'review'"
                             name="docs/review"
-                            customColour="orange"
+                            custom-colour="orange"
                             size="xs"
                         />
                         <VsIcon
                             v-if="component.status === 'prototype'"
                             name="docs/prototype"
-                            customColour="blue"
+                            custom-colour="blue"
                             size="xs"
                         />
                         <VsIcon
                             v-if="component.status === 'deprecated'"
                             name="docs/deprecated"
-                            customColour="red"
+                            custom-colour="red"
                             size="xs"
                         />
                     </td>
@@ -124,16 +124,16 @@
 <script>
 // If you want to use your own tokens here, change the following line to:
 // import designTokens from "@/assets/tokens/tokens.raw.json"
-import orderBy from "lodash/orderBy"
-import get from "lodash/get"
-import designTokens from "../../styles/docs.tokens.json"
+import orderBy from 'lodash/orderBy';
+import get from 'lodash/get';
+import designTokens from '../../styles/docs.tokens.json';
 
 export default {
-    name: "Components",
+    name: 'Components',
     props: {
         show: {
             type: String,
-            default: "all",
+            default: 'all',
             validator: (value) => value.match(/(all|patterns|templates|elements|modules)/),
         },
     },
@@ -141,27 +141,27 @@ export default {
         return {
             components: this.orderData(this.getComponents()),
             tokens: designTokens.props,
-        }
+        };
     },
     methods: {
         getComponents() {
-            let contexts
+            let contexts;
 
-            if ((this.show === "all") || this.show === "modules") {
+            if ((this.show === 'all') || this.show === 'modules') {
                 contexts = [
-                    require.context("@components/elements/", true, /\.vue$/),
-                    require.context("@components/patterns/", true, /\.vue$/),
-                    // require.context("@components/modules/", true, /\.vue$/),
-                    require.context("@components/templates/", true, /\.vue$/),
+                    require.context('@components/elements/', true, /\.vue$/),
+                    require.context('@components/patterns/', true, /\.vue$/),
+                    // require.context('@components/modules/', true, /\.vue$/),
+                    require.context('@components/templates/', true, /\.vue$/),
                 ]
-            } else if (this.show === "elements") {
-                contexts = [require.context("@components/elements/", true, /\.vue$/)]
-            } else if (this.show === "patterns") {
-                contexts = [require.context("@components/patterns/", true, /\.vue$/)]
-                // } else if (this.show === "modules") {
-                //   contexts = [require.context("@components/modules/", true, /\.vue$/)]
-            } else if (this.show === "templates") {
-                contexts = [require.context("@components/templates/", true, /\.vue$/)]
+            } else if (this.show === 'elements') {
+                contexts = [require.context('@components/elements/', true, /\.vue$/)]
+            } else if (this.show === 'patterns') {
+                contexts = [require.context('@components/patterns/', true, /\.vue$/)]
+                // } else if (this.show === 'modules') {
+                //   contexts = [require.context('@components/modules/', true, /\.vue$/)]
+            } else if (this.show === 'templates') {
+                contexts = [require.context('@components/templates/', true, /\.vue$/)]
             }
 
             const components = []
@@ -172,113 +172,122 @@ export default {
             return components
         },
         orderData(data) {
-            return orderBy(data, "name", "asc")
+            return orderBy(data, 'name', 'asc')
         },
         getColour(colourName) {
-            return get(this.tokens, `docs_color_${colourName}.value`, "#aaa")
+            return get(this.tokens, `docs_color_${colourName}.value`, '#aaa')
         },
     },
 }
 </script>
 
-<style lang="scss" scoped>
-@import "../../styles/docs.tokens.scss";
-@import "../../styles/docs.mixins.scss";
-@import "../../styles/docs.functions.scss";
+<style lang='scss'>
+    @import '../../styles/docs.tokens.scss';
+    @import '../../styles/docs.mixins.scss';
+    @import '../../styles/docs.functions.scss';
 
-/* STYLES
---------------------------------------------- */
+    /* STYLES
+    --------------------------------------------- */
 
-.component-status {
-  @include reset;
-  font-family: $font-heading;
-  font-weight: $weight-normal;
-  line-height: $line-height-xs;
-  color: $docs-color-rich-black;
-  margin-bottom: $space-s;
-  font-style: normal;
-  @media (max-width: 1000px) {
-    overflow-x: auto;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-    width: 100%;
-  }
-  thead th {
-    padding: $space-s;
-    background: $docs-color-cloud;
-    font-size: $size-s;
-    font-weight: $weight-bold;
-    color: $docs-color-oxford-blue;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: $weight-semi-bold;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: left;
-    // Chrome has a bug related to thead, this only works on th:
-    position: -webkit-sticky;
-    position: sticky;
-    top: -1px;
-    &:first-child {
-      border-top-left-radius: $radius-default;
-      border-bottom-left-radius: $radius-default;
-    }
-    &:last-child {
-      border-top-right-radius: $radius-default;
-      border-bottom-right-radius: $radius-default;
-    }
-  }
-  tr {
-    border-bottom: 1px solid #dfe3e6;
-    &:last-child {
-      border: 0;
-    }
-  }
-  td {
-    font-size: $size-s;
-    padding: $space-s;
-    &:first-child {
-      font-weight: $weight-bold;
-      white-space: nowrap;
-    }
-  }
-  .status-list {
-    margin: 0 0 $space-m;
-    overflow: hidden;
-    padding: 0;
-    list-style: none;
-    flex-direction: row;
-    align-items: center;
-    display: flex;
-    @media (max-width: 1000px) {
-      display: block;
-    }
-    li {
-      margin: 0 $space-m 0 0;
-      color: shade($docs-color-silver, 20%);
-      font-size: $size-s;
-      align-items: center;
-      display: flex;
-      @media (max-width: 1000px) {
-        width: 50%;
-        float: left;
-        margin: 0;
-      }
-      svg,
-      span {
-        margin: 0 calc(#{$space-s} / 2) 0 0;
-      }
-      p {
-        margin-bottom: 0;
+    .component-status {
+        @include reset;
+        font-weight: $weight-normal;
+        line-height: $line-height-xs;
+        color: $docs-color-rich-black;
+        margin-bottom: $space-s;
+        font-style: normal;
+
         @media (max-width: 1000px) {
-          margin: $space-xs;
+            overflow-x: auto;
         }
-      }
+
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+            width: 100%;
+        }
+
+        thead th {
+            padding: $space-s;
+            background: $docs-color-purple;
+            font-size: $size-m;
+            font-weight: $weight-normal;
+            color: $docs-color-white;
+            font-weight: $weight-semi-bold;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: left;
+            // Chrome has a bug related to thead, this only works on th:
+            position: -webkit-sticky;
+            position: sticky;
+            top: -1px;
+
+            &:first-child {
+                border-top-left-radius: $radius-m;
+            }
+
+            &:last-child {
+                border-top-right-radius: $radius-m;
+            }
+        }
+
+        tr {
+            border-bottom: 1px solid #dfe3e6;
+
+            &:last-child {
+                border: 0;
+            }
+        }
+        td {
+            font-size: $size-s;
+            padding: $space-s;
+
+            &:first-child {
+                font-weight: $weight-bold;
+                white-space: nowrap;
+            }
+        }
+        .status-list {
+            margin: 0 0 $space-m;
+            overflow: hidden;
+            padding: 0;
+            list-style: none;
+            flex-direction: row;
+            align-items: center;
+            display: flex;
+
+            @media (max-width: 1000px) {
+                display: block;
+            }
+
+            li {
+                margin: 0 $space-m 0 0;
+                color: shade($docs-color-silver, 20%);
+                font-size: $size-s;
+                align-items: center;
+                display: flex;
+
+                @media (max-width: 1000px) {
+                    width: 50%;
+                    float: left;
+                    margin: 0;
+                }
+
+                svg,
+                span {
+                    margin: 0 calc(#{$space-s} / 2) 0 0;
+                }
+
+                p {
+                    margin-bottom: 0;
+
+                    @media (max-width: 1000px) {
+                        margin: $space-xs;
+                    }
+                }
+            }
+        }
     }
-  }
-}
 </style>
 
 <docs>
