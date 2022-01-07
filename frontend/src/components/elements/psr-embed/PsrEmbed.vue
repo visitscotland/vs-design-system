0c8b704d8af9d66514ebf2e7de0b5dc24291da73
jhaddrill <james.haddrill@visitscotland.com>
2022-01-07 13:50:09 +0000

    VS-3054 FEAT (PsrEmbed) add domain prop to component example

--- frontend/src/components/elements/psr-embed/PsrEmbed.vue
+++ frontend/src/components/elements/psr-embed/PsrEmbed.vue
@@ -124,7 +124,7 @@ export default {
         <VsRow>
             <VsCol md="6">
                 <vs-psr-embed
-                    :config="[{'lang': 'fr'},{'subSearchType': 'acco'},{'locplace': '4161'}]"
+                    :config="[{'lang': 'fr'},{'subSearchType': 'acco'},{'locplace': '4161'},{'domain':'http://172.28.81.65:8089'}]"
                 />
             </VsCol>
         </VsRow>
