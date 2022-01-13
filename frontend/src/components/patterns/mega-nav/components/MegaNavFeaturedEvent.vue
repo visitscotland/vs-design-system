4e7b23359f1e62786d48c79842a426366c14c6c4
Michael Prosser <michael.prosser@visitscotland.com>
2022-01-13 12:12:54 +0000

    VS-3058 FEAT (Featured Event) Framework of feat ev component

---
+++ frontend/src/components/patterns/mega-nav/components/MegaNavFeaturedEvent.vue
@@ -0,0 +1,238 @@
+<template>
+    <VsStretchedLinkCard
+        :link="link"
+        type="internal"
+        :img-src="imgUrl"
+        :img-alt="imgAlt"
+        class="vs-mega-nav-featured-event"
+        data-test="vs-mega-nav-featured-event"
+    >
+        <template slot="stretchedCardContent">
+            <p>{{ datesLabel }}{{ dates }}</p>
+            <p>{{ title }}</p>
+            <p>{{ location }} </p>
+        </template>
+
+        <template slot="stretchedCardLink">
+            <!-- @slot Featured Event link text  -->
+            <slot name="vsFeaturedEventLink" />
+        </template>
+    </VsStretchedLinkCard>
+</template>
+
+<script>
+import VsStretchedLinkCard from '@components/patterns/stretched-link-card/StretchedLinkCard';
+
+/**
+ *  This component is used to show a featured event in the meganav
+ *
+ * @displayName MegaNav Featured Event
+ */
+
+export default {
+    name: 'VsMegaNavFeaturedEvent',
+    status: 'prototype',
+    release: '0.1.0',
+    components: {
+        VsStretchedLinkCard,
+    },
+    props: {
+        /**
+        * Link URL for the featured item
+        */
+        link: {
+            type: String,
+            required: true,
+        },
+        /**
+        * Image URL for the featured item
+        */
+        imgUrl: {
+            type: String,
+            required: true,
+        },
+        /**
+        * Image alt text for the featured item
+        */
+        imgAlt: {
+            type: String,
+            default: '',
+        },
+        datesLabel: {
+            type: String,
+            default: '',
+        },
+        dates: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        location: {
+            type: String,
+            default: '',
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+    .card.vs-mega-nav-featured-event {
+        border-top: 1px solid #e0e0e0;
+
+        &:hover {
+            box-shadow: -7px 12px 27px -13px rgba(0, 0, 0, 0.4);
+        }
+
+        &:focus {
+            .vs-stretched-link-card__link {
+                outline: 2px;
+            }
+        }
+
+        &--bottom {
+            bottom: 0;
+            top: auto;
+        }
+
+        .card-body {
+            position: absolute;
+            width: 80%;
+            bottom: 0;
+            left: 0;
+            background: $color-white;
+            padding: $spacer-5 $spacer-3;
+        }
+
+        .card-title {
+            margin-top: $spacer-0;
+            font-size: $font-size-sm;
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+        }
+
+        .vs-stretched-link-card__content {
+            display: none;
+            -webkit-line-clamp: 4;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+        }
+
+        .vs-stretched-link-card__link {
+            font-size: $small-font-size;
+        }
+
+        @include media-breakpoint-up(sm) {
+            display: flex;
+            flex-direction: row-reverse;
+
+            .card-title {
+                font-size: $font-size-base;
+            }
+
+            .card-body {
+                position: initial;
+                width: auto;
+                max-width: 100%;
+                padding: $spacer-8;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+
+                // IE11 fix as it doesn't like position: initial
+                @media screen and (-ms-high-contrast: active), screen and
+                    (-ms-high-contrast: none) {
+                    position: relative;
+                }
+            }
+
+            .vs-stretched-link-card__img {
+                width: 245px;
+                height: auto;
+            }
+        }
+
+        @include media-breakpoint-up(lg) {
+            padding: $spacer-3;
+            flex-direction: column;
+            border-top: none;
+
+            .card-body {
+                padding: $spacer-5 $spacer-3;
+            }
+
+            .vs-stretched-link-card__img {
+                width: 100%;
+            }
+
+            .card-title {
+                font-size: $font-size-sm;
+            }
+
+            .vs-stretched-link-card__content {
+                display: -webkit-box;
+                margin: $spacer-0;
+                color: $color-base-text;
+                font-size: $small-font-size;
+            }
+
+            .vs-stretched-link-card__link {
+                align-self: flex-end;
+                padding: $spacer-2;
+            }
+        }
+    }
+
+    @include no-js {
+        @include media-breakpoint-down(md) {
+            .card.vs-mega-nav-featured-event {
+                .vs-stretched-link-card__img {
+                    width: 100%;
+                }
+
+                .card-body {
+                    position: absolute;
+                    width: 80%;
+                    bottom: 0;
+                    left: 0;
+                    background: #fff;
+                    padding: $spacer-5 $spacer-3;
+                }
+
+                .card-title {
+                    font-size: $font-size-sm;
+                    margin-bottom: 0;
+                }
+            }
+        }
+    }
+</style>
+
+<docs>
+    ```jsx
+        <VsContainer>
+            <VsRow>
+                <VsCol cols="12" lg="4">
+                    <MegaNavFeaturedEvent
+                        link="www.visitscotland.com"
+                        img-url="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
+                        img-alt="Alt text"
+                        title="Autism Friendly Ancient Egyptian Family Workshop Day"
+                        location="South Nutshill Industrial Park"
+                        dates-label="Dates from"
+                        dates="4th Dec 2020"
+                    >
+
+                        <template slot="vsFeaturedEventLink">
+                            View event details
+                        </template>
+                    </MegaNavFeaturedEvent>
+                </VsCol>
+            </VsRow>
+        </VsContainer>
+    ```
+</docs>
