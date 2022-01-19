43bf0214245d7d824c1c42046e342744b88e6929
Michael Prosser <michael.prosser@visitscotland.com>
2022-01-19 14:03:24 +0000

    VS-3058 FEAT (Featured Event) Pull down event details from DMS

--- frontend/src/components/patterns/mega-nav/__examples__/meganav.example.vue
+++ frontend/src/components/patterns/mega-nav/__examples__/meganav.example.vue
@@ -50,18 +50,8 @@
                     v-if="item.title === 'Things to do'"
                 >
                     <MegaNavFeaturedEvent
-                        link="www.visitscotland.com"
-                        img-url="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
-                        img-alt="Alt text"
-                        title="Autism Friendly Ancient Egyptian Family Workshop Day"
-                        location="South Nutshill Industrial Park"
-                        dates-label="Dates from"
-                        dates="4 Dec 2020"
-                    >
-                        <template slot="vsFeaturedEventLink">
-                            View event details
-                        </template>
-                    </MegaNavFeaturedEvent>
+                        source-url="http://172.28.81.65:8089/data/component/cannedsearch?prodtypes=even&locplace=&locprox=&loc=Scotland&size=1"
+                    />
                 </template>
 
                 <template
@@ -174,18 +164,8 @@
                             v-if="item.title === 'Things to do'"
                         >
                             <MegaNavFeaturedEvent
-                                link="www.visitscotland.com"
-                                img-url="https://cimg.visitscotland.com/cms-images/attractions/outlander/claire-standing-stones-craigh-na-dun-outlander?size=sm"
-                                img-alt="Alt text"
-                                title="Autism Friendly Ancient Egyptian Family Workshop Day"
-                                location="South Nutshill Industrial Park"
-                                dates-label="Dates from"
-                                dates="4 Dec 2020"
-                            >
-                                <template slot="vsFeaturedEventLink">
-                                    View event details
-                                </template>
-                            </MegaNavFeaturedEvent>
+                                source-url="http://172.28.81.65:8089/data/component/cannedsearch?prodtypes=even&locplace=&locprox=&loc=Scotland&size=1"
+                            />
                         </template>
                         <template
                             v-if="item.title === 'Accommodation' || item.title === 'Inspiration'"
