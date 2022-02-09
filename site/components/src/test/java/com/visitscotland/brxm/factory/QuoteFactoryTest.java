45233500df6182c074305ce4a32b3af6d67ccbec
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2022-02-09 18:50:54 +0000

    VS-3225 FEAT (Quote) fixing tests

--- site/components/src/test/java/com/visitscotland/brxm/factory/QuoteFactoryTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/factory/QuoteFactoryTest.java
@@ -6,6 +6,7 @@ import com.visitscotland.brxm.hippobeans.SharedLink;
 import com.visitscotland.brxm.hippobeans.capabilities.Linkable;
 import com.visitscotland.brxm.model.FlatLink;
 import com.visitscotland.brxm.model.FlatQuote;
+import com.visitscotland.brxm.model.Module;
 import com.visitscotland.brxm.model.megalinks.EnhancedLink;
 import com.visitscotland.brxm.services.LinkService;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
@@ -54,7 +55,7 @@ class QuoteFactoryTest {
         when(link.getLinkType()).thenReturn(link);
         when((linkService).createCTALink(any(), eq(Locale.UK), eq(link))).thenReturn(flatLink);
 
-
+        when(enhancedLink.getLink()).thenReturn("www.google.com");
         FlatQuote flat = embedder.getQuote(quote, null, Locale.UK);
 
         verify(linkService).createEnhancedLink(link, null, Locale.UK, false);
