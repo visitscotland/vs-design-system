fa35b0ff0116f137ecf2255b9048676307ff4d91
Ben Banerjee-Richards <Ben.Banerjee-Richards@visitscotland.com>
2022-02-11 09:41:21 +0000

    VS-3224 (Banner) Check banner path property exists

--- site/components/src/main/java/com/visitscotland/brxm/factory/BannerFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/BannerFactory.java
@@ -6,6 +6,7 @@ import com.visitscotland.brxm.model.FlatLink;
 import com.visitscotland.brxm.services.LinkService;
 import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.brxm.utils.HippoUtilsService;
+import com.visitscotland.utils.Contract;
 import org.hippoecm.hst.content.beans.ObjectBeanManagerException;
 import org.hippoecm.hst.content.beans.query.exceptions.QueryException;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 import java.util.Locale;
+import java.util.NoSuchElementException;
 
 @Component
 public class BannerFactory {
@@ -35,6 +37,9 @@ public class BannerFactory {
 
     public BannerModule getBannerModule(HstRequest request) {
         String relativeBannerPath = bundle.getResourceBundle("banner", "path", Locale.UK);
+        if (Contract.isEmpty(relativeBannerPath)) {
+            return null;
+        }
         try {
             HippoBean bean = hippoUtilsService.getDocumentFromContent(relativeBannerPath);
             if (bean instanceof Banner) {
@@ -42,8 +47,8 @@ public class BannerFactory {
             } else {
                contentLogger.warn("Expected Banner bean at {}, but found {}", relativeBannerPath, bean == null ? "null" : bean.getClass().getSimpleName());
             }
-        } catch (PathNotFoundException e) {
-            logger.debug("No published banner found at {}", relativeBannerPath);
+        } catch (PathNotFoundException | NoSuchElementException e) {
+            logger.trace("No published banner found at {}", relativeBannerPath);
         } catch (RepositoryException | ObjectBeanManagerException | QueryException e) {
             logger.error("Failed to access repo to access banner at location {}", relativeBannerPath, e);
         }
