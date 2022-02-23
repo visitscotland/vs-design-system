92ca975d8768ff6a10106ea474fbe068bf138c74
Gemma <gemma.stephen@visitscotland.com>
2022-02-23 12:22:56 +0000

    VS-2774 FIX (DesignSystem) Adding svgs in to the code

--- frontend/docs/components/rsg-components/LogoRenderer.js
+++ frontend/docs/components/rsg-components/LogoRenderer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Styled from 'rsg-components/Styled';
-import logo from '@images/ds-logo-png.png';
+import logo from '@images/design_system_logo.svg';
 
 const styles = () => ({
 	image: {
