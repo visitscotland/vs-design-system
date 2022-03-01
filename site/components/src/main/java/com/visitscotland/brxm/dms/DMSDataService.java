b8421cb25fb650cef310b4f54c94d5bf49d5cd40
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2022-03-01 14:57:02 +0000

    VS-2536 FEAT (Cache) remove unused imports

--- site/components/src/main/java/com/visitscotland/brxm/dms/DMSDataService.java
+++ site/components/src/main/java/com/visitscotland/brxm/dms/DMSDataService.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.visitscotland.utils.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
