dfc36ff69c99dc94b8fb61cf3985e7cee051c9e8
ahenderson <allan.henderson@visitscotland.com>
2022-03-14 12:13:24 +0000

    Nuxt3 testing

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-index">
+Hello
+  </div>
+</template>
+
+<style>
+body {
+    font-family: Source Sans Pro, Helvetica, Arial, sans-serif, "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+}
+.page-index {
+  padding-top: 60px;
+  text-align: center;
+}
+</style>
+<script>
+export default {
+    head() {
+        return {
+            script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        };
+    }
+}
+</script>
