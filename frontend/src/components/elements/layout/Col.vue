47aa17d8a4de5b4bf94c97150d0342e49e396518
Gemma <gemma.stephen@visitscotland.com>
2022-03-15 11:32:13 +0000

    FEAT (DesignSystem) Updating layout docs for col, ro and container

--- frontend/src/components/elements/layout/Col.vue
+++ frontend/src/components/elements/layout/Col.vue
@@ -10,10 +10,7 @@ import { BCol } from 'bootstrap-vue';
 
 /**
  * Column components are used to organise, align and space sibling blocks of content within row
- * elements. They are always direct children of row elements/components. Our column component
- * utilises <a href="https://bootstrap-vue.js.org/docs/components/layout">the Bootstrap Vue BCol component</a>.
- *
- * NOTE: this component is a wrapper for the BCol component so any BCol prop can be applied to it.
+ * elements.
  *
  * @displayName Col
  */
@@ -27,25 +24,3 @@ export default {
     },
 };
 </script>
-
-<docs>
-  ```jsx
-    <div class="style-cols">
-        <VsContainer>
-            <VsRow>
-                <VsCol cols="6" sm="12">
-                    <pre>cols="6" sm="12"</pre>
-                </VsCol>
-            </VsRow>
-            <VsRow>
-                <VsCol cols="8" lg="3">
-                    <pre>cols="8" lg="3"</pre>
-                </VsCol>
-                <VsCol>
-                    <pre>no cols props</pre>
-                </VsCol>
-            </VsRow>
-        </VsContainer>
-    </div>
-  ```
-</docs>
