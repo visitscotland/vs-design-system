8c7a27b70cd64778741754d82bceea2c31c13cc3
mprosser <Michael.Prosser@visitscotland.com>
2022-05-06 12:28:44 +0100

    VS-3401 FEAT (Video Megalinks) Add test for videoId

--- frontend/src/components/patterns/stretched-link-card/StretchedLinkCard.vue
+++ frontend/src/components/patterns/stretched-link-card/StretchedLinkCard.vue
@@ -24,6 +24,7 @@
         <div class="card-body">
             <VsButton
                 class="vs-stretched-link-card__video-button"
+                data-test="vs-stretched-link-card__video-button"
                 icon="play"
                 icon-position="left"
                 size="md"
