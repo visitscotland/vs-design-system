c4ca81d81df9187cde3fcaf9b5150ea8f69bc24d
jhaddrill <james.haddrill@visitscotland.com>
2022-05-09 13:56:03 +0100

    VS-3497 FEAT (cookie.store) add cookie store

--- frontend/src/components/elements/cookies/index.js
+++ frontend/src/components/elements/cookies/index.js
@@ -1,6 +1,5 @@
-import VsCookiesChecker from './CookiesChecker';
 import VsCookiesFallback from './CookiesFallback';
 
 export default {
-    VsCookiesChecker, VsCookiesFallback,
+    VsCookiesFallback,
 };
