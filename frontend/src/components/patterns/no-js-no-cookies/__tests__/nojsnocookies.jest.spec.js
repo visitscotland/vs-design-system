11a94cc256fdc177091177be998926b40181f3fa
mprosser <Michael.Prosser@visitscotland.com>
2022-05-20 14:59:01 +0100

    VS-3432 FEAT (JS Cookie Checker) Add tests for all props

--- frontend/src/components/patterns/no-js-no-cookies/__tests__/nojsnocookies.jest.spec.js
+++ frontend/src/components/patterns/no-js-no-cookies/__tests__/nojsnocookies.jest.spec.js
@@ -6,8 +6,116 @@ const factoryShallowMount = (slotData) => shallowMount(VsNoJsNoCookies, {
 });
 
 describe('VsNoJsNoCookies', () => {
-    it('should render a component with the data-test attribute `vs-no-js-no-cookies`', () => {
+    it('should render a component with the data-test attribute `vs-no-js-cookies`', () => {
         const wrapper = factoryShallowMount();
-        expect(wrapper.attributes('data-test')).toBe('vs-no-js-no-cookies');
+        expect(wrapper.attributes('data-test')).toBe('vs-no-js-cookies');
+    });
+
+    describe(':props', () => {
+        it('should render a wrapper with the test attr `vs-no-js-cookies__no-js` if `jsDisabled` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            await wrapper.setProps({
+                jsDisabled: true,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-js"]').exists()).toBe(true);
+        });
+
+        it('should render a wrapper with the test attr `vs-no-js-cookies__no-cookies` if `cookiesMissing` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            await wrapper.setProps({
+                cookiesMissing: true,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies"]').exists()).toBe(true);
+        });
+
+        it('should render the no-js wrapper and not the no-cookies wrapper if both `jsDisabled` and `cookiesMissing` are set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            await wrapper.setProps({
+                jsDisabled: true,
+                cookiesMissing: true,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-js"]').exists()).toBe(true);
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies"]').exists()).toBe(false);
+        });
+
+        it('should render the `noJsMessage` prop if `jsDisabled` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            const noJsMessage = 'There is no javascript';
+
+            await wrapper.setProps({
+                jsDisabled: true,
+                noJsMessage,
+            });
+
+            expect(wrapper.html()).toContain(noJsMessage);
+        });
+
+        it('should render the `noCookiesMessage` prop if `cookiesMissing` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            const noCookiesMessage = 'There are missing cookies';
+
+            await wrapper.setProps({
+                cookiesMissing: true,
+                noCookiesMessage,
+            });
+
+            expect(wrapper.html()).toContain(noCookiesMessage);
+        });
+
+        it('should render a link with the test attr `vs-no-js-cookies__no-cookies-link` if `noCookiesLink` provided and `cookiesMissing` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            const noCookiesLink = {
+                url: 'https://google.com',
+                lable: 'Update settings here',
+            };
+
+            await wrapper.setProps({
+                cookiesMissing: true,
+                noCookiesLink,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies-link"]').exists()).toBe(true);
+        });
+
+        it('should render a link with the href provided in `noCookiesLink` if `cookiesMissing` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            const noCookiesLink = {
+                url: 'https://google.com',
+                label: 'Update settings here',
+            };
+
+            await wrapper.setProps({
+                cookiesMissing: true,
+                noCookiesLink,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies-link"]').props().href).toBe(noCookiesLink.url);
+        });
+
+        it('should render a link with the content provided in `noCookiesLink` if `cookiesMissing` is set to true', async() => {
+            const wrapper = factoryShallowMount();
+
+            const noCookiesLink = {
+                url: 'https://google.com',
+                label: 'Update settings here',
+            };
+
+            await wrapper.setProps({
+                cookiesMissing: true,
+                noCookiesLink,
+            });
+
+            expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies-link"]').html()).toContain(noCookiesLink.label);
+        });
     });
 });
