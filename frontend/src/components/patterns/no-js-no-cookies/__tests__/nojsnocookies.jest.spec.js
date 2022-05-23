f92eefc8b88ab8f6d1bf762b4390936b3e3e57f1
mprosser <Michael.Prosser@visitscotland.com>
2022-05-23 12:10:04 +0100

    VS-3432 FEAT (JS Cookie Checker) Update tests with default jsDisabled value

--- frontend/src/components/patterns/no-js-no-cookies/__tests__/nojsnocookies.jest.spec.js
+++ frontend/src/components/patterns/no-js-no-cookies/__tests__/nojsnocookies.jest.spec.js
@@ -1,23 +1,22 @@
 import { shallowMount } from '@vue/test-utils';
 import VsNoJsNoCookies from '../NoJsNoCookies';
 
-const factoryShallowMount = (slotData) => shallowMount(VsNoJsNoCookies, {
-    ...slotData,
+const factoryShallowMount = () => shallowMount(VsNoJsNoCookies, {
+    propsData: {
+        jsDisabled: true,
+    },
 });
 
 describe('VsNoJsNoCookies', () => {
     it('should render a component with the data-test attribute `vs-no-js-cookies`', () => {
         const wrapper = factoryShallowMount();
+
         expect(wrapper.attributes('data-test')).toBe('vs-no-js-cookies');
     });
 
     describe(':props', () => {
-        it('should render a wrapper with the test attr `vs-no-js-cookies__no-js` if `jsDisabled` is set to true', async() => {
+        it('should render a wrapper with the test attr `vs-no-js-cookies__no-js` if `jsDisabled` is set to true', () => {
             const wrapper = factoryShallowMount();
-
-            await wrapper.setProps({
-                jsDisabled: true,
-            });
 
             expect(wrapper.find('[data-test="vs-no-js-cookies__no-js"]').exists()).toBe(true);
         });
@@ -27,6 +26,7 @@ describe('VsNoJsNoCookies', () => {
 
             await wrapper.setProps({
                 cookiesMissing: true,
+                jsDisabled: false,
             });
 
             expect(wrapper.find('[data-test="vs-no-js-cookies__no-cookies"]').exists()).toBe(true);
@@ -63,6 +63,7 @@ describe('VsNoJsNoCookies', () => {
             const noCookiesMessage = 'There are missing cookies';
 
             await wrapper.setProps({
+                jsDisabled: false,
                 cookiesMissing: true,
                 noCookiesMessage,
             });
@@ -79,6 +80,7 @@ describe('VsNoJsNoCookies', () => {
             };
 
             await wrapper.setProps({
+                jsDisabled: false,
                 cookiesMissing: true,
                 noCookiesLink,
             });
@@ -95,6 +97,7 @@ describe('VsNoJsNoCookies', () => {
             };
 
             await wrapper.setProps({
+                jsDisabled: false,
                 cookiesMissing: true,
                 noCookiesLink,
             });
@@ -111,6 +114,7 @@ describe('VsNoJsNoCookies', () => {
             };
 
             await wrapper.setProps({
+                jsDisabled: false,
                 cookiesMissing: true,
                 noCookiesLink,
             });
