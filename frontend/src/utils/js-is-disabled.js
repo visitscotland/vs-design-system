77f594c893fe67e3761a0ff54b2caa60f9697059
mprosser <Michael.Prosser@visitscotland.com>
2022-05-27 12:23:14 +0100

    VS-3432 FEAT (JS Cookie Checker) Integrate js-is-disabled check and warnings into stretched link card

--- frontend/src/utils/js-is-disabled.js
+++ frontend/src/utils/js-is-disabled.js
@@ -3,7 +3,7 @@ import noJsClass from '@/utils/no-js-class';
 const jsIsDisabled = () => {
     const elements = document.getElementsByClassName(noJsClass);
 
-    return elements;
+    return elements.length > 0;
 };
 
 export default jsIsDisabled;
