4f04a04d5dc727e800a550d33e4ad44676199d4a
jhaddrill <james.haddrill@visitscotland.com>
2022-06-07 09:37:22 +0100

    VS-3611 FEAT (required-cookies-data) update settings

--- frontend/src/utils/required-cookies-data.js
+++ frontend/src/utils/required-cookies-data.js
@@ -1,6 +1,6 @@
 const cookieValues = {
     youtube: ['C0001', 'C0004'],
-    languageSelector: ['C0001'],
+    languageSelector: ['C0001, C0003'],
     embed: ['C0001', 'C0003', 'C0004'],
     recaptcha: ['C0001'],
     map: ['C0001'],
