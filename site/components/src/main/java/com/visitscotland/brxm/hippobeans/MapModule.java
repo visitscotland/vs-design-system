d7bb686011a34c3acdd8fcec13c13f68761d58d0
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2022-06-15 09:23:20 +0100

    VS-3488 FEAT (Maps) beans

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/MapModule.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/MapModule.java
@@ -3,6 +3,7 @@ package com.visitscotland.brxm.hippobeans;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import java.util.Calendar;
 
 /** 
  * TODO: Beanwriter: Failed to create getter for node type: hippo:compound
@@ -28,5 +29,25 @@ public class MapModule extends BaseDocument {
     @HippoEssentialsGenerated(internalName = "hippotaxonomy:keys")
     public String[] getKeys() {
         return getMultipleProperty("hippotaxonomy:keys");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:diff")
+    public String getDiff() {
+        return getSingleProperty("visitscotland:diff");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationDeadline")
+    public Calendar getTranslationDeadline() {
+        return getSingleProperty("visitscotland:translationDeadline");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationFlag")
+    public Boolean getTranslationFlag() {
+        return getSingleProperty("visitscotland:translationFlag");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translationPriority")
+    public String getTranslationPriority() {
+        return getSingleProperty("visitscotland:translationPriority");
     }
 }
