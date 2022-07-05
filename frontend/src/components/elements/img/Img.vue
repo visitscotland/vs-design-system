82903516d627d844c9b741cc958a18ec5954b612
jhaddrill <james.haddrill@visitscotland.com>
2022-07-05 11:24:13 +0100

    VS-3678 FEAT (ImageWithCaption, Img) add optional lazy loading

--- frontend/src/components/elements/img/Img.vue
+++ frontend/src/components/elements/img/Img.vue
@@ -5,7 +5,7 @@
         :alt="alt"
         :fluid="fluid"
         :fluid-grow="fluidGrow"
-        loading="lazy"
+        :loading="useLazyLoading ? 'lazy' : 'eager'"
         :style="imgStyle"
         class="low-res-img"
     >
@@ -69,6 +69,13 @@ export default {
          */
         fluidGrow: {
             type: Boolean,
+        },
+        /**
+         * If true switches on lazy loading for the image
+        */
+        useLazyLoading: {
+            type: Boolean,
+            default: true,
         },
     },
     computed: {
