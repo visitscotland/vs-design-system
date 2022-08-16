d843bdf6d1933378f1a7999d51afacf49cd4753a
jhaddrill <james.haddrill@visitscotland.com>
2022-08-16 14:33:03 +0100

    VS-3845 FEAT (tests) fix failing tests

--- frontend/src/components/elements/tag-manager-wrapper/__tests__/TagManager.jest.spec.js
+++ frontend/src/components/elements/tag-manager-wrapper/__tests__/TagManager.jest.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
 import VsTagManagerWrapper from '../TagManagerWrapper';
 
-const factoryShallowMount = shallowMount(VsTagManagerWrapper);
+const factoryShallowMount = () => shallowMount(VsTagManagerWrapper);
 
 describe('VsTagManagerWrapper', () => {
     it('should render a b-link with class vs-link', () => {
         const wrapper = factoryShallowMount();
 
-        expect(wrapper.find('div[data-test=vs-tag-manager-wrapper]').exists()).toBe(true);
+        expect(wrapper.find('span[data-test=vs-tag-manager-wrapper]').exists()).toBe(true);
     });
 });
