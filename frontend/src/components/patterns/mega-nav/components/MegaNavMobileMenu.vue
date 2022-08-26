47c3cb6d820773c64bd4aec6ddc48de7a33068cb
jhaddrill <james.haddrill@visitscotland.com>
2022-08-26 09:12:10 +0100

    VS-3827 FEAT (MegaNav) refactor mega nav

---
+++ frontend/src/components/patterns/mega-nav/components/MegaNavMobileMenu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+        class="mega-nav-mobile-menu">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VsMegaNavDropdown',
+    status: 'prototype',
+    release: '0.1.0',
+};
+</script>
+
+<style lang="scss">
+    .mega-nav-mobile-menu {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+    }
+</style>
