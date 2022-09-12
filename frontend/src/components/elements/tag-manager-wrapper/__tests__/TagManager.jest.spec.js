688797f4eca8055bd68ed375fca504a527ba05be
jhaddrill <james.haddrill@visitscotland.com>
2022-09-12 13:05:08 +0100

    VS-3771 FEAT (various) Add and fix unit tests

--- frontend/src/components/elements/tag-manager-wrapper/__tests__/TagManager.jest.spec.js
+++ frontend/src/components/elements/tag-manager-wrapper/__tests__/TagManager.jest.spec.js
@@ -5,7 +5,7 @@ const factoryShallowMount = () => shallowMount(VsTagManagerWrapper);
 
 describe('VsTagManagerWrapper', () => {
     it('should render a span with test attribute of vs-tag-manager-wrapper', () => {
-        const wrapper = factoryShallowMount;
+        const wrapper = factoryShallowMount();
 
         expect(wrapper.find('span[data-test=vs-tag-manager-wrapper]').exists()).toBe(true);
     });
