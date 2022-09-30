1e60a2463ed69620eb8923aefe4d691d78150935
jhaddrill <james.haddrill@visitscotland.com>
2022-09-30 10:49:55 +0100

    VS-3998 FEAT (MainMapWrapperPanel) create side panel component

---
+++ frontend/src/components/patterns/main-map-wrapper/components/MainMapWrapperPanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div data-test="vs-main-map-wrapper-panel">
+        Panel
+    </div>
+</template>
+
+<script>
+/**
+ * Renders a side panel for the map wrapper component
+ *
+ * @displayName Main Map Wrapper Panel
+ */
+
+export default {
+    name: 'VsMainMapWrapperPanel',
+    status: 'prototype',
+    release: '0.0.1',
+};
+</script>
+
+<style lang="scss">
+
+</style>
