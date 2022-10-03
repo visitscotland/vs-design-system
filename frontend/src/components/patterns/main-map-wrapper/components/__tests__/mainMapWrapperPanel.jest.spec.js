42eb5144d71827cc4fa1c355e92eac9735ce4d7a
jhaddrill <james.haddrill@visitscotland.com>
2022-10-03 14:24:18 +0100

    VS-3998 FEAT (Map) add tests

--- frontend/src/components/patterns/main-map-wrapper/components/__tests__/mainMapWrapperPanel.jest.spec.js
+++ frontend/src/components/patterns/main-map-wrapper/components/__tests__/mainMapWrapperPanel.jest.spec.js
@@ -6,4 +6,13 @@ describe('VsMainMapWrapperPanel', () => {
         const wrapper = shallowMount(VsMainMapWrapperPanel);
         expect(wrapper.attributes('data-test')).toBe('vs-main-map-wrapper-panel');
     });
+
+    it('should emit `close-panel` when the close button is clicked', async() => {
+        const wrapper = shallowMount(VsMainMapWrapperPanel);
+        const closeBtn = wrapper.find('[data-test="vs-main-map-wrapper-panel--btn"]');
+
+        await closeBtn.trigger('click');
+
+        expect(wrapper.emitted('close-panel')).toBeTruthy();
+    });
 });
