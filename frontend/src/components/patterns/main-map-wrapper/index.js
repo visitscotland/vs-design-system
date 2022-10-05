4817a7937b3337334a42d8e57f3da59cada5e1c6
jhaddrill <james.haddrill@visitscotland.com>
2022-10-05 16:00:14 +0100

    VS-3999 FEAT (MainMapWrapperCategories) add categories view

--- frontend/src/components/patterns/main-map-wrapper/index.js
+++ frontend/src/components/patterns/main-map-wrapper/index.js
@@ -1,7 +1,9 @@
 import VsMainMapWrapper from './MainMapWrapper';
 import VsMainMapWrapperPanel from './components/MainMapWrapperPanel';
+import VsMainMapWrapperCategories from './components/MainMapWrapperCategories';
 
 export default {
     VsMainMapWrapper,
     VsMainMapWrapperPanel,
+    VsMainMapWrapperCategories,
 };
