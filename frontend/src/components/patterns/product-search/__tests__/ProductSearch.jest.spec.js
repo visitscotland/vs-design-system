53f5748e6dffaad07e9d85c95d8e63e3da58bb84
jhaddrill <james.haddrill@visitscotland.com>
2022-10-06 09:23:44 +0100

    VS-3449 FIX (ProductSearch) change no-js message to a prop

--- frontend/src/components/patterns/product-search/__tests__/ProductSearch.jest.spec.js
+++ frontend/src/components/patterns/product-search/__tests__/ProductSearch.jest.spec.js
@@ -8,6 +8,7 @@ const factoryShallowMount = (slotData) => shallowMount(VsProductSearch, {
                 subSearchType: 'acco',
             },
         ],
+        noJsMessage: 'This is the no JS message',
     },
     ...slotData,
 });
@@ -33,16 +34,6 @@ describe('VsProductSearch', () => {
 
             expect(wrapper.html()).toContain('This is the Product Search intro');
         });
-
-        it('should render no-js slot content', () => {
-            const wrapper = factoryShallowMount({
-                slots: {
-                    vsNoJsMessage: 'This is the no JS message',
-                },
-            });
-
-            expect(wrapper.html()).toContain('This is the no JS message');
-        });
     });
 
     describe(':props', () => {
@@ -51,6 +42,12 @@ describe('VsProductSearch', () => {
 
             const embedStub = wrapper.find('vsProductSearchEmbed-stub');
             expect(embedStub.attributes('config')).toBe('[object Object]');
+        });
+
+        it('should render `noJsMessage` prop content', () => {
+            const wrapper = factoryShallowMount();
+
+            expect(wrapper.html()).toContain('This is the no JS message');
         });
     });
 });
