5e45f604587154236a0c5cc3faaf34cd303c1baf
jhaddrill <james.haddrill@visitscotland.com>
2022-10-14 10:55:50 +0100

    VS-4001 FEAT (MainMapWrapperButtons) add buttons component

---
+++ frontend/src/components/patterns/main-map-wrapper/components/MainMapWrapperButtons.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="vs-main-map-wrapper-buttons">
+        <VsButton
+            block
+            :href="contentData.properties.link"
+        >
+            {{ discoverText }} {{ contentData.properties.title }}
+        </VsButton>
+    </div>
+</template>
+
+<script>
+import VsButton from '@components/elements/button/Button';
+
+/**
+ * Renders link buttons for a map place
+ *
+ * @displayName Main Map Wrapper Buttons
+ */
+export default {
+    name: 'VsMainMapWrapperButtons',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        VsButton,
+    },
+    props: {
+        /**
+         * Data for component content
+         */
+        contentData: {
+            type: Object,
+            required: true,
+        },
+    },
+    inject: [
+        'discoverText',
+    ],
+};
+</script>
+
+<style lang="scss">
+    .vs-main-map-wrapper-buttons {
+        position: sticky;
+        bottom: 0;
+        padding: 1rem 0;
+        width: 100%;
+        background: white;
+        border-top: 4px solid red;
+        text-align: center;
+
+        @include media-breakpoint-up(lg) {
+            padding: 1rem;
+        }
+    }
+</style>
