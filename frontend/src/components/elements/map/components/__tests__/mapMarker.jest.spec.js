be65d69e78c6605629edd6fec016d5eb2b62a02f
jhaddrill <james.haddrill@visitscotland.com>
2022-10-20 11:51:47 +0100

    VS-4087 FEAT (Map) add store to allow communication between components

--- frontend/src/components/elements/map/components/__tests__/mapMarker.jest.spec.js
+++ frontend/src/components/elements/map/components/__tests__/mapMarker.jest.spec.js
@@ -4,13 +4,46 @@ import VsMapMarker from '../MapMarker';
 import markerExample from './data/markerData.json';
 
 describe('VsMapMarker', () => {
-    it('should render a map component with the data-test attribute `vs-map`', () => {
+    it('should render a map component with the data-test attribute `vs-map-marker`', () => {
         const wrapper = shallowMount(VsMapMarker, {
             propsData: {
                 feature: markerExample,
             },
+            computed: {
+                isActive() {
+                    return true;
+                },
+                highlightedPlace() {
+                    return 'highlighted-id';
+                },
+            },
         });
 
         expect(wrapper.attributes('data-test')).toBe('vs-map-marker');
+    });
+
+    describe(':methods', () => {
+        it('should fire the `handleClick` method when clicked', async() => {
+            const mockMethod = jest.fn();
+            const wrapper = shallowMount(VsMapMarker, {
+                propsData: {
+                    feature: markerExample,
+                },
+                computed: {
+                    isActive() {
+                        return true;
+                    },
+                    highlightedPlace() {
+                        return 'highlighted-id';
+                    },
+                },
+                methods: {
+                    handleClick: mockMethod,
+                },
+            });
+
+            await wrapper.trigger('click');
+            expect(mockMethod).toHaveBeenCalled();
+        });
     });
 });
--- frontend/src/stores/map.store.js
+++ frontend/src/stores/map.store.js
@@ -4,33 +4,89 @@ import Vue from 'vue';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {
-        itineraries: {
-            namespaced: true,
-            state: {
-                highlightedStop: null,
-            },
-            mutations: {
-                SET_STOP_HIGHLIGHTED: (state, payload) => {
+    state: {
+        maps: [],
+    },
+    mutations: {
+        addMapInstance: (state, payload) => {
+            state.maps.push({
+                id: payload.id,
+                filters: payload.filters,
+                places: payload.places,
+                activePins: payload.activePins,
+                hovered: '',
+            });
+
+            console.log(state);
+        },
+        SET_HOVERED_PLACE: (state, payload) => {
+            state.maps.forEach((map) => {
+                if (map.id === payload.mapId) {
                     /* eslint-disable no-param-reassign */
-                    state.highlightedStop = payload;
-                },
-            },
-            actions: {
-                setStopHighlighted: ({ commit, getters }, payload) => {
-                    if (getters.getHighlightedStop === payload) {
-                        return false;
-                    }
-                    commit('SET_STOP_HIGHLIGHTED', payload);
-                    return true;
-                },
-            },
-            getters: {
-                getHighlightedStop: (state) => state.highlightedStop,
-                getHighlightedStopCoordinates: (state) => (state.highlightedStop === null
-                    ? [-4.07083, 56.18882]
-                    : state.highlightedStop.geometry.coordinates),
-            },
+                    map.hovered = payload.hoveredId;
+                }
+            });
+        },
+        SET_ACTIVE_PLACE: (state, payload) => {
+            state.maps.forEach((map) => {
+                if (map.id === payload.mapId) {
+                    /* eslint-disable no-param-reassign */
+                    map.activePlace = payload.placeId;
+                }
+            });
         },
     },
+    actions: {
+        setHoveredPlace: ({ commit }, payload) => {
+            // if (getters.getHoveredStop(payload.mapId) === payload.id) {
+            //     return false;
+            // }
+            commit('SET_HOVERED_PLACE', payload);
+        },
+        setActivePlace: ({ commit }, payload) => {
+            commit('SET_ACTIVE_PLACE', payload);
+        },
+    },
+    getters: {
+        getMaps: (state) => state,
+        getMapById: (state) => (id) => state.maps.find((map) => map.id === id),
+        getHoveredStop: (state) => (mapId) => {
+            const activeMap = state.maps.find((map) => map.id === mapId);
+            return activeMap.hovered;
+        },
+        getActivePlace: (state) => (mapId) => {
+            const activeMap = state.maps.find((map) => map.id === mapId);
+            return activeMap.activePlace;
+        },
+    },
+
+    // modules: {
+    //     itineraries: {
+    //         namespaced: true,
+    //         state: {
+    //             highlightedStop: null,
+    //         },
+    //         mutations: {
+    //             SET_STOP_HIGHLIGHTED: (state, payload) => {
+    //                 /* eslint-disable no-param-reassign */
+    //                 state.highlightedStop = payload;
+    //             },
+    //         },
+    //         actions: {
+    //             setStopHighlighted: ({ commit, getters }, payload) => {
+    //                 if (getters.getHighlightedStop === payload) {
+    //                     return false;
+    //                 }
+    //                 commit('SET_STOP_HIGHLIGHTED', payload);
+    //                 return true;
+    //             },
+    //         },
+    //         getters: {
+    //             getHighlightedStop: (state) => state.highlightedStop,
+    //             getHighlightedStopCoordinates: (state) => (state.highlightedStop === null
+    //                 ? [-4.07083, 56.18882]
+    //                 : state.highlightedStop.geometry.coordinates),
+    //         },
+    //     },
+    // },
 });
