fc8216d579770a56de0609127c544dec1fd2f58c
jhaddrill <james.haddrill@visitscotland.com>
2022-10-20 13:40:08 +0100

    VS-4087 FEAT (Map) remove commented out code

--- frontend/src/stores/map.store.js
+++ frontend/src/stores/map.store.js
@@ -36,9 +36,6 @@ export default new Vuex.Store({
     },
     actions: {
         setHoveredPlace: ({ commit }, payload) => {
-            // if (getters.getHoveredStop(payload.mapId) === payload.id) {
-            //     return false;
-            // }
             commit('SET_HOVERED_PLACE', payload);
         },
         setActivePlace: ({ commit }, payload) => {
@@ -57,34 +54,4 @@ export default new Vuex.Store({
             return activeMap.activePlace;
         },
     },
-
-    // modules: {
-    //     itineraries: {
-    //         namespaced: true,
-    //         state: {
-    //             highlightedStop: null,
-    //         },
-    //         mutations: {
-    //             SET_STOP_HIGHLIGHTED: (state, payload) => {
-    //                 /* eslint-disable no-param-reassign */
-    //                 state.highlightedStop = payload;
-    //             },
-    //         },
-    //         actions: {
-    //             setStopHighlighted: ({ commit, getters }, payload) => {
-    //                 if (getters.getHighlightedStop === payload) {
-    //                     return false;
-    //                 }
-    //                 commit('SET_STOP_HIGHLIGHTED', payload);
-    //                 return true;
-    //             },
-    //         },
-    //         getters: {
-    //             getHighlightedStop: (state) => state.highlightedStop,
-    //             getHighlightedStopCoordinates: (state) => (state.highlightedStop === null
-    //                 ? [-4.07083, 56.18882]
-    //                 : state.highlightedStop.geometry.coordinates),
-    //         },
-    //     },
-    // },
 });
