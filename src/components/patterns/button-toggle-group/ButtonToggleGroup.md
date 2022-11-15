b067cff430eab057a43e01932bb56a574e669ae3
jameshaddrillvs <james.haddrill@visitscotland.com>
2022-11-15 11:00:50 +0000

    FEAT (ButtonToggleGroup) tidy code

--- src/components/patterns/button-toggle-group/ButtonToggleGroup.md
+++ src/components/patterns/button-toggle-group/ButtonToggleGroup.md
@@ -1,5 +1,5 @@
 ## Usage
-The button toggle group creates a set of radio inputs that are styles as buttons.
+The button toggle group creates a set of radio inputs that are styled as buttons.
 They only allow one to be selected at a time.
 
 It takes two props - one which gives the data for the buttons and another which identifies
@@ -13,10 +13,12 @@ the initially selected item.
             {
                 text: 'Regions',
                 value: 'regions',
+                icon: 'map',
             },
             {
                 text: 'Places',
                 value: 'places',
+                icon: 'pin',
             },
         ]"
     />
--- src/components/patterns/button-toggle-group/ButtonToggleGroup.vue
+++ src/components/patterns/button-toggle-group/ButtonToggleGroup.vue
@@ -9,17 +9,47 @@
             class="vs-button-toggle-group--radios"
             id="btn-radios-1"
             v-model="selected"
-            :options="options"
             :aria-describedby="ariaDescribedby"
             name="radios-btn-default"
             buttons
             @change="toggleChange"
-        />
+            :class="groupTabbedInto ?
+                'vs-button-toggle-group--tabbed-focus' : ''"
+            @focusout="removeTabClass"
+        >
+            <div
+                v-for="option in options"
+                :key="option.text"
+                class="vs-button-toggle-group--button"
+                @keyup.tab="addTabClass"
+                @focusout="removeTabClass"
+            >
+                <BFormRadio
+                    :value="option.value"
+                    :key="option.text"
+                >
+                    <span>
+                        <VsSvg
+                            :path="iconPath(option)"
+                            class="mr-2"
+                            v-if="option.icon"
+                        />
+                        {{ option.text }}
+                    </span>
+                </BFormRadio>
+            </div>
+        </BFormRadioGroup>
     </BFormGroup>
 </template>
 
 <script>
-import { BFormGroup, BFormRadioGroup } from 'bootstrap-vue';
+import {
+    BFormGroup,
+    BFormRadioGroup,
+    BFormRadio,
+} from 'bootstrap-vue';
+import VsSvg from '@components/elements/svg/Svg';
+import initFontAwesome from '../../../utils/init-font-awesome';
 
 /**
  * A group of buttons that allow only one to be selected at a time
@@ -34,6 +64,8 @@ export default {
     components: {
         BFormGroup,
         BFormRadioGroup,
+        BFormRadio,
+        VsSvg,
     },
     props: {
         /**
@@ -62,9 +94,16 @@ export default {
     data() {
         return {
             selected: this.initialSelected,
+            groupTabbedInto: false,
         };
     },
+    watch: {
+        initialSelected(newVal) {
+            this.selected = newVal;
+        },
+    },
     mounted() {
+        initFontAwesome();
         if (this.initialSelected === '') {
             this.selected = this.options[0].value;
         }
@@ -77,6 +116,22 @@ export default {
         toggleChange(checked) {
             this.$emit('toggleChanged', checked);
         },
+        /**
+         * Return icon path for icon
+         */
+        iconPath(option) {
+            const color = this.selected === option.value ? 'purple' : 'white';
+            return `${option.icon}-${color}`;
+        },
+        addTabClass() {
+            this.groupTabbedInto = true;
+        },
+        removeTabClass(event) {
+            if (event.target.tagName !== 'INPUT') {
+                console.log('remove');
+                this.groupTabbedInto = false;
+            }
+        },
     },
 };
 </script>
@@ -84,7 +139,8 @@ export default {
 <style lang="scss">
     .vs-button-toggle-group {
         width: 100%;
-        text-align: center;
+        display: flex;
+        justify-content: center;
 
         legend {
             // sr-only styles
@@ -100,7 +156,7 @@ export default {
 
         &--radios {
             background-color: $color-purple;
-            display: inline-block;
+            display: flex;
             border-radius: 1000px;
             overflow: hidden;
 
@@ -113,6 +169,17 @@ export default {
             label.btn-secondary {
                 @extend %button-default-styles;
                 text-transform: uppercase;
+                padding-top: $spacer-2;
+                padding-bottom: $spacer-2;
+                display: flex;
+                align-items: center;
+
+                & > span {
+                    display: flex;
+                    height: 32px;
+                    align-items: center;
+                    padding: 0 $spacer-2;
+                }
 
                 @include vs-button-variant(
                     $color-white, $color-purple, $color-purple,
@@ -121,18 +188,64 @@ export default {
                 );
 
                 &:not(:disabled):not(.disabled).active {
+                    z-index: 2;
                     border-radius: 1000px;
 
                     @include vs-button-variant(
                         $color-purple, $color-white, $color-purple,
-                        $color-white, $color-purple, $color-purple,
+                        $color-purple, $color-white, $color-purple,
                         $color-white, $color-purple, $color-purple,
                     );
+
+                    @include media-breakpoint-up(lg) {
+                        @include vs-button-variant(
+                            $color-purple, $color-white, $color-purple,
+                            $color-white, $color-purple, $color-purple,
+                            $color-white, $color-purple, $color-purple,
+                        );
+
+                        &:hover {
+                            @include vs-button-variant(
+                                $color-purple, $color-white, $color-purple,
+                                $color-purple, $color-white, $color-purple,
+                                $color-white, $color-purple, $color-purple,
+                            );
+                        }
+                    }
                 }
 
-                &.active {
-                    z-index: 2;
+                &.focus {
+                    box-shadow: none;
                 }
+
+                &:hover {
+                    background-color: $color_pink_shade_4;
+                }
+            }
+        }
+
+        &--tabbed-focus {
+            input:focus {
+                + span {
+                    background-color: $color_yellow_tint_4;
+                    border-bottom: 3px solid $color_theme_primary;
+                }
+            }
+        }
+
+        &--button {
+            display: flex;
+            border: 1px solid $color-purple;
+
+            &:first-child {
+                margin-right: -(#{$spacer-4});
+                border-top-left-radius: 10000px;
+                border-bottom-left-radius: 10000px;
+            }
+
+            &:last-child {
+                border-top-right-radius: 10000px;
+                border-bottom-right-radius: 10000px;
             }
         }
     }
